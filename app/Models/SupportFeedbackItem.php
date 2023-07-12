2b46867a07386a679a43a3b0ae36b61ef8a98e34
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-07-12 15:33:40 -0400

    Fixes more model resolution
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- app/Models/SupportFeedbackItem.php
+++ app/Models/SupportFeedbackItem.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @mixin IdeHelperSupportFeedbackItem
+ */
 class SupportFeedbackItem extends Model
 {
     use HasFactory;
--- app/Models/SupportTrainingItem.php
+++ app/Models/SupportTrainingItem.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @mixin IdeHelperSupportTrainingItem
+ */
 class SupportTrainingItem extends Model
 {
     use HasFactory;
