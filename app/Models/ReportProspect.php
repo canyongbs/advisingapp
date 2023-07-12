e9f41a6bc7418dfe76eac5dc92cdcb1f784cd392
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-07-12 15:33:40 -0400

    Fixes more model resolution
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- app/Models/ReportProspect.php
+++ app/Models/ReportProspect.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @mixin IdeHelperReportProspect
+ */
 class ReportProspect extends Model
 {
     use HasFactory;
--- app/Models/ReportStudent.php
+++ app/Models/ReportStudent.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @mixin IdeHelperReportStudent
+ */
 class ReportStudent extends Model
 {
     use HasFactory;
