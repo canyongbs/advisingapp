f9151f43549cc2cefb131dea610e3bdffc84537b
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-07-12 15:10:17 -0400

    Resolve the last code analysis errors
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

---
+++ app/Models/ReportProspect.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ReportProspect extends Model
+{
+    use HasFactory;
+
+    // TODO: This was not originally present in the application, added it for now to not throw code analysis errors. We will need to figure out whether this is needed or not.
+}
