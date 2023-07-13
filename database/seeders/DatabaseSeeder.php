00d3dfd0e5ba81e8fd9ff7443dfe1e5b5e33ca97
Derek Goetz <derek@kirschbaumdevelopment.com>
2023-07-13 11:46:41 -0400

    Remove existing conventions for roles/permissions.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,11 +9,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            PermissionsTableSeeder::class,
-            RolesTableSeeder::class,
-            PermissionRoleTableSeeder::class,
             UsersTableSeeder::class,
-            RoleUserTableSeeder::class,
         ]);
     }
 }
