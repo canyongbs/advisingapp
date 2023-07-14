e8295c025ff3d7a8cccc48d5006cbfe6cab35b5e
Derek Goetz <derek@kirschbaumdevelopment.com>
2023-07-14 13:51:06 -0400

    Add to sync roles and permissions test.

--- tests/Feature/Console/Commands/SyncRolesAndPermissionsTest.php
+++ tests/Feature/Console/Commands/SyncRolesAndPermissionsTest.php
@@ -26,13 +26,29 @@ class SyncRolesAndPermissionsTest extends TestCase
             ],
         ]);
 
-        // When we run the command
+        Config::set('permissions.api.custom', [
+            'export_reports',
+        ]);
+
+        Config::set('roles.api.admin', [
+            'custom' => ['export_reports'],
+            'model' => [],
+        ]);
+
+        // When we run the SyncRolesAndPermissions command
         Artisan::call(SyncRolesAndPermissions::class);
 
-        // Our roles we have defined should have the corresponding permissions defined in configuration
-        $role = Role::web()->firstWhere('name', 'admin');
+        // Our roles we have defined should have attached the corresponding permissions defined in configuration
+        $webAdmin = Role::web()->firstWhere('name', 'admin');
 
-        $this->assertTrue($role->hasPermissionTo('user.*.view'));
-        $this->assertFalse($role->hasPermissionTo('user.*.update'));
+        $this->assertTrue($webAdmin->hasPermissionTo('user.*.view'));
+        $this->assertFalse($webAdmin->hasPermissionTo('user.*.update'));
+        $this->assertFalse($webAdmin->hasPermissionTo('export_reports'));
+
+        // Our roles we have defined should have attached the corresponding permissions defined in configuration
+        $apiAdmin = Role::api()->firstWhere('name', 'admin');
+
+        $this->assertTrue($apiAdmin->hasPermissionTo('export_reports'));
+        $this->assertFalse($apiAdmin->hasPermissionTo('user.*.view'));
     }
 }
