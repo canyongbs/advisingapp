fbef5720efa4271f0e15d46bb4d6c413ca637cae
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-07-14 17:32:03 -0400

    Prefixing filament admin for now
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -25,7 +25,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('admin')
+            ->path('v2/admin')
             ->login()
             ->colors([
                 'primary' => Color::Amber,
