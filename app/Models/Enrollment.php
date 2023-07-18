5f75a49eaf3e5481f21e68344e19fff064e83c61
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-07-17 22:12:22 -0400

    IDE helper generation
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- app/Models/Enrollment.php
+++ app/Models/Enrollment.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+/**
+ * @mixin IdeHelperEnrollment
+ */
 class Enrollment extends Model
 {
     use HasFactory;
