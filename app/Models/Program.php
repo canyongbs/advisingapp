41da68f52529a999a56d6c9238445dfd97e979dd
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-07-17 22:12:22 -0400

    IDE helper generation
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- app/Models/Program.php
+++ app/Models/Program.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+/**
+ * @mixin IdeHelperProgram
+ */
 class Program extends Model
 {
     use HasFactory;
--- app/Models/Student.php
+++ app/Models/Student.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+/**
+ * @mixin IdeHelperStudent
+ */
 class Student extends Model
 {
     use HasFactory;
