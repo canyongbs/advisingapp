9297f1cdf32e957ed5ffae975126b8ff8f4f2d20
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-07-21 16:53:04 -0500

    Setup a Students Seeder
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             UsersTableSeeder::class,
+            StudentSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/StudentSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Student;
+use Illuminate\Database\Seeder;
+
+class StudentSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Student::factory()
+            ->count(10)
+            ->create();
+    }
+}
