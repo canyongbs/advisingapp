5e4698506f7f73e408ae2eafe9da0db21bd69871
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-07-23 07:45:14 -0400

    Change test case setup
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,12 +2,16 @@
 
 namespace Tests;
 
+use Illuminate\Contracts\Console\Kernel;
 use Spatie\Permission\PermissionRegistrar;
+use Illuminate\Foundation\Testing\RefreshDatabaseState;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\Traits\CanConfigureMigrationCommands;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use CanConfigureMigrationCommands;
 
     protected function setUp(): void
     {
@@ -15,9 +19,28 @@ abstract class TestCase extends BaseTestCase
 
         app()[PermissionRegistrar::class]->forgetCachedPermissions();
 
-        $this->artisan('migrate:fresh');
-        $this->artisan('migrate:fresh', ['--database' => 'sis', '--path' => 'database/migrations/sis']);
+        $this->artisan('migrate:fresh', $this->migrateFreshUsing());
+
+        $this->artisan('migrate:fresh', [
+            '--database' => 'sis',
+            '--path' => 'database/migrations/sis',
+            ...$this->migrateFreshUsing(),
+        ]);
 
         $this->artisan('app:setup-foreign-data-wrapper');
+
+        $this->app[Kernel::class]->setArtisan(null);
+
+        $this->beforeApplicationDestroyed(function () {
+            $this->artisan('migrate:rollback');
+            $this->artisan('migrate:rollback', [
+                '--database' => 'sis',
+                '--path' => 'database/migrations/sis',
+            ]);
+
+            $this->artisan('app:remove-foreign-data-wrapper');
+
+            RefreshDatabaseState::$migrated = false;
+        });
     }
 }
