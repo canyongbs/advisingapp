2b19b71b860c309fde9805ce232193bedb06af0b
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-07-23 12:41:58 -0400

    Replace bcrypt call with Hash
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -15,7 +15,7 @@ class UsersTableSeeder extends Seeder
         $superAdmin = User::factory()->create([
             'name' => 'Super Admin',
             'email' => 'superadmin@assist.com',
-            'password' => bcrypt('password'),
+            'password' => Hash::make('password'),
         ]);
 
         $superAdminRoles = Role::superAdmin()->get();
