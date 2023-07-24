5c34e1e902a2f48b5be43c6a29d25ace2b80e045
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-07-24 13:45:46 -0400

    Adjustments to keep access to old tables
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- app/Http/Livewire/CaseItem/Index.php
+++ app/Http/Livewire/CaseItem/Index.php
@@ -69,7 +69,7 @@ class Index extends Component
 
     public function render()
     {
-        $query = CaseItem::with(['respondent', 'institution', 'state', 'type', 'priority', 'assignedTo', 'createdBy'])->advancedFilter([
+        $query = CaseItem::with(['institution', 'state', 'type', 'priority', 'assignedTo', 'createdBy'])->advancedFilter([
             's' => $this->search ?: null,
             'order_column' => $this->sortBy,
             'order_direction' => $this->sortDirection,
