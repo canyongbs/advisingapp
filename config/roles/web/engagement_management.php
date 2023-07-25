b195704d7c5d507ca47d85cc65b2fdd58ac948c7
Derek Goetz <derek@kirschbaumdevelopment.com>
2023-07-24 21:19:24 -0400

    Add more initial roles and role group.

---
+++ config/roles/web/engagement_management.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'model' => [
+        'engagement_email_item' => [
+            '*',
+        ],
+        'engagement_interaction_driver' => [
+            '*',
+        ],
+        'engagement_interaction_item' => [
+            '*',
+        ],
+        'engagement_interaction_outcome' => [
+            '*',
+        ],
+        'engagement_interaction_relation' => [
+            '*',
+        ],
+        'engagement_interaction_type' => [
+            '*',
+        ],
+        'engagement_student_file' => [
+            '*',
+        ],
+        'engagement_text_item' => [
+            '*',
+        ],
+    ],
+];
---
+++ config/roles/web/knowledge_base_management.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'model' => [
+        'kb_item' => [
+            '*',
+        ],
+        'kb_item_category' => [
+            '*',
+        ],
+        'kb_item_quality' => [
+            '*',
+        ],
+        'kb_item_status' => [
+            '*',
+        ],
+    ],
+];
