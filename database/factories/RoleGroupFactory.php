9b43a68a158dedfc14050c4798224b1e808a843e
Derek Goetz <derek@kirschbaumdevelopment.com>
2023-07-24 21:32:39 -0400

    Role group policy.

---
+++ database/factories/RoleGroupFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\RoleGroup>
+ */
+class RoleGroupFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+        ];
+    }
+}
