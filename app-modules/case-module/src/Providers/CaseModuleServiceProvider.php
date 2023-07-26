bfa754f84fcb44ecc44e30272d1300ec989137c4
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-07-25 21:16:27 -0400

    Add comments
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- app-modules/case-module/src/Providers/CaseModuleServiceProvider.php
+++ app-modules/case-module/src/Providers/CaseModuleServiceProvider.php
@@ -10,12 +10,14 @@ class CaseModuleServiceProvider extends ServiceProvider
 {
     public function register()
     {
+        // TODO: use once we upgrade to Filament beta 10 or higher
         Panel::configureUsing(fn (Panel $panel) => $panel->plugin(new CasePlugin()));
     }
 
-    //public function boot()
-    //{
-    //    filament()->getPanel('admin')
-    //        ->plugin(new CasePlugin());
-    //}
+    public function boot()
+    {
+        // TODO: Remove once we upgrade to Filament beta 10 or higher
+        filament()->getPanel('admin')
+            ->plugin(new CasePlugin());
+    }
 }
