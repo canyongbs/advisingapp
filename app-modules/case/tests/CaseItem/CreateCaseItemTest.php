280e9d3dabee3669afb50341bebc301cb5e93477
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-08-03 18:03:49 -0400

    Tests for CreateCaseItem
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- app-modules/case/tests/CaseItem/CreateCaseItemTest.php
+++ app-modules/case/tests/CaseItem/CreateCaseItemTest.php
@@ -3,20 +3,13 @@
 use Assist\Case\Models\CaseItem;
 
 use function Tests\asSuperAdmin;
-
-use Assist\Case\Models\CaseUpdate;
-
 use function Pest\Livewire\livewire;
-
-use Illuminate\Validation\Rules\Enum;
-
 use function PHPUnit\Framework\assertCount;
-use function PHPUnit\Framework\assertEmpty;
 use function Pest\Laravel\assertDatabaseHas;
+use function Pest\Laravel\assertDatabaseMissing;
 
 use Assist\Case\Filament\Resources\CaseItemResource;
 use Assist\Case\Tests\RequestFactories\CreateCaseItemRequestFactory;
-use Assist\Case\Tests\RequestFactories\CreateCaseUpdateRequestFactory;
 
 test('A successful action on the CreateCaseUpdate page', function () {
     asSuperAdmin()
@@ -58,23 +51,52 @@ test('A successful action on the CreateCaseUpdate page', function () {
         ->toEqual($request->get('type'));
 });
 
-test('CreateCaseUpdate requires valid data', function ($data, $errors) {
+test('CreateCaseItem requires valid data', function ($data, $errors, $setup = null) {
+    if ($setup) {
+        $setup();
+    }
+
     asSuperAdmin();
 
-    livewire(CaseUpdateResource\Pages\CreateCaseUpdate::class)
-        ->fillForm(CreateCaseUpdateRequestFactory::new($data)->create())
+    $request = collect(CreateCaseItemRequestFactory::new($data)->create());
+
+    livewire(CaseItemResource\Pages\CreateCaseItem::class)
+        ->fillForm($request->toArray())
         ->call('create')
         ->assertHasFormErrors($errors);
 
-    assertEmpty(CaseUpdate::all());
+    assertDatabaseMissing(CaseItem::class, $request->except(['institution', 'state', 'priority', 'type'])->toArray());
 })->with(
     [
-        'case missing' => [CreateCaseUpdateRequestFactory::new()->without('case'), ['case' => 'required']],
-        'case not existing case id' => [CreateCaseUpdateRequestFactory::new()->state(['case' => 99]), ['case' => 'exists']],
-        'update missing' => [CreateCaseUpdateRequestFactory::new()->without('update'), ['update' => 'required']],
-        'update is not a string' => [CreateCaseUpdateRequestFactory::new()->state(['update' => 99]), ['update' => 'string']],
-        'direction missing' => [CreateCaseUpdateRequestFactory::new()->without('direction'), ['direction' => 'required']],
-        'direction not a valid enum' => [CreateCaseUpdateRequestFactory::new()->state(['direction' => 'invalid']), ['direction' => Enum::class]],
-        'internal not a boolean' => [CreateCaseUpdateRequestFactory::new()->state(['internal' => 'invalid']), ['internal' => 'boolean']],
+        'casenumber missing' => [CreateCaseItemRequestFactory::new()->without('casenumber'), ['casenumber' => 'required']],
+        'casenumber should be unique' => [
+            CreateCaseItemRequestFactory::new()->state(['casenumber' => 99]),
+            ['casenumber' => 'unique'],
+            function () {
+                CaseItem::factory()->create(['casenumber' => 99]);
+            },
+        ],
+        'institution missing' => [CreateCaseItemRequestFactory::new()->without('institution'), ['institution' => 'required']],
+        'institution does not exist' => [
+            CreateCaseItemRequestFactory::new()->state(['institution' => 99]),
+            ['institution' => 'exists'],
+        ],
+        'state missing' => [CreateCaseItemRequestFactory::new()->without('state'), ['state' => 'required']],
+        'state does not exist' => [
+            CreateCaseItemRequestFactory::new()->state(['state' => 99]),
+            ['state' => 'exists'],
+        ],
+        'priority missing' => [CreateCaseItemRequestFactory::new()->without('priority'), ['priority' => 'required']],
+        'priority does not exist' => [
+            CreateCaseItemRequestFactory::new()->state(['priority' => 99]),
+            ['priority' => 'exists'],
+        ],
+        'type missing' => [CreateCaseItemRequestFactory::new()->without('type'), ['type' => 'required']],
+        'type does not exist' => [
+            CreateCaseItemRequestFactory::new()->state(['type' => 99]),
+            ['type' => 'exists'],
+        ],
+        'close_details is not a string' => [CreateCaseItemRequestFactory::new()->state(['close_details' => 1]), ['close_details' => 'string']],
+        'res_details is not a string' => [CreateCaseItemRequestFactory::new()->state(['res_details' => 1]), ['res_details' => 'string']],
     ]
 );
