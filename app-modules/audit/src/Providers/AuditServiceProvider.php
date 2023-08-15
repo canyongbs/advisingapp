c5fd54b7fc3583ed65d0d7a67ac743d570471e30
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-08-15 14:07:20 -0400

    Modify audit configuration
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- app-modules/audit/src/Providers/AuditServiceProvider.php
+++ app-modules/audit/src/Providers/AuditServiceProvider.php
@@ -3,14 +3,39 @@
 namespace Assist\Audit\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Assist\Authorization\AuthorizationRoleRegistry;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Assist\Authorization\AuthorizationPermissionRegistry;
 
 class AuditServiceProvider extends ServiceProvider
 {
     public function register()
     {
+        app('config')->set('audit', require base_path('app-modules/audit/config/audit.php'));
     }
 
-    public function boot()
+    public function boot(AuthorizationPermissionRegistry $permissionRegistry, AuthorizationRoleRegistry $roleRegistry): void
     {
+        Relation::morphMap([]);
+
+        $permissionRegistry->registerApiPermissions(
+            module: 'audit',
+            path: 'permissions/api/custom'
+        );
+
+        $permissionRegistry->registerWebPermissions(
+            module: 'audit',
+            path: 'permissions/web/custom'
+        );
+
+        $roleRegistry->registerApiRoles(
+            module: 'audit',
+            path: 'roles/api'
+        );
+
+        $roleRegistry->registerWebRoles(
+            module: 'audit',
+            path: 'roles/web'
+        );
     }
 }
