c13e6ca6e100a5ead85218e57103d1df99cb386c
Derek Goetz <derek@kirschbaumdevelopment.com>
2023-08-15 15:31:18 -0400

    Notifications module.

---
+++ app-modules/notifications/src/Providers/NotificationsServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Assist\Notifications\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class NotificationsServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+    }
+
+    public function boot()
+    {
+    }
+}
