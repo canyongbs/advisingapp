4a051794f77328bdce81b36f8af97c588f2bd187
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-08-15 16:38:12 -0400

    Custom AuditAttachAction
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

---
+++ app-modules/audit/src/Filament/Actions/AuditAttachAction.php
@@ -0,0 +1,20 @@
+<?php
+
+use OwenIt\Auditing\Contracts\Auditable;
+use Filament\Tables\Actions\AttachAction;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class AuditAttachAction extends AttachAction
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->using(function ($data, $record, BelongsToMany $relationship) {
+            /** @var Auditable $parent */
+            $parent = $relationship->getParent();
+
+            $parent->auditAttach($relationship->getRelationName(), $record);
+        });
+    }
+}
