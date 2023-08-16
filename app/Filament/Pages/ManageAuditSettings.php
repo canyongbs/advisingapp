a4d350f3d80882cbe234e9f39beee9ea57225d91
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-08-15 23:29:40 -0400

    Install and start to set up Audit settings config
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

---
+++ app/Filament/Pages/ManageAuditSettings.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Forms\Form;
+use Filament\Pages\SettingsPage;
+use Assist\Audit\Settings\AuditSettings;
+
+class ManageAuditSettings extends SettingsPage
+{
+    protected static ?string $navigationIcon = 'heroicon-o-cog-6-tooth';
+
+    protected static string $settings = AuditSettings::class;
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                // ...
+            ]);
+    }
+}
