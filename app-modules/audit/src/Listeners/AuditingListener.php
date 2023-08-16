9e69a1f1c122572709c875ccf13c5aee7dec3b61
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-08-16 11:56:45 -0400

    Set up filtering of Audit events
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- app-modules/audit/src/Listeners/AuditingListener.php
+++ app-modules/audit/src/Listeners/AuditingListener.php
@@ -3,16 +3,17 @@
 namespace Assist\Audit\Listeners;
 
 use OwenIt\Auditing\Events\Auditing;
+use Illuminate\Database\Eloquent\Model;
+use Assist\Audit\Settings\AuditSettings;
 
 class AuditingListener
 {
-    public function __construct()
-    {
-    }
-
     public function handle(Auditing $event): bool
     {
-        //ray($event);
-        return true;
+        /** @var Model $model */
+        $model = $event->model;
+
+        return collect(resolve(AuditSettings::class)->audited_models)
+            ->contains($model->getMorphClass());
     }
 }
