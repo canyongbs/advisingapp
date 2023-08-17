6b5ad10eb80e90622b440fc828ecfdec57117399
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-08-17 14:31:12 -0400

    Setup retention config and fix rules
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- app-modules/audit/database/migrations/2023_08_16_025823_create_audit_settings.php
+++ app-modules/audit/database/migrations/2023_08_16_025823_create_audit_settings.php
@@ -6,7 +6,7 @@ use Spatie\LaravelSettings\Migrations\SettingsMigration;
 return new class () extends SettingsMigration {
     public function up(): void
     {
-        $this->migrator->add('audit.retention_duration', 129600);
+        $this->migrator->add('audit.retention_duration', 90);
         $this->migrator->add('audit.audited_models', AuditableModels::all()->keys());
     }
 };
