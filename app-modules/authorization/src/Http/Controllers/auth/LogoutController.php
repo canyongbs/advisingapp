99bc3493774f00fc5e9264cda14ce987f1c7973f
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-08-21 22:22:30 -0400

    Reconfigure logout logic
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- app-modules/authorization/src/Http/Controllers/auth/LogoutController.php
+++ app-modules/authorization/src/Http/Controllers/auth/LogoutController.php
@@ -2,6 +2,8 @@
 
 namespace Assist\Authorization\Http\Controllers\auth;
 
+use Filament\Facades\Filament;
+use Assist\Authorization\Enums\SocialiteProvider;
 use Filament\Http\Responses\Auth\Contracts\LogoutResponse;
 use Assist\Authorization\Http\Responses\Auth\SocialiteLogoutResponse;
 use Filament\Http\Controllers\Auth\LogoutController as FilamentLogoutController;
@@ -10,16 +12,19 @@ class LogoutController extends FilamentLogoutController
 {
     public function __invoke(): LogoutResponse
     {
-        if (session()->missing('auth_via')) {
-            return parent::__invoke();
-        }
-
+        /** @var SocialiteProvider|null $socialiteProvider */
         $socialiteProvider = session('auth_via');
 
-        parent::__invoke();
+        Filament::auth()->logout();
 
-        return app(SocialiteLogoutResponse::class, [
-            'provider' => $socialiteProvider,
-        ]);
+        session()->invalidate();
+        session()->regenerateToken();
+
+        return match ($socialiteProvider?->value) {
+            'azure' => app(SocialiteLogoutResponse::class, [
+                'provider' => $socialiteProvider,
+            ]),
+            default => app(LogoutResponse::class),
+        };
     }
 }
