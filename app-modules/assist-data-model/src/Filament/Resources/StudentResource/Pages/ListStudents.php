399e1945adf6ba20fb937da4b4c4f8248fcde86a
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-08-24 11:27:04 -0400

    Add filtering by subscribed to Student List
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- app-modules/assist-data-model/src/Filament/Resources/StudentResource/Pages/ListStudents.php
+++ app-modules/assist-data-model/src/Filament/Resources/StudentResource/Pages/ListStudents.php
@@ -3,12 +3,48 @@
 namespace Assist\AssistDataModel\Filament\Resources\StudentResource\Pages;
 
 use Filament\Actions;
+use Filament\Tables\Table;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Tables\Actions\CreateAction;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Tables\Actions\BulkActionGroup;
+use Filament\Tables\Actions\DeleteBulkAction;
 use Assist\AssistDataModel\Filament\Resources\StudentResource;
 
 class ListStudents extends ListRecords
 {
     protected static string $resource = StudentResource::class;
+
+    public function table(Table $table): Table
+    {
+        return parent::table($table)
+            ->columns([
+                TextColumn::make('full')
+                    ->label('Name')
+                    ->searchable()
+                    ->sortable(),
+            ])
+            ->filters([
+                Filter::make('subscribed')
+                    ->query(fn (Builder $query): Builder => $query->whereRelation('subscriptions.user', 'id', auth()->id())),
+            ])
+            ->actions([
+                ViewAction::make(),
+                EditAction::make(),
+            ])
+            ->bulkActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                CreateAction::make(),
+            ]);
+    }
 
     protected function getHeaderActions(): array
     {
