e97b3dddac02e873555c5d23a827f03ad04a0622
Derek Goetz <derek@kirschbaumdevelopment.com>
2023-08-25 12:36:33 -0400

    Webhook module, module path helper

--- app/Filament/Resources/StudentResource/RelationManagers/EngagementsRelationManager.php
+++ app/Filament/Resources/StudentResource/RelationManagers/EngagementsRelationManager.php
@@ -2,14 +2,16 @@
 
 namespace App\Filament\Resources\StudentResource\RelationManagers;
 
-use Filament\Tables;
-use Filament\Forms\Form;
 use Filament\Tables\Table;
-use Filament\Forms\Components\Repeater;
+use Filament\Infolists\Infolist;
 use Filament\Tables\Actions\ViewAction;
 use Filament\Tables\Columns\TextColumn;
 use Assist\Engagement\Models\Engagement;
-use Filament\Forms\Components\TextInput;
+use Filament\Tables\Actions\CreateAction;
+use Filament\Infolists\Components\Fieldset;
+use Filament\Infolists\Components\IconEntry;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Components\RepeatableEntry;
 use Assist\Engagement\Enums\EngagementDeliveryStatus;
 use Filament\Resources\RelationManagers\RelationManager;
 
@@ -17,32 +19,36 @@ class EngagementsRelationManager extends RelationManager
 {
     protected static string $relationship = 'engagements';
 
-    public function form(Form $form): Form
+    public function infolist(Infolist $infolist): Infolist
     {
-        return $form
+        return $infolist
             ->schema([
-                TextInput::make('subject'),
-                TextInput::make('description'),
-                Repeater::make('deliverables')
-                    ->relationship()
+                TextEntry::make('user.name')
+                    ->label('Created By'),
+
+                Fieldset::make('Content')
+                    ->schema([
+                        TextEntry::make('subject'),
+                        TextEntry::make('description'),
+                    ]),
+
+                RepeatableEntry::make('deliverables')
                     ->columnSpanFull()
                     ->schema([
-                        TextInput::make('channel')->required(),
-                        TextInput::make('delivery_status'),
-                        TextInput::make('delivered_at'),
-                        TextInput::make('delvery_response'),
-                        // TODO Need to make a custom view field in order to render what we'd like here...
-                        // TextInput::make('delivery_status')
-                        //     ->state(fn ($state): string => match ($state) {
-                        //         EngagementDeliveryStatus::SUCCESSFUL => 'Successful',
-                        //         EngagementDeliveryStatus::AWAITING => 'Awaiting Delivery',
-                        //         EngagementDeliveryStatus::FAILED => 'Failed',
-                        //     }),
-                        // ->color(fn (EngagementDeliveryStatus $state): string => match ($state) {
-                        //     EngagementDeliveryStatus::SUCCESSFUL => 'success',
-                        //     EngagementDeliveryStatus::AWAITING => 'info',
-                        //     EngagementDeliveryStatus::FAILED => 'danger',
-                        // }),
+                        TextEntry::make('channel'),
+                        IconEntry::make('delivery_status')
+                            ->icon(fn (EngagementDeliveryStatus $state): string => match ($state) {
+                                EngagementDeliveryStatus::SUCCESSFUL => 'heroicon-o-check-circle',
+                                EngagementDeliveryStatus::AWAITING => 'heroicon-o-clock',
+                                EngagementDeliveryStatus::FAILED => 'heroicon-o-x-circle',
+                            })
+                            ->color(fn (EngagementDeliveryStatus $state): string => match ($state) {
+                                EngagementDeliveryStatus::SUCCESSFUL => 'success',
+                                EngagementDeliveryStatus::AWAITING => 'info',
+                                EngagementDeliveryStatus::FAILED => 'danger',
+                            }),
+                        TextEntry::make('delivered_at'),
+                        TextEntry::make('delivery_response'),
                     ])
                     ->columns(2),
             ]);
@@ -67,20 +73,17 @@ class EngagementsRelationManager extends RelationManager
             ->filters([
             ])
             ->headerActions([
-                Tables\Actions\CreateAction::make(),
+                // TODO Enable Engagement creation from the StudentResource
+                // CreateAction::make(),
             ])
             ->actions([
                 ViewAction::make(),
-                // Tables\Actions\EditAction::make(),
-                // Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
-                // Tables\Actions\BulkActionGroup::make([
-                //     Tables\Actions\DeleteBulkAction::make(),
-                // ]),
             ])
             ->emptyStateActions([
-                Tables\Actions\CreateAction::make(),
+                // TODO Enable Engagement creation from the StudentResource
+                // CreateAction::make(),
             ]);
     }
 }
