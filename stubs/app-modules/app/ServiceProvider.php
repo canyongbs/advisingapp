b499d57b788083a5ce46913f448f436fb031f6ec
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-08-28 11:40:45 -0400

    Setup ServiceProvider stub to setup custom auth registry
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- stubs/app-modules/app/ServiceProvider.php
+++ stubs/app-modules/app/ServiceProvider.php
@@ -4,7 +4,9 @@ namespace StubModuleNamespace\StubClassNamePrefix\Providers;
 
 use Filament\Panel;
 use Illuminate\Support\ServiceProvider;
+use Assist\Authorization\AuthorizationRoleRegistry;
 use Illuminate\Database\Eloquent\Relations\Relation;
+use Assist\Authorization\AuthorizationPermissionRegistry;
 use StubModuleNamespace\StubClassNamePrefix\StubClassNamePrefixPlugin;
 
 class StubClassNamePrefixServiceProvider extends ServiceProvider
@@ -17,5 +19,34 @@ class StubClassNamePrefixServiceProvider extends ServiceProvider
     public function boot()
     {
         Relation::morphMap([]);
+
+        $this->registerRolesAndPermissions();
+    }
+
+    protected function registerRolesAndPermissions()
+    {
+        $permissionRegistry = app(AuthorizationPermissionRegistry::class);
+
+        $permissionRegistry->registerApiPermissions(
+            module: 'StubModuleName',
+            path: 'permissions/api/custom'
+        );
+
+        $permissionRegistry->registerWebPermissions(
+            module: 'StubModuleName',
+            path: 'permissions/web/custom'
+        );
+
+        $roleRegistry = app(AuthorizationRoleRegistry::class);
+
+        $roleRegistry->registerApiRoles(
+            module: 'StubModuleName',
+            path: 'roles/api'
+        );
+
+        $roleRegistry->registerWebRoles(
+            module: 'StubModuleName',
+            path: 'roles/web'
+        );
     }
 }
