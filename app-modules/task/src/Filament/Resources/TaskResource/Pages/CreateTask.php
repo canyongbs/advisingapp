47432ec1a390aeca74888e6885f1cbde9c3c40f2
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-08-29 14:09:04 -0400

    Create, View, and Mark completed
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- app-modules/task/src/Filament/Resources/TaskResource/Pages/CreateTask.php
+++ app-modules/task/src/Filament/Resources/TaskResource/Pages/CreateTask.php
@@ -2,10 +2,48 @@
 
 namespace Assist\Task\Filament\Resources\TaskResource\Pages;
 
+use Filament\Forms\Form;
+use Assist\Prospect\Models\Prospect;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Assist\AssistDataModel\Models\Student;
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Forms\Components\MorphToSelect;
+use Filament\Forms\Components\DateTimePicker;
 use Assist\Task\Filament\Resources\TaskResource;
+use Filament\Forms\Components\MorphToSelect\Type;
 
 class CreateTask extends CreateRecord
 {
     protected static string $resource = TaskResource::class;
+
+    public function form(Form $form): Form
+    {
+        return parent::form($form)
+            ->schema([
+                TextInput::make('description')
+                    ->label('Description')
+                    ->required()
+                    ->string(),
+                DateTimePicker::make('due')
+                    ->label('Due Date')
+                    ->native(false),
+                Select::make('assigned_to')
+                    ->label('Assigned To')
+                    ->relationship('assignedTo', 'name')
+                    ->nullable()
+                    ->searchable()
+                    ->default(auth()->id()),
+                MorphToSelect::make('concern')
+                    ->label('Concern')
+                    ->searchable()
+                    ->preload()
+                    ->types([
+                        Type::make(Student::class)
+                            ->titleAttribute('full'),
+                        Type::make(Prospect::class)
+                            ->titleAttribute('full'),
+                    ]),
+            ]);
+    }
 }
--- app-modules/task/src/Filament/Resources/TaskResource/Pages/ListTasks.php
+++ app-modules/task/src/Filament/Resources/TaskResource/Pages/ListTasks.php
@@ -6,16 +6,21 @@ use Filament\Actions;
 use Filament\Tables\Table;
 use Assist\Task\Models\Task;
 use Assist\Task\Enums\TaskStatus;
+use Filament\Tables\Actions\Action;
 use Filament\Tables\Filters\Filter;
 use Assist\Prospect\Models\Prospect;
+use Filament\Infolists\Components\Grid;
 use Filament\Tables\Actions\EditAction;
 use Filament\Tables\Actions\ViewAction;
 use Filament\Tables\Columns\TextColumn;
 use App\Filament\Resources\UserResource;
+use Filament\Infolists\Components\Split;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Tables\Actions\CreateAction;
 use Filament\Tables\Filters\SelectFilter;
 use Assist\AssistDataModel\Models\Student;
+use Filament\Infolists\Components\Fieldset;
+use Filament\Infolists\Components\TextEntry;
 use Filament\Tables\Actions\BulkActionGroup;
 use Filament\Tables\Actions\DeleteBulkAction;
 use Assist\Task\Filament\Resources\TaskResource;
@@ -76,9 +81,58 @@ class ListTasks extends ListRecords
                     ),
             ])
             ->actions([
-                ViewAction::make(),
+                ViewAction::make()
+                    //->action(fn (Task $record) => $record->update(['status' => TaskStatus::COMPLETED]))
+                    ->extraModalFooterActions(
+                        [
+                            Action::make('mark_as_completed')
+                                ->label('Mark as Completed')
+                                ->action(fn (Task $record) => $record->update(['status' => TaskStatus::COMPLETED]))
+                                ->cancelParentActions()
+                                ->hidden(fn (Task $record) => $record->status->value === TaskStatus::COMPLETED->value),
+                        ]
+                    )
+                    ->infolist(
+                        [
+                            Split::make(
+                                [
+                                    Grid::make()
+                                        ->schema(
+                                            [
+                                                TextEntry::make('description')
+                                                    ->columnSpanFull(),
+                                                TextEntry::make('assignedTo.name')
+                                                    ->label('Assigned To')
+                                                    ->url(fn (Task $record) => $record->assignedTo ? UserResource::getUrl('view', ['record' => $record->assignedTo]) : null),
+                                                TextEntry::make('concern.full')
+                                                    ->label('Concern')
+                                                    ->url(fn (Task $record) => match ($record->concern ? $record->concern::class : null) {
+                                                        Student::class => StudentResource::getUrl('view', ['record' => $record->concern]),
+                                                        Prospect::class => ProspectResource::getUrl('view', ['record' => $record->concern]),
+                                                        default => null,
+                                                    }),
+                                            ]
+                                        ),
+                                    Fieldset::make('metadata')
+                                        ->label('Metadata')
+                                        ->schema(
+                                            [
+                                                TextEntry::make('status')
+                                                    ->formatStateUsing(fn (TaskStatus $state): string => str($state->value)->title()->headline())
+                                                    ->badge()
+                                                    ->color(fn (Task $record) => $record->status->getTableColor()),
+                                                TextEntry::make('due')
+                                                    ->label('Due Date'),
+                                            ]
+                                        ),
+                                ]
+                            )->from('md'),
+                        ]
+                    ),
+                //->modalSubmitAction(fn (StaticAction $action) => $action->label('Mark as Completed')),
                 EditAction::make(),
             ])
+            ->recordUrl(null)
             ->bulkActions([
                 BulkActionGroup::make([
                     DeleteBulkAction::make(),
