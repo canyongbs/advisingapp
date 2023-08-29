31048ec849398d58f2337e95803a64115ab53d73
Derek Goetz <derek@kirschbaumdevelopment.com>
2023-08-29 13:59:31 -0400

    Improvements to inbound webhook visibility.

--- app-modules/webhook/src/Filament/Resources/InboundWebhookResource.php
+++ app-modules/webhook/src/Filament/Resources/InboundWebhookResource.php
@@ -2,14 +2,10 @@
 
 namespace Assist\Webhook\Filament\Resources;
 
-use Filament\Tables;
-use Filament\Forms\Form;
-use Filament\Tables\Table;
 use Filament\Resources\Resource;
 use Assist\Webhook\Models\InboundWebhook;
-use Assist\Webhook\Filament\Resources\InboundWebhookResource\Pages\EditInboundWebhook;
+use Assist\Webhook\Filament\Resources\InboundWebhookResource\Pages\ViewInboundWebhook;
 use Assist\Webhook\Filament\Resources\InboundWebhookResource\Pages\ListInboundWebhooks;
-use Assist\Webhook\Filament\Resources\InboundWebhookResource\Pages\CreateInboundWebhook;
 
 class InboundWebhookResource extends Resource
 {
@@ -17,45 +13,11 @@ class InboundWebhookResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-signal';
 
-    public static function form(Form $form): Form
-    {
-        return $form
-            ->schema([
-            ]);
-    }
-
-    public static function table(Table $table): Table
-    {
-        return $table
-            ->columns([
-            ])
-            ->filters([
-            ])
-            ->actions([
-                Tables\Actions\EditAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
-            ])
-            ->emptyStateActions([
-                Tables\Actions\CreateAction::make(),
-            ]);
-    }
-
-    public static function getRelations(): array
-    {
-        return [
-        ];
-    }
-
     public static function getPages(): array
     {
         return [
             'index' => ListInboundWebhooks::route('/'),
-            'create' => CreateInboundWebhook::route('/create'),
-            'edit' => EditInboundWebhook::route('/{record}/edit'),
+            'view' => ViewInboundWebhook::route('/{record}'),
         ];
     }
 }
