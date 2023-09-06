c5913a95bcb35e458483b8d1802f7419e344495d
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-09-06 18:13:48 -0400

    Default to s3 disk
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- config/media-library.php
+++ config/media-library.php
@@ -5,7 +5,7 @@ return [
      * The disk on which to store added files and derived images by default. Choose
      * one or more of the disks you've configured in config/filesystems.php.
      */
-    'disk_name' => env('MEDIA_DISK', 'public'),
+    'disk_name' => env('MEDIA_DISK', 's3'),
 
     /*
      * The maximum file size of an item in bytes.
