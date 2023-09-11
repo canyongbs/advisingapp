d0fc7b36733847bbccf7cabaa3a8d1ca8d2401bb
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-09-11 17:21:08 -0400

    Start a little bit of the chat boxes
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- app-modules/assistant/resources/views/filament/pages/a-i-assistant.blade.php
+++ app-modules/assistant/resources/views/filament/pages/a-i-assistant.blade.php
@@ -1,11 +1,11 @@
 <x-filament-panels::page>
-    <div class="mx-auto w-full px-4 sm:px-6 lg:px-8 bg-red-50">
-        <h1>You</h1>
+    <div class="mx-auto w-full p-4 sm:p-6 lg:px-8 bg-primary-500">
+        <h1 class="text-2xl">You</h1>
         <p>This is a question</p>
     </div>
 
-    <div class="mx-auto w-full px-4 sm:px-6 lg:px-8 bg-gray">
-        <h1>You</h1>
-        <p>This is a question</p>
+    <div class="mx-auto w-full p-4 sm:p-6 lg:px-8 bg-gray-500">
+        <h1 class="text-2xl">AI Assistant</h1>
+        <p>This is the answer</p>
     </div>
 </x-filament-panels::page>
