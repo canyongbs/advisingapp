4213b729207139cfcaf00ff0a4f6ed155bbc9d83
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-09-11 17:05:58 -0400

    Remove duplicate tailwind configs
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,9 @@ export default {
     content: [
         './app/Filament/**/*.php',
         './app-modules/**/src/Filament/**/*.php',
+        './app-modules/**/resources/views/**/*.php',
         './resources/views/**/*.blade.php',
+        './resources/views/filament/**/*.blade.php',
         './vendor/filament/**/*.blade.php',
         './vendor/awcodes/filament-tiptap-editor/resources/**/*.blade.php',
     ],
