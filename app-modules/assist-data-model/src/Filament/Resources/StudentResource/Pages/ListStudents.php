1c96993a184083e20f9884b391178b0c99fdde0b
Matthew Myers <mxm1070@users.noreply.github.com>
2023-09-14 11:54:00 -0400

    lint

--- app-modules/assist-data-model/src/Filament/Resources/StudentResource/Pages/ListStudents.php
+++ app-modules/assist-data-model/src/Filament/Resources/StudentResource/Pages/ListStudents.php
@@ -2,26 +2,20 @@
 
 namespace Assist\AssistDataModel\Filament\Resources\StudentResource\Pages;
 
-use Assist\Notifications\Filament\Actions\SubscribeBulkAction;
-use Assist\Notifications\Filament\Actions\SubscribeTableAction;
-use Assist\Notifications\Models\Contracts\Subscribable;
 use Filament\Tables\Table;
 use Filament\Actions\CreateAction;
-use Filament\Tables\Actions\Action;
 use Filament\Tables\Filters\Filter;
-use Filament\Tables\Actions\BulkAction;
 use Filament\Tables\Actions\ViewAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Resources\Pages\ListRecords;
 use Illuminate\Database\Eloquent\Builder;
-use Assist\AssistDataModel\Models\Student;
 use Filament\Tables\Actions\BulkActionGroup;
-use Illuminate\Database\Eloquent\Collection;
 use Filament\Tables\Actions\DeleteBulkAction;
-use Assist\Notifications\Actions\SubscriptionToggle;
 use Assist\Engagement\Filament\Actions\BulkEngagementAction;
 use Assist\AssistDataModel\Filament\Resources\StudentResource;
+use Assist\Notifications\Filament\Actions\SubscribeBulkAction;
 use Filament\Tables\Actions\CreateAction as TableCreateAction;
+use Assist\Notifications\Filament\Actions\SubscribeTableAction;
 
 class ListStudents extends ListRecords
 {
