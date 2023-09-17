4eeda4b197679e70729dc66b69737926bd68ca60
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-09-17 13:36:47 -0400

    Fix AssistantChatMessageLog
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -312,6 +312,36 @@ namespace Assist\Assistant\Models{
  class IdeHelperAssistantChatMessage {}
 }
 
+namespace Assist\Assistant\Models{
+/**
+ * Assist\Assistant\Models\AssistantChatMessageLog
+ *
+ * @property string $id
+ * @property string $message
+ * @property array $metadata
+ * @property string $user_id
+ * @property array $request
+ * @property int $sent_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read User $user
+ * @method static Builder|AssistantChatMessageLog newModelQuery()
+ * @method static Builder|AssistantChatMessageLog newQuery()
+ * @method static Builder|AssistantChatMessageLog query()
+ * @method static Builder|AssistantChatMessageLog whereCreatedAt($value)
+ * @method static Builder|AssistantChatMessageLog whereId($value)
+ * @method static Builder|AssistantChatMessageLog whereMessage($value)
+ * @method static Builder|AssistantChatMessageLog whereMetadata($value)
+ * @method static Builder|AssistantChatMessageLog whereRequest($value)
+ * @method static Builder|AssistantChatMessageLog whereSentAt($value)
+ * @method static Builder|AssistantChatMessageLog whereUpdatedAt($value)
+ * @method static Builder|AssistantChatMessageLog whereUserId($value)
+ * @mixin \Eloquent
+ */
+	#[\AllowDynamicProperties]
+ class IdeHelperAssistantChatMessageLog {}
+}
+
 namespace Assist\Audit\Models{
 /**
  * Assist\Audit\Models\Audit
