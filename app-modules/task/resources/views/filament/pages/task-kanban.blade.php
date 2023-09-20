900ef76ab536f2681c330446f3e890a05a135154
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-09-20 17:16:59 -0400

    Cleanup
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- app-modules/task/resources/views/filament/pages/task-kanban.blade.php
+++ app-modules/task/resources/views/filament/pages/task-kanban.blade.php
@@ -32,32 +32,12 @@
                                                 </button>
                                             </div>
 
-                                            {{--                                    {{ if .attachment }}--}}
-                                            {{--                                    <div class="flex items-center justify-center pb-4">--}}
-                                            {{--                                        <img class="bg-contain rounded-lg" src="{{ .attachment }}" alt="attachment" />--}}
-                                            {{--                                    </div>--}}
-                                            {{--                                    {{ end }}--}}
-
                                             <div class="flex flex-col">
                                                 <div class="pb-4 text-sm font-normal text-gray-700 dark:text-gray-400">This
                                                     is the task description
                                                 </div>
 
-                                                <div class="flex justify-between">
-{{--                                                    <div class="flex items-center justify-start">--}}
-{{--                                                        <!-- foreach of "users" -->--}}
-{{--                                                        <a href="#" data-tooltip-target="user_taskid_user_id" class="-mr-3">--}}
-{{--                                                            <img class="border-2 border-white rounded-full h-7 w-7 dark:border-gray-800"--}}
-{{--                                                                 src="" alt=""/>--}}
-{{--                                                        </a>--}}
-{{--                                                        <div id="user_taskid_userid" role="tooltip"--}}
-{{--                                                             class="absolute z-50 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">--}}
-{{--                                                            User Name--}}
-{{--                                                            <div class="tooltip-arrow" data-popper-arrow></div>--}}
-{{--                                                        </div>--}}
-{{--                                                        <!-- end of "users" foreach -->--}}
-{{--                                                    </div>--}}
-                                                    <!-- TODO: if task is done -->
+                                                <div class="flex justify-end">
                                                     @if($task->status === TaskStatus::COMPLETED)
                                                         <div class="flex items-center justify-center px-3 text-sm font-medium text-green-800 bg-green-100 rounded-lg dark:bg-green-200">
                                                             <svg class="w-4 h-4 mr-1" fill="currentColor" viewBox="0 0 20 20"
@@ -68,15 +48,21 @@
                                                             </svg>
                                                             Done
                                                         </div>
-                                                    @else
-                                                        <div class="flex items-center justify-center px-3 text-sm font-medium text-purple-800 bg-purple-100 rounded-lg dark:bg-purple-200">
+                                                    @elseif (!empty($task->due))
+                                                        <div
+                                                            @class([
+                                                                'flex items-center justify-center px-3 text-sm font-medium rounded-lg',
+                                                                'text-danger-800 bg-danger-100 dark:bg-danger-200' => $task->due->isPast(),
+                                                                'text-warning-800 bg-warning-100 dark:bg-warning-200' => $task->due->isFuture(),
+                                                            ])
+                                                        >
                                                             <svg class="w-4 h-4 mr-1" fill="currentColor" viewBox="0 0 20 20"
                                                                  xmlns="http://www.w3.org/2000/svg">
                                                                 <path fill-rule="evenodd"
                                                                       d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
                                                                       clip-rule="evenodd"></path>
                                                             </svg>
-                                                            3 days left
+                                                            {{ now()->diffForHumans($task->due) }} due
                                                         </div>
                                                     @endif
                                                 </div>
