d92f0c0be8377cc8a776fe38bc47fbf8cad47dab
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-09-21 16:18:18 -0400

    Move the scripts
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- app-modules/task/resources/views/filament/pages/list-tasks.blade.php
+++ app-modules/task/resources/views/filament/pages/list-tasks.blade.php
@@ -69,4 +69,49 @@
     @elseif($viewType === 'kanban')
         <livewire:task-kanban />
     @endif
+    <script>
+        document.addEventListener('alpine:init', () => {
+            Alpine.data('kanban', ($wire) => ({
+                init() {
+                    const kanbanLists = document.querySelectorAll('[id^="kanban-list-"]');
+
+                    kanbanLists.forEach(kanbanList => {
+                        window.Sortable.create(kanbanList, {
+                            group: 'kanban',
+                            animation: 100,
+                            forceFallback: true,
+                            dragClass: 'drag-card',
+                            ghostClass: 'ghost-card',
+                            easing: 'cubic-bezier(0, 0.55, 0.45, 1)',
+                            onAdd: async function (evt) {
+                                try {
+                                    const result = await $wire.movedTask(evt.item.dataset.task, evt.from.dataset.status, evt.to.dataset.status);
+
+                                    if (result.original.success) {
+                                        new FilamentNotification()
+                                            .icon('heroicon-o-check-circle')
+                                            .title(result.original.message)
+                                            .iconColor('success')
+                                            .send()
+                                    } else {
+                                        new FilamentNotification()
+                                            .icon('heroicon-o-x-circle')
+                                            .title(result.original.message)
+                                            .iconColor('danger')
+                                            .send()
+                                    }
+                                } catch (e) {
+                                    new FilamentNotification()
+                                        .icon('heroicon-o-x-circle')
+                                        .title('Something went wrong, please try again later')
+                                        .iconColor('danger')
+                                        .send()
+                                }
+                            },
+                        });
+                    });
+                },
+            }))
+        })
+    </script>
 </x-filament-panels::page>
--- resources/views/livewire/task-kanban.blade.php
+++ resources/views/livewire/task-kanban.blade.php
@@ -80,49 +80,4 @@
         </div>
     </div>
     <x-filament-actions::modals />
-    <script>
-        document.addEventListener('alpine:init', () => {
-            Alpine.data('kanban', ($wire) => ({
-                init() {
-                    const kanbanLists = document.querySelectorAll('[id^="kanban-list-"]');
-
-                    kanbanLists.forEach(kanbanList => {
-                        window.Sortable.create(kanbanList, {
-                            group: 'kanban',
-                            animation: 100,
-                            forceFallback: true,
-                            dragClass: 'drag-card',
-                            ghostClass: 'ghost-card',
-                            easing: 'cubic-bezier(0, 0.55, 0.45, 1)',
-                            onAdd: async function (evt) {
-                                try {
-                                    const result = await $wire.movedTask(evt.item.dataset.task, evt.from.dataset.status, evt.to.dataset.status);
-
-                                    if (result.original.success) {
-                                        new FilamentNotification()
-                                            .icon('heroicon-o-check-circle')
-                                            .title(result.original.message)
-                                            .iconColor('success')
-                                            .send()
-                                    } else {
-                                        new FilamentNotification()
-                                            .icon('heroicon-o-x-circle')
-                                            .title(result.original.message)
-                                            .iconColor('danger')
-                                            .send()
-                                    }
-                                } catch (e) {
-                                    new FilamentNotification()
-                                        .icon('heroicon-o-x-circle')
-                                        .title('Something went wrong, please try again later')
-                                        .iconColor('danger')
-                                        .send()
-                                }
-                            },
-                        });
-                    });
-                },
-            }))
-        })
-    </script>
 </div>
