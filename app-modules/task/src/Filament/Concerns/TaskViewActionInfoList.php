bce3dee84866ace9b8f1ac5a6a7ea8459b139063
Matthew Myers <mxm1070@users.noreply.github.com>
2023-09-22 15:16:47 -0400

    task changes

--- app-modules/task/src/Filament/Concerns/TaskViewActionInfoList.php
+++ app-modules/task/src/Filament/Concerns/TaskViewActionInfoList.php
@@ -19,47 +19,43 @@ trait TaskViewActionInfoList
     public function taskInfoList(): array
     {
         return [
-            Split::make(
-                [
-                    Grid::make()
-                        ->schema(
-                            [
-                                TextEntry::make('description')
-                                    ->columnSpanFull(),
-                                TextEntry::make('assignedTo.name')
-                                    ->label('Assigned To')
-                                    ->url(fn (Task $record) => $record->assignedTo ? UserResource::getUrl('view', ['record' => $record->assignedTo]) : null)
-                                    ->default('Unassigned'),
-                                TextEntry::make('concern.display_name')
-                                    ->label('Concern')
-                                    ->getStateUsing(fn (Task $record) => $record->concern->{$record->concern::displayNameKey()})
-                                    ->url(fn (Task $record) => match ($record->concern ? $record->concern::class : null) {
-                                        Student::class => StudentResource::getUrl('view', ['record' => $record->concern]),
-                                        Prospect::class => ProspectResource::getUrl('view', ['record' => $record->concern]),
-                                        default => null,
-                                    })
-                                    ->default('Unrelated'),
-                            ]
-                        ),
-                    Fieldset::make('metadata')
-                        ->label('Metadata')
-                        ->schema(
-                            [
-                                TextEntry::make('status')
-                                    ->formatStateUsing(fn (TaskStatus $state): string => str($state->value)->title()->headline())
-                                    ->badge()
-                                    ->color(fn (Task $record) => $record->status->getTableColor()),
-                                TextEntry::make('due')
-                                    ->label('Due Date')
-                                    ->default('N/A'),
-                                TextEntry::make('createdBy.name')
-                                    ->label('Created By')
-                                    ->default('N/A')
-                                    ->url(fn (Task $record) => $record->createdBy ? UserResource::getUrl('view', ['record' => $record->createdBy]) : null),
-                            ]
-                        ),
-                ]
-            )->from('md'),
+            Split::make([
+                Grid::make()
+                    ->schema([
+                        TextEntry::make('title')
+                            ->columnSpanFull(),
+                        TextEntry::make('description')
+                            ->columnSpanFull(),
+                        TextEntry::make('assignedTo.name')
+                            ->label('Assigned To')
+                            ->url(fn (Task $record) => $record->assignedTo ? UserResource::getUrl('view', ['record' => $record->assignedTo]) : null)
+                            ->default('Unassigned'),
+                        TextEntry::make('concern.display_name')
+                            ->label('Concern')
+                            ->getStateUsing(fn (Task $record) => $record->concern->{$record->concern::displayNameKey()})
+                            ->url(fn (Task $record) => match ($record->concern ? $record->concern::class : null) {
+                                Student::class => StudentResource::getUrl('view', ['record' => $record->concern]),
+                                Prospect::class => ProspectResource::getUrl('view', ['record' => $record->concern]),
+                                default => null,
+                            })
+                            ->default('Unrelated'),
+                    ]),
+                Fieldset::make('metadata')
+                    ->label('Metadata')
+                    ->schema([
+                        TextEntry::make('status')
+                            ->formatStateUsing(fn (TaskStatus $state): string => str($state->value)->title()->headline())
+                            ->badge()
+                            ->color(fn (Task $record) => $record->status->getTableColor()),
+                        TextEntry::make('due')
+                            ->label('Due Date')
+                            ->default('N/A'),
+                        TextEntry::make('createdBy.name')
+                            ->label('Created By')
+                            ->default('N/A')
+                            ->url(fn (Task $record) => $record->createdBy ? UserResource::getUrl('view', ['record' => $record->createdBy]) : null),
+                    ]),
+            ])->from('md'),
         ];
     }
 }
