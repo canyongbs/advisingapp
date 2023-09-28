b19beb1595792e3566015cc020bc14f251815536
Matthew Myers <mxm1070@users.noreply.github.com>
2023-09-28 09:43:18 -0400

    wip

--- app-modules/assist-data-model/src/Filament/Resources/StudentResource/Pages/ManageStudentEngagement.php
+++ app-modules/assist-data-model/src/Filament/Resources/StudentResource/Pages/ManageStudentEngagement.php
@@ -2,10 +2,46 @@
 
 namespace Assist\AssistDataModel\Filament\Resources\StudentResource\Pages;
 
+use Illuminate\Database\Eloquent\Model;
+use Filament\Resources\Pages\ManageRelatedRecords;
 use Assist\AssistDataModel\Filament\Resources\StudentResource;
-use Assist\Engagement\Filament\Resources\EngagementResource\Pages\ManageEngagements;
+use Assist\AssistDataModel\Filament\Resources\StudentResource\RelationManagers\EngagementsRelationManager;
+use Assist\AssistDataModel\Filament\Resources\StudentResource\RelationManagers\EngagementResponsesRelationManager;
 
-class ManageStudentEngagement extends ManageEngagements
+class ManageStudentEngagement extends ManageRelatedRecords
 {
     protected static string $resource = StudentResource::class;
+
+    // TODO: Obsolete when there is no table, remove from Filament
+    protected static string $relationship = 'engagements';
+
+    protected static ?string $navigationLabel = 'Engagements';
+
+    protected static ?string $breadcrumb = 'Engagements';
+
+    protected static ?string $navigationIcon = 'heroicon-o-chat-bubble-bottom-center-text';
+
+    public static function canAccess(?Model $record = null): bool
+    {
+        foreach ([
+            EngagementsRelationManager::class,
+            EngagementResponsesRelationManager::class,
+        ] as $relationManager) {
+            if (! $relationManager::canViewForRecord($record, static::class)) {
+                continue;
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public function getRelationManagers(): array
+    {
+        return [
+            EngagementsRelationManager::class,
+            EngagementResponsesRelationManager::class,
+        ];
+    }
 }
--- app-modules/assist-data-model/src/Filament/Resources/StudentResource/Pages/ManageStudentFiles.php
+++ app-modules/assist-data-model/src/Filament/Resources/StudentResource/Pages/ManageStudentFiles.php
@@ -2,10 +2,43 @@
 
 namespace Assist\AssistDataModel\Filament\Resources\StudentResource\Pages;
 
+use Illuminate\Database\Eloquent\Model;
+use Filament\Resources\Pages\ManageRelatedRecords;
 use Assist\AssistDataModel\Filament\Resources\StudentResource;
-use Assist\Engagement\Filament\Resources\EngagementResource\Pages\ManageEngagementFiles;
+use Assist\AssistDataModel\Filament\Resources\StudentResource\RelationManagers\EngagementFilesRelationManager;
 
-class ManageStudentFiles extends ManageEngagementFiles
+class ManageStudentFiles extends ManageRelatedRecords
 {
     protected static string $resource = StudentResource::class;
+
+    // TODO: Obsolete when there is no table, remove from Filament
+    protected static string $relationship = 'engagementFiles';
+
+    protected static ?string $navigationLabel = 'Files and Documents';
+
+    protected static ?string $breadcrumb = 'Files';
+
+    protected static ?string $navigationIcon = 'heroicon-o-folder';
+
+    public static function canAccess(?Model $record = null): bool
+    {
+        foreach ([
+            EngagementFilesRelationManager::class,
+        ] as $relationManager) {
+            if (! $relationManager::canViewForRecord($record, static::class)) {
+                continue;
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public function getRelationManagers(): array
+    {
+        return [
+            EngagementFilesRelationManager::class,
+        ];
+    }
 }
