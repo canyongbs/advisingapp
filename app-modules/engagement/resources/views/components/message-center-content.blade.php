12d263acd4ba5e04d6c26ca78dfa8aa1d7a06572
Derek Goetz <derek@kirschbaumdevelopment.com>
2023-10-02 16:37:36 -0400

    Fix styles, add sticky header.

--- app-modules/engagement/resources/views/components/message-center-content.blade.php
+++ app-modules/engagement/resources/views/components/message-center-content.blade.php
@@ -1,12 +1,21 @@
 <div
-    class="max-h-content w-full overflow-y-scroll rounded-r-lg border-b-2 border-r-2 border-t-2 bg-white p-4 dark:border-gray-700 dark:bg-gray-800">
+    class="max-h-content w-full overflow-y-scroll rounded-r-lg border-b-2 border-r-2 border-t-2 bg-white dark:border-gray-700 dark:bg-gray-900">
     @if ($loadingTimeline)
         <x-filament::loading-indicator class="h-12 w-12" />
     @else
         @if (is_null($educatable))
-            <x-engagement::empty-state />
+            <div class="p-4">
+                <x-engagement::empty-state />
+            </div>
         @else
-            <x-timeline::timeline :aggregateRecords="$aggregateRecords" />
+            <div>
+                <div class="sticky top-0 z-[5] flex h-12 w-full items-center bg-gray-100 dark:bg-gray-700">
+                    <h1 class="ml-2">{{ $educatable->display_name }}</h1>
+                </div>
+                <div class="p-6">
+                    <x-timeline::timeline :aggregateRecords="$aggregateRecords" />
+                </div>
+            </div>
         @endif
     @endif
 </div>
