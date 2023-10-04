0c0ae94923755733646311b65b90b01dba25acc5
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-10-04 13:28:55 -0400

    Fix formatting
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- app-modules/assistant/resources/views/filament/pages/ai-assistant.blade.php
+++ app-modules/assistant/resources/views/filament/pages/ai-assistant.blade.php
@@ -29,11 +29,20 @@ use Assist\Assistant\Services\AIInterface\Enums\AIChatMessageFrom;
                         </div>
                     @endforeach
                     @if ($showCurrentResponse)
-                        <div class="my-4 w-3/4 rounded-lg bg-gray-500 p-4 sm:p-6 lg:px-8" x-data="currentResponseData">
+                        <div
+                            class="my-4 w-3/4 rounded-lg bg-gray-500 p-4 sm:p-6 lg:px-8"
+                            x-data="currentResponseData"
+                        >
                             <h1 class="mb-1 text-2xl">AI Assistant</h1>
-                            <p class="hidden" wire:stream="currentResponse"
-                               id="hidden_current_response">{{ $currentResponse }}</p>
-                            <p id="current_response" class="prose dark:prose-invert"></p>
+                            <p
+                                class="hidden"
+                                id="hidden_current_response"
+                                wire:stream="currentResponse"
+                            >{{ $currentResponse }}</p>
+                            <p
+                                class="prose dark:prose-invert"
+                                id="current_response"
+                            ></p>
                         </div>
                     @endif
                     @if ($renderError)
@@ -47,64 +56,64 @@ use Assist\Assistant\Services\AIInterface\Enums\AIChatMessageFrom;
 
             <form wire:submit.prevent="sendMessage">
                 <label
-                        class="sr-only"
-                        for="chat"
+                    class="sr-only"
+                    for="chat"
                 >Your message</label>
                 <div class="flex items-center rounded-lg bg-gray-50 px-3 py-2 dark:bg-gray-700">
                     @if (!$chat->id)
                         <button
-                                class="inline-flex cursor-pointer justify-center rounded-lg p-2 text-gray-500 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white"
-                                type="button"
-                                wire:loading.attr="disabled"
-                                wire:click="save"
+                            class="inline-flex cursor-pointer justify-center rounded-lg p-2 text-gray-500 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white"
+                            type="button"
+                            wire:loading.attr="disabled"
+                            wire:click="save"
                         >
-                            <x-heroicon-s-bookmark class="h-6 w-6"/>
+                            <x-heroicon-s-bookmark class="h-6 w-6" />
                             <span class="sr-only">Save</span>
                         </button>
                     @endif
                     <div class="mx-4 block w-full p-2.5">
                         <textarea
-                                class="mx-4 block w-full rounded-lg border border-gray-300 bg-white p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-                                id="chat"
-                                wire:model.debounce="message"
-                                wire:loading.attr="disabled"
-                                rows="5"
-                                placeholder="Your message..."
+                            class="mx-4 block w-full rounded-lg border border-gray-300 bg-white p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                            id="chat"
+                            wire:model.debounce="message"
+                            wire:loading.attr="disabled"
+                            rows="5"
+                            placeholder="Your message..."
                         ></textarea>
                         <div class="text-red-600">
                             @error('message')
-                            {{ $message }}
+                                {{ $message }}
                             @enderror
                         </div>
                     </div>
                     <button
-                            class="inline-flex cursor-pointer justify-center rounded-full p-2 text-primary-600 hover:bg-primary-100 dark:text-primary-500 dark:hover:bg-gray-600"
-                            type="submit"
-                            wire:loading.remove
-                            x-on:click="$wire.showCurrentResponse = true"
+                        class="inline-flex cursor-pointer justify-center rounded-full p-2 text-primary-600 hover:bg-primary-100 dark:text-primary-500 dark:hover:bg-gray-600"
+                        type="submit"
+                        wire:loading.remove
+                        x-on:click="$wire.showCurrentResponse = true"
                     >
-                        <x-heroicon-s-paper-airplane class="h-6 w-6"/>
+                        <x-heroicon-s-paper-airplane class="h-6 w-6" />
                         <span class="sr-only">Send message</span>
                     </button>
                     <svg
-                            class="-ml-1 mr-3 h-5 w-5 animate-spin text-primary-600"
-                            wire:loading
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
+                        class="-ml-1 mr-3 h-5 w-5 animate-spin text-primary-600"
+                        wire:loading
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
                     >
                         <circle
-                                class="opacity-25"
-                                cx="12"
-                                cy="12"
-                                r="10"
-                                stroke="currentColor"
-                                stroke-width="4"
+                            class="opacity-25"
+                            cx="12"
+                            cy="12"
+                            r="10"
+                            stroke="currentColor"
+                            stroke-width="4"
                         ></circle>
                         <path
-                                class="opacity-75"
-                                fill="currentColor"
-                                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                            class="opacity-75"
+                            fill="currentColor"
+                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
                         ></path>
                     </svg>
                 </div>
@@ -117,18 +126,18 @@ use Assist\Assistant\Services\AIInterface\Enums\AIChatMessageFrom;
             </div>
         @else
             <div class="flex h-full w-full items-center justify-center">
-                <x-filament::loading-indicator class="h-12 w-12"/>
+                <x-filament::loading-indicator class="h-12 w-12" />
             </div>
         @endif
 
         @if ($consentedToTerms === false)
             {{-- TODO potentially prevent closure of modal by pressing escape --}}
             <x-filament::modal
-                    id="consent-agreement"
-                    width="5xl"
-                    alignment="center"
-                    :close-by-clicking-away="false"
-                    :close-button="false"
+                id="consent-agreement"
+                width="5xl"
+                alignment="center"
+                :close-by-clicking-away="false"
+                :close-button="false"
             >
                 @if ($loading === false)
                     <x-slot name="trigger">
@@ -158,13 +167,13 @@ use Assist\Assistant\Services\AIInterface\Enums\AIChatMessageFrom;
 
                 <x-slot name="footer">
                     <form
-                            class="flex w-full flex-col"
-                            wire:submit="confirmConsent"
+                        class="flex w-full flex-col"
+                        wire:submit="confirmConsent"
                     >
                         <label class="mx-auto">
                             <x-filament::input.checkbox
-                                    wire:model="consentedToTerms"
-                                    required
+                                wire:model="consentedToTerms"
+                                required
                             />
                             <span class="ml-2">
                                 I agree to the terms and conditions
@@ -173,17 +182,17 @@ use Assist\Assistant\Services\AIInterface\Enums\AIChatMessageFrom;
 
                         <div class="mt-4 flex justify-center space-x-4">
                             <x-filament::button
-                                    class="mt-4 md:mt-0"
-                                    wire:click="denyConsent"
-                                    outlined
-                                    color="warning"
+                                class="mt-4 md:mt-0"
+                                wire:click="denyConsent"
+                                outlined
+                                color="warning"
                             >
                                 Cancel
                             </x-filament::button>
                             <x-filament::button
-                                    class="mt-4 md:mt-0"
-                                    type="submit"
-                                    color="success"
+                                class="mt-4 md:mt-0"
+                                type="submit"
+                                color="success"
                             >
                                 I understand
                             </x-filament::button>
