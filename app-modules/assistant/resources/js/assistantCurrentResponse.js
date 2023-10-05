fdca2afa9037554025df1a01b891094b79d2c89f
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-10-05 18:24:54 -0400

    Prettier formatting
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- app-modules/assistant/resources/js/assistantCurrentResponse.js
+++ app-modules/assistant/resources/js/assistantCurrentResponse.js
@@ -4,18 +4,23 @@ import DOMPurify from 'dompurify';
 document.addEventListener('alpine:init', () => {
     Alpine.data('currentResponseData', () => ({
         init() {
-            const observer = new MutationObserver((mutations) =>{
-                mutations.forEach(function(mutation) {
+            const observer = new MutationObserver((mutations) => {
+                mutations.forEach(function (mutation) {
                     if (mutation.type === 'childList') {
                         const converter = new showdown.Converter();
                         document.getElementById('current_response').innerHTML = converter.makeHtml(
-                            DOMPurify.sanitize(document.getElementById('hidden_current_response').innerText)
+                            DOMPurify.sanitize(document.getElementById('hidden_current_response').innerText),
                         );
                     }
                 });
             });
 
-            observer.observe(document.getElementById('hidden_current_response'), { attributes: false, childList: true, characterData: false, subtree: true });
+            observer.observe(document.getElementById('hidden_current_response'), {
+                attributes: false,
+                childList: true,
+                characterData: false,
+                subtree: true,
+            });
         },
-    }))
+    }));
 });
