a9495abe9790322660f981a354d4bfa5f1dd8879
Derek Goetz <derek@kirschbaumdevelopment.com>
2023-10-05 02:37:01 -0400

    Search width.

--- app-modules/engagement/resources/views/components/search.blade.php
+++ app-modules/engagement/resources/views/components/search.blade.php
@@ -1,5 +1,8 @@
 <div class="flex flex-col items-center space-x-2 rounded-tl-lg border-b border-gray-200 p-4 dark:border-gray-700">
-    <x-filament::input.wrapper prefix-icon="heroicon-o-magnifying-glass">
+    <x-filament::input.wrapper
+        class="w-full"
+        prefix-icon="heroicon-o-magnifying-glass"
+    >
         <x-filament::input
             wire:model.live.debounce.150ms="search"
             placeholder="Search Engagements"
