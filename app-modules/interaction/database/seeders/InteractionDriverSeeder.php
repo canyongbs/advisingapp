1bb1c96343021d8e60426d6b0ef31f293e384171
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-10-05 11:17:05 -0400

    New InteractionDriver seeds

--- app-modules/interaction/database/seeders/InteractionDriverSeeder.php
+++ app-modules/interaction/database/seeders/InteractionDriverSeeder.php
@@ -10,7 +10,25 @@ class InteractionDriverSeeder extends Seeder
     public function run(): void
     {
         InteractionDriver::factory()
-            ->count(10)
-            ->create();
+            ->createMany(
+                [
+                    ['name' => 'N/A'],
+                    ['name' => 'College Applicant to Matriculation'],
+                    ['name' => 'College Inquiry to Applicant'],
+                    ['name' => 'College Matriculation to Enroll'],
+                    ['name' => 'College RFI'],
+                    ['name' => 'District Inquiry to Applicant'],
+                    ['name' => 'Dual Reengagement'],
+                    ['name' => 'Early College Plan Update'],
+                    ['name' => 'Early College Transition'],
+                    ['name' => 'Enrollment Cancellation Outreach'],
+                    ['name' => 'Enrollment Cancellation Recovery'],
+                    ['name' => 'Financial Aid Awarded Not Enrolled'],
+                    ['name' => 'Financial Aid Task List'],
+                    ['name' => 'MIH Follow Up'],
+                    ['name' => 'MIH Text'],
+                    ['name' => 'Third Party SEM'],
+                ]
+            );
     }
 }
