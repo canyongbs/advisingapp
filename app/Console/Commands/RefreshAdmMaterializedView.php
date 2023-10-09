c2bc300313eed0699cd7a85a2e16e2643cf77e36
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-10-09 18:53:56 -0400

    Log the process of the ADM MV refresh
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- app/Console/Commands/RefreshAdmMaterializedView.php
+++ app/Console/Commands/RefreshAdmMaterializedView.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use Exception;
 use Illuminate\Console\Command;
 use Illuminate\Support\Benchmark;
 use Illuminate\Support\Facades\DB;
@@ -23,22 +24,35 @@ class RefreshAdmMaterializedView extends Command
      */
     public function handle()
     {
-        $benchmark = Benchmark::measure(function () {
-            $database = DB::connection($this->option('connection'));
+        try {
+            $benchmark = Benchmark::measure(function () {
+                $database = DB::connection($this->option('connection'));
 
-            $remoteTable = $this->argument('remoteTable');
+                $remoteTable = $this->argument('remoteTable');
 
-            $localTable = $remoteTable . '_local';
+                $localTable = $remoteTable . '_local';
 
-            $temporaryTable = $localTable . '_tmp';
+                $temporaryTable = $localTable . '_tmp';
 
-            $database->unprepared("CREATE MATERIALIZED VIEW {$temporaryTable} AS SELECT * FROM {$remoteTable};");
+                $database->unprepared("CREATE MATERIALIZED VIEW {$temporaryTable} AS SELECT * FROM {$remoteTable};");
 
-            $indexColumn = $this->option('indexColumn');
+                $indexColumn = $this->option('indexColumn');
 
-            $database->unprepared("BEGIN TRANSACTION; DROP MATERIALIZED VIEW IF EXISTS {$localTable}; ALTER MATERIALIZED VIEW {$temporaryTable} RENAME TO {$localTable}; CREATE INDEX idx_{$remoteTable}_{$indexColumn} ON {$localTable} ({$indexColumn}); COMMIT;");
-        });
+                $database->unprepared("BEGIN TRANSACTION; DROP MATERIALIZED VIEW IF EXISTS {$localTable}; ALTER MATERIALIZED VIEW {$temporaryTable} RENAME TO {$localTable}; CREATE INDEX idx_{$remoteTable}_{$indexColumn} ON {$localTable} ({$indexColumn}); COMMIT;");
+            });
 
-        Log::info("RefreshAdmMaterializedView {$this->argument('remoteTable')} {$benchmark}");
+            Log::channel('amd_refresh')->info($benchmark, [
+                'remoteTable' => $this->argument('remoteTable'),
+                'indexColumn' => $this->option('indexColumn'),
+                'connection' => $this->option('connection'),
+            ]);
+        } catch (Exception $e) {
+            // TODO: Notify someone
+            Log::channel('amd_refresh')->error($e->getMessage(), [
+                'remoteTable' => $this->argument('remoteTable'),
+                'indexColumn' => $this->option('indexColumn'),
+                'connection' => $this->option('connection'),
+            ]);
+        }
     }
 }
