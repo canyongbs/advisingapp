c2b25d791165857652c47b1b84eb0db47acdae69
Derek Goetz <derek@kirschbaumdevelopment.com>
2023-10-10 19:36:48 -0400

    Add comments, color palettes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,6 +23,32 @@ export default {
         },
         // Changes to colors also need to be reflected in FilamentServiceProvider
         colors: {
+            black: {
+                50: '#f6f6f6',
+                100: '#e7e7e7',
+                200: '#d1d1d1',
+                300: '#b0b0b0',
+                400: '#888888',
+                500: '#6d6d6d',
+                600: '#5d5d5d',
+                700: '#4f4f4f',
+                800: '#454545',
+                900: '#3d3d3d',
+                950: '#000000',
+            },
+            white: {
+                50: '#ffffff',
+                100: '#efefef',
+                200: '#dcdcdc',
+                300: '#bdbdbd',
+                400: '#989898',
+                500: '#7c7c7c',
+                600: '#656565',
+                700: '#525252',
+                800: '#464646',
+                900: '#3d3d3d',
+                950: '#292929',
+            },
             'dodger-blue': {
                 50: '#eef3ff',
                 100: '#dae4ff',
