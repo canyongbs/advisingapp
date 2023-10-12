45e190c1ded2a4e12d1e8156e52cbfe6cc726592
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-10-11 20:02:43 -0400

    Use the underlying trait
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- app/Providers/MultiConnectionParallelTestingServiceProvider.php
+++ app/Providers/MultiConnectionParallelTestingServiceProvider.php
@@ -5,11 +5,9 @@ namespace App\Providers;
 use Closure;
 use Illuminate\Support\Arr;
 use PHPUnit\Framework\TestCase;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
-use Doctrine\DBAL\Query\QueryException;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Testing\Concerns\TestDatabases;
 use Illuminate\Support\Facades\ParallelTesting;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -19,11 +17,11 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 /**
  * @class MultiConnectionParallelTestingServiceProvider
  *
- * Based on vendor/laravel/framework/src/Illuminate/Testing/Concerns/TestDatabases.php and https://sarahjting.com/blog/laravel-paratest-multiple-db-connections
+ * Based on https://sarahjting.com/blog/laravel-paratest-multiple-db-connections
  */
 class MultiConnectionParallelTestingServiceProvider extends ServiceProvider
 {
-    protected static bool $schemaIsUpToDate = false;
+    use TestDatabases;
 
     protected array $parallelConnections = [
         [
@@ -102,129 +100,6 @@ class MultiConnectionParallelTestingServiceProvider extends ServiceProvider
                 });
             });
         }
-    }
-
-    /**
-     * Ensure a test database exists and returns its name.
-     *
-     * @param  string  $database
-     *
-     * @return array
-     */
-    protected function ensureTestDatabaseExists($database)
-    {
-        $testDatabase = $this->testDatabase($database);
-
-        try {
-            $this->usingDatabase($testDatabase, function () {
-                Schema::hasTable('dummy');
-            });
-        } catch (QueryException) {
-            $this->usingDatabase($database, function () use ($testDatabase) {
-                Schema::dropDatabaseIfExists($testDatabase);
-                Schema::createDatabase($testDatabase);
-            });
-
-            return [$testDatabase, true];
-        }
-
-        return [$testDatabase, false];
-    }
-
-    /**
-     * Ensure the current database test schema is up to date.
-     *
-     * @return void
-     */
-    protected function ensureSchemaIsUpToDate()
-    {
-        if (! static::$schemaIsUpToDate) {
-            Artisan::call('migrate');
-
-            static::$schemaIsUpToDate = true;
-        }
-    }
-
-    /**
-     * Runs the given callable using the given database.
-     *
-     * @param  string  $database
-     * @param  callable  $callable
-     *
-     * @return void
-     */
-    protected function usingDatabase($database, $callable)
-    {
-        $original = DB::getConfig('database');
-
-        try {
-            $this->switchToDatabase($database);
-            $callable();
-        } finally {
-            $this->switchToDatabase($original);
-        }
-    }
-
-    /**
-     * Apply the given callback when tests are not using in memory database.
-     *
-     * @param  callable  $callback
-     *
-     * @return void
-     */
-    protected function whenNotUsingInMemoryDatabase($callback)
-    {
-        if (ParallelTesting::option('without_databases')) {
-            return;
-        }
-
-        $database = DB::getConfig('database');
-
-        if ($database !== ':memory:') {
-            $callback($database);
-        }
-    }
-
-    /**
-     * Switch to the given database.
-     *
-     * @param  string  $database
-     *
-     * @return void
-     */
-    protected function switchToDatabase($database)
-    {
-        DB::purge();
-
-        $default = config('database.default');
-
-        $url = config("database.connections.{$default}.url");
-
-        if ($url) {
-            config()->set(
-                "database.connections.{$default}.url",
-                preg_replace('/^(.*)(\/[\w-]*)(\??.*)$/', "$1/{$database}$3", $url),
-            );
-        } else {
-            config()->set(
-                "database.connections.{$default}.database",
-                $database,
-            );
-        }
-    }
-
-    /**
-     * Returns the test database name.
-     *
-     * @param mixed $database
-     *
-     * @return string
-     */
-    protected function testDatabase($database)
-    {
-        $token = ParallelTesting::token();
-
-        return "{$database}_test_{$token}";
     }
 
     protected function usingConnection(array $connection, Closure $callable): void
