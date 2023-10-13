dfbfede4cd806d482915e20d797fd25d9263b6f9
Derek Goetz <derek@kirschbaumdevelopment.com>
2023-10-13 10:54:07 -0400

    Clean up pagination to avoid overflow.

--- app-modules/engagement/resources/views/filament/pages/message-center.blade.php
+++ app-modules/engagement/resources/views/filament/pages/message-center.blade.php
@@ -5,7 +5,7 @@
 >
     <div
         class="h-full w-full flex-1 flex-col rounded-lg border-0 border-gray-200 dark:border-gray-700 md:overflow-y-auto md:border">
-        <div class="grid h-full grid-cols-1 md:grid-cols-4">
+        <div class="grid h-full grid-cols-1 md:grid-cols-8">
             @if ($loadingInbox)
                 <x-filament::loading-indicator class="col-span-full h-12 w-12" />
             @else
