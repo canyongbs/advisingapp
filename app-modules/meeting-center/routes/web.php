ed6fcbb30de240401662be629b5dd94356ca672c
Matthew Myers <mxm1070@users.noreply.github.com>
2023-10-19 13:09:35 -0400

    Fix route helper.

--- app-modules/meeting-center/routes/web.php
+++ app-modules/meeting-center/routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Routing\RouteRegistrar;
 use Assist\MeetingCenter\Enums\CalendarProvider;
 use Assist\MeetingCenter\Http\Controllers\GoogleCalendarController;
 use Assist\MeetingCenter\Http\Controllers\OutlookCalendarController;
@@ -9,17 +8,6 @@ Route::middleware(['web', 'auth'])
     ->name('calendar.')
     ->prefix('/calendar')
     ->group(function () {
-        providerRoutes(CalendarProvider::Google, GoogleCalendarController::class);
-        providerRoutes(CalendarProvider::Outlook, OutlookCalendarController::class);
+        provider_routes(CalendarProvider::Google, GoogleCalendarController::class);
+        provider_routes(CalendarProvider::Outlook, OutlookCalendarController::class);
     });
-
-function providerRoutes(CalendarProvider $provider, string $controller): RouteRegistrar
-{
-    return Route::name($provider->value . '.')
-        ->prefix('/' . $provider->value)
-        ->controller($controller)
-        ->group(function () {
-            Route::get('/login', 'login')->name('login');
-            Route::get('/callback', 'callback')->name('callback');
-        });
-}
