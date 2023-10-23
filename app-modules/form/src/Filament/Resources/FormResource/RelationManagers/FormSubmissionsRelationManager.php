d027654f11d78e7219a4a6396649e1e7f982e094
Matthew Myers <mxm1070@users.noreply.github.com>
2023-10-23 11:25:19 -0400

    wip

--- app-modules/form/src/Filament/Resources/FormResource/RelationManagers/FormSubmissionsRelationManager.php
+++ app-modules/form/src/Filament/Resources/FormResource/RelationManagers/FormSubmissionsRelationManager.php
@@ -4,13 +4,17 @@ namespace Assist\Form\Filament\Resources\FormResource\RelationManagers;
 
 use Filament\Forms\Form;
 use Filament\Tables\Table;
+use Maatwebsite\Excel\Excel;
 use App\Filament\Columns\IdColumn;
 use Filament\Forms\Components\KeyValue;
 use Filament\Tables\Actions\ViewAction;
 use Filament\Tables\Columns\TextColumn;
+use pxlrbt\FilamentExcel\Columns\Column;
 use Filament\Tables\Actions\DeleteAction;
 use Filament\Tables\Actions\BulkActionGroup;
 use Filament\Tables\Actions\DeleteBulkAction;
+use pxlrbt\FilamentExcel\Exports\ExcelExport;
+use pxlrbt\FilamentExcel\Actions\Tables\ExportAction;
 use App\Filament\Resources\RelationManagers\RelationManager;
 
 class FormSubmissionsRelationManager extends RelationManager
@@ -38,6 +42,30 @@ class FormSubmissionsRelationManager extends RelationManager
             ->filters([
             ])
             ->headerActions([
+                ExportAction::make()
+                    ->exports([
+                        ExcelExport::make()
+                            ->modifyQueryUsing(function ($query) {
+                                return $query->where('form_id', $this->getOwnerRecord()->id);
+                            })
+                            ->withFilename(function () {
+                                return str("form-submissions-{$this->getOwnerRecord()->name}-")
+                                    ->append(now()->format('Y-m-d-Hisv'))
+                                    ->slug();
+                            })
+                            ->withWriterType(Excel::CSV)
+                            ->withColumns([
+                                Column::make('id'),
+                                Column::make('form_id'),
+                                Column::make('content')
+                                    ->formatStateUsing(function ($state, $record) {
+                                        return json_encode($record->content);
+                                    }),
+                                Column::make('created_at'),
+                                Column::make('updated_at'),
+                            ])
+                            ->withNamesAsHeadings(),
+                    ]),
             ])
             ->actions([
                 ViewAction::make(),
