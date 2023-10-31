0dd746e34caff4c475ba47da954a6be6559d89cb
Derek Goetz <derek@kirschbaumdevelopment.com>
2023-10-31 00:59:57 -0400

    Format date.

--- resources/views/filament/forms/components/campaigns/actions/service-request.blade.php
+++ resources/views/filament/forms/components/campaigns/actions/service-request.blade.php
@@ -1,4 +1,5 @@
 @php
+    use Carbon\Carbon;
     use App\Models\User;
     use Assist\Division\Models\Division;
     use Assist\ServiceManagement\Models\ServiceRequestType;
@@ -42,7 +43,7 @@
         </div>
         <div class="flex flex-col pt-3">
             <dt class="mb-1 text-sm text-gray-500 dark:text-gray-400">Execute At</dt>
-            <dd class="text-sm font-semibold">{{ $action['execute_at'] }}</dd>
+            <dd class="text-sm font-semibold">{{ Carbon::parse($action['execute_at'])->format('m/d/Y H:i:s') }}</dd>
         </div>
     </dl>
 
