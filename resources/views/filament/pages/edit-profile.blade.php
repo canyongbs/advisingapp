57826eb20382fd9103e56d274b89c2fb16f85b98
Orrison <Orrison@users.noreply.github.com>
2023-11-02 15:44:28 +0000

    chore: fix code style

--- resources/views/filament/pages/edit-profile.blade.php
+++ resources/views/filament/pages/edit-profile.blade.php
@@ -3,8 +3,8 @@
         {{ $this->form }}
 
         <x-filament-panels::form.actions
-                :actions="$this->getCachedFormActions()"
-                :full-width="$this->hasFullWidthFormActions()"
+            :actions="$this->getCachedFormActions()"
+            :full-width="$this->hasFullWidthFormActions()"
         />
     </x-filament-panels::form>
 </x-filament-panels::page>
