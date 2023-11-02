b44f594eaa7d24b92c8dad67450be1fb6cb03a1d
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-11-02 08:34:14 -0700

    Create different sections on the profile page
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- app/Filament/Pages/EditProfile.php
+++ app/Filament/Pages/EditProfile.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Hash;
 use Filament\Forms\Components\Select;
 use Filament\Support\Enums\Alignment;
 use Filament\Support\Exceptions\Halt;
+use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Checkbox;
 use Illuminate\Database\Eloquent\Model;
 use Filament\Forms\Components\Component;
@@ -44,49 +45,63 @@ class EditProfile extends Page
 
         return $form
             ->schema([
-                SpatieMediaLibraryFileUpload::make('avatar')
-                    ->label('Avatar')
-                    ->visibility('private')
-                    ->disk('s3')
-                    ->collection('avatar')
-                    ->hidden($user->is_external)
-                    ->avatar(),
-                $this->getNameFormComponent()
-                    ->disabled($user->is_external),
-                $this->getEmailFormComponent()
-                    ->disabled($user->is_external),
-                $this->getPasswordFormComponent()
-                    ->hidden($user->is_external),
-                $this->getPasswordConfirmationFormComponent()
-                    ->hidden($user->is_external),
-                TimezoneSelect::make('timezone')
-                    ->required()
-                    ->selectablePlaceholder(false),
-                RichEditor::make('bio')
-                    ->label('Personal Bio')
-                    ->toolbarButtons(['bold', 'italic', 'underline', 'link', 'blockquote', 'bulletList', 'orderedList'])
-                    ->hint(fn (Get $get): string => $get('is_bio_visible_on_profile') ? 'Visible on profile' : 'Not visible on profile'),
-                Checkbox::make('is_bio_visible_on_profile')
-                    ->label('Show Bio on profile')
-                    ->live(),
-                Select::make('pronouns_id')
-                    ->relationship('pronouns', 'label')
-                    ->hint(fn (Get $get): string => $get('are_pronouns_visible_on_profile') ? 'Visible on profile' : 'Not visible on profile'),
-                Checkbox::make('are_pronouns_visible_on_profile')
-                    ->label('Show Pronouns on profile')
-                    ->live(),
-                Placeholder::make('teams')
-                    ->label(str('Team')->plural($user->teams->count()))
-                    ->content($user->teams->pluck('name')->join(', ', ' and '))
-                    ->hidden($user->teams->isEmpty())
-                    ->hint(fn (Get $get): string => $get('are_teams_visible_on_profile') ? 'Visible on profile' : 'Not visible on profile'),
-                Checkbox::make('are_teams_visible_on_profile')
-                    ->label('Show ' . str('team')->plural($user->teams->count()) . ' on profile')
-                    ->live(),
-                Placeholder::make('external_avatar')
-                    ->label('Avatar')
-                    ->content('Your authentication into this application is managed through single sign on (SSO). Please update your profile picture in your source authentication system and then logout and login here to persist that update into this application.')
-                    ->visible($user->is_external),
+                Section::make('Profile Information')
+                    ->description('This information is visible to other users on your profile page, if you choose to make it visible.')
+                    ->aside()
+                    ->schema(
+                        [
+                            SpatieMediaLibraryFileUpload::make('avatar')
+                                ->label('Avatar')
+                                ->visibility('private')
+                                ->disk('s3')
+                                ->collection('avatar')
+                                ->hidden($user->is_external)
+                                ->avatar(),
+                            $this->getNameFormComponent()
+                                ->disabled($user->is_external),
+                            RichEditor::make('bio')
+                                ->label('Personal Bio')
+                                ->toolbarButtons(['bold', 'italic', 'underline', 'link', 'blockquote', 'bulletList', 'orderedList'])
+                                ->hint(fn (Get $get): string => $get('is_bio_visible_on_profile') ? 'Visible on profile' : 'Not visible on profile'),
+                            Checkbox::make('is_bio_visible_on_profile')
+                                ->label('Show Bio on profile')
+                                ->live(),
+                            Select::make('pronouns_id')
+                                ->relationship('pronouns', 'label')
+                                ->hint(fn (Get $get): string => $get('are_pronouns_visible_on_profile') ? 'Visible on profile' : 'Not visible on profile'),
+                            Checkbox::make('are_pronouns_visible_on_profile')
+                                ->label('Show Pronouns on profile')
+                                ->live(),
+                            Placeholder::make('teams')
+                                ->label(str('Team')->plural($user->teams->count()))
+                                ->content($user->teams->pluck('name')->join(', ', ' and '))
+                                ->hidden($user->teams->isEmpty())
+                                ->hint(fn (Get $get): string => $get('are_teams_visible_on_profile') ? 'Visible on profile' : 'Not visible on profile'),
+                            Checkbox::make('are_teams_visible_on_profile')
+                                ->label('Show ' . str('team')->plural($user->teams->count()) . ' on profile')
+                                ->live(),
+                            Placeholder::make('external_avatar')
+                                ->label('Avatar')
+                                ->content('Your authentication into this application is managed through single sign on (SSO). Please update your profile picture in your source authentication system and then logout and login here to persist that update into this application.')
+                                ->visible($user->is_external),
+                        ]
+                    ),
+                Section::make('Account Information')
+                    ->description('Update your account\'s information.')
+                    ->aside()
+                    ->schema(
+                        [
+                            $this->getEmailFormComponent()
+                                ->disabled($user->is_external),
+                            $this->getPasswordFormComponent()
+                                ->hidden($user->is_external),
+                            $this->getPasswordConfirmationFormComponent()
+                                ->hidden($user->is_external),
+                            TimezoneSelect::make('timezone')
+                                ->required()
+                                ->selectablePlaceholder(false),
+                        ]
+                    ),
             ]);
     }
 
