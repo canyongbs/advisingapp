7092c0204e7180778bb0a45c22fff7008ab6ca50
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-11-02 15:29:20 -0700

    Change uuid owner
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- app-modules/assist-data-model/database/migrations/2023_08_11_235451_create_implicit_varchar_as_uuid_cast.php
+++ app-modules/assist-data-model/database/migrations/2023_08_11_235451_create_implicit_varchar_as_uuid_cast.php
@@ -9,6 +9,10 @@ return new class () extends Migration {
      */
     public function up(): void
     {
+        $dbUser = config('database.connections.pgsql.username');
+
+        DB::unprepared("ALTER TYPE uuid OWNER TO {$dbUser};");
+
         DB::unprepared('DROP CAST IF EXISTS (VARCHAR AS uuid)');
 
         DB::unprepared('CREATE CAST (VARCHAR AS uuid) WITH INOUT AS IMPLICIT');
