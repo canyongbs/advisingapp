901df30a3d7c3fa9080b3d6a8d6a37dea3861206
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-11-06 16:29:29 -0500

    Write a test for CareTeam Campaign Action
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- app-modules/assist-data-model/src/Models/Contracts/Educatable.php
+++ app-modules/assist-data-model/src/Models/Contracts/Educatable.php
@@ -2,8 +2,12 @@
 
 namespace Assist\AssistDataModel\Models\Contracts;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
+/**
+* @property-read Collection $careTeam
+ */
 interface Educatable extends Identifiable
 {
     public static function displayNameKey(): string;
