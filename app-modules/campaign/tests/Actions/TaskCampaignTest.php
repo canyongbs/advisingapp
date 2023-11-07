1703050db9050b912eefc0fe129eb043b45bff32
Matthew Myers <mxm1070@users.noreply.github.com>
2023-11-07 18:02:48 -0500

    Add test.

---
+++ app-modules/campaign/tests/Actions/TaskCampaignTest.php
@@ -0,0 +1,58 @@
+<?php
+
+use App\Models\User;
+use Assist\Task\Models\Task;
+use Assist\Campaign\Models\Campaign;
+use Assist\Prospect\Models\Prospect;
+use Assist\Campaign\Models\CampaignAction;
+use Assist\Campaign\Enums\CampaignActionType;
+use Assist\CaseloadManagement\Models\Caseload;
+use Assist\CaseloadManagement\Enums\CaseloadType;
+
+it('will create the task records for educatables in the caseload', function () {
+    expect(Task::count())->toBe(0);
+
+    $prospects = Prospect::factory()->count(3)->create([
+        'first_name' => 'TestTest',
+    ]);
+
+    $caseload = Caseload::factory()->create([
+        'type' => CaseloadType::Static,
+    ]);
+
+    $prospects->each(function (Prospect $prospect) use ($caseload) {
+        $caseload->subjects()->create([
+            'subject_id' => $prospect->getKey(),
+            'subject_type' => $prospect->getMorphClass(),
+        ]);
+    });
+
+    $campaign = Campaign::factory()->create([
+        'caseload_id' => $caseload->id,
+    ]);
+
+    $data = [
+        'title' => 'Title',
+        'description' => 'This is a description.',
+        'due' => now()->addDay(),
+        'assigned_to' => User::factory()->create()->id,
+    ];
+
+    $action = CampaignAction::factory()
+        ->for($campaign, 'campaign')
+        ->create([
+            'type' => CampaignActionType::Task,
+            'data' => $data,
+        ]);
+
+    $action->execute();
+
+    expect(Task::count())->toBe($prospects->count());
+
+    $task = Task::first();
+
+    expect($task->title)->toBe($data['title']);
+    expect($task->description)->toBe($data['description']);
+    expect($task->due->toString())->toBe($data['due']->toString());
+    expect($task->assigned_to)->toBe($data['assigned_to']);
+});
