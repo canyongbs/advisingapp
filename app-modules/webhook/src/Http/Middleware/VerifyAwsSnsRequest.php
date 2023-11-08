025d501583cd1c4421288b4ffa14a47b8c4408de
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-11-08 14:20:18 -0500

    Refactor and test SNS verifications middleware
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- app-modules/webhook/src/Http/Middleware/VerifyAwsSnsRequest.php
+++ app-modules/webhook/src/Http/Middleware/VerifyAwsSnsRequest.php
@@ -3,6 +3,7 @@
 namespace Assist\Webhook\Http\Middleware;
 
 use Closure;
+use Exception;
 use Aws\Sns\Message;
 use Illuminate\Http\Request;
 use Aws\Sns\MessageValidator;
@@ -12,18 +13,21 @@ class VerifyAwsSnsRequest
 {
     public function handle(Request $request, Closure $next): Response
     {
-        if (! $request->hasHeader('x-amz-sns-message-type')) {
-            abort(404);
+        try {
+            if (! $request->hasHeader('x-amz-sns-message-type')) {
+                throw new Exception('SNS message type header not provided.', 404);
+            }
+
+            $message = new Message(json_decode($request->getContent(), true));
+
+            if (! app(MessageValidator::class)->isValid($message)) {
+                throw new Exception('SNS message validation failed.', 404);
+            }
+
+            return $next($request);
+        } catch (Exception $exception) {
+            report($exception);
+            abort(array_key_exists($exception->getCode(), Response::$statusTexts) ? $exception->getCode() : 500);
         }
-
-        $message = Message::fromRawPostData();
-
-        $validator = new MessageValidator();
-
-        if (! $validator->isValid($message)) {
-            abort(404);
-        }
-
-        return $next($request);
     }
 }
