311dc16d43fae1a6634eb70149ecdc33f7e3d066
Matthew Myers <mxm1070@users.noreply.github.com>
2023-11-08 14:46:15 -0500

    Change test.

--- app-modules/campaign/tests/Actions/TaskCampaignTest.php
+++ app-modules/campaign/tests/Actions/TaskCampaignTest.php
@@ -4,23 +4,22 @@ use App\Models\User;
 use Assist\Task\Models\Task;
 use Assist\Campaign\Models\Campaign;
 use Assist\Prospect\Models\Prospect;
+use Assist\AssistDataModel\Models\Student;
 use Assist\Campaign\Models\CampaignAction;
+use Illuminate\Database\Eloquent\Collection;
 use Assist\Campaign\Enums\CampaignActionType;
 use Assist\CaseloadManagement\Models\Caseload;
 use Assist\CaseloadManagement\Enums\CaseloadType;
+use Assist\AssistDataModel\Models\Contracts\Educatable;
 
-it('will create the task records for educatables in the caseload', function () {
+it('will create the task records for educatables in the caseload', function (Collection $educatables) {
     expect(Task::count())->toBe(0);
-
-    $prospects = Prospect::factory()->count(3)->create([
-        'first_name' => 'TestTest',
-    ]);
 
     $caseload = Caseload::factory()->create([
         'type' => CaseloadType::Static,
     ]);
 
-    $prospects->each(function (Prospect $prospect) use ($caseload) {
+    $educatables->each(function (Educatable $prospect) use ($caseload) {
         $caseload->subjects()->create([
             'subject_id' => $prospect->getKey(),
             'subject_type' => $prospect->getMorphClass(),
@@ -47,15 +46,24 @@ it('will create the task records for educatables in the caseload', function () {
 
     $action->execute();
 
-    expect(Task::count())->toBe($prospects->count());
+    expect(Task::count())->toBe($educatables->count());
 
-    $task = Task::first();
-    $prospect = $prospects->first();
+    $educatables->each(function (Educatable $educatable) use ($data) {
+        expect($educatable->tasks()->count())->toBe(1);
 
-    expect($task->title)->toBe($data['title']);
-    expect($task->description)->toBe($data['description']);
-    expect($task->due->toString())->toBe($data['due']->toString());
-    expect($task->assigned_to)->toBe($data['assigned_to']);
-    expect($task->concern_id)->toBe($prospect->getKey());
-    expect($task->concern_type)->toBe($prospect->getMorphClass());
-});
+        $task = $educatable->tasks()->first();
+        expect($task->title)->toBe($data['title']);
+        expect($task->description)->toBe($data['description']);
+        expect($task->due->toString())->toBe($data['due']->toString());
+        expect($task->assigned_to)->toBe($data['assigned_to']);
+        expect($task->concern_id)->toBe($educatable->getKey());
+        expect($task->concern_type)->toBe($educatable->getMorphClass());
+    });
+})->with([
+    'prospects' => [
+        'educatables' => fn () => Prospect::factory()->count(3)->create(),
+    ],
+    'students' => [
+        'educatables' => fn () => Student::factory()->count(3)->create(),
+    ],
+]);
