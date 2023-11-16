aa961ed2b105f0ea6bc8ff12fa7a83f0a7b00097
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-11-16 11:36:29 -0500

    Prettier format
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- app-modules/in-app-communication/resources/views/filament/pages/user-chat.blade.php
+++ app-modules/in-app-communication/resources/views/filament/pages/user-chat.blade.php
@@ -6,47 +6,47 @@ use Assist\InAppCommunication\Models\TwilioConversation;
 ?>
 
 <x-filament-panels::page
-        class="max-h-screen"
-        full-height="true"
+    class="max-h-screen"
+    full-height="true"
 >
-    <div
-            class="flex h-full flex-col"
-    >
+    <div class="flex h-full flex-col">
         <div class="grid flex-1 grid-cols-1 gap-6 md:grid-cols-4">
             <div class="col-span-1">
                 <div class="flex flex-col gap-y-2">
                     {{ $this->newChatAction }}
 
-
-                    @if($this->conversations->isNotEmpty())
+                    @if ($this->conversations->isNotEmpty())
                         <ul
-                                class="flex flex-col gap-y-1 rounded-xl border border-gray-950/5 bg-white p-2 shadow-sm dark:border-white/10 dark:bg-gray-900">
+                            class="flex flex-col gap-y-1 rounded-xl border border-gray-950/5 bg-white p-2 shadow-sm dark:border-white/10 dark:bg-gray-900">
                             @foreach ($this->conversations as $conversation)
                                 @php
                                     /** @var TwilioConversation $conversation */
                                 @endphp
                                 <li @class([
-                                        'px-2 group cursor-pointer flex rounded-lg w-full items-center outline-none transition duration-75 hover:bg-gray-100 focus:bg-gray-100 dark:hover:bg-white/5 dark:focus:bg-white/5 space-x-1',
-                                        'bg-gray-100 dark:bg-white/5' => $selectedConversation === $conversation['sid'],
-                                    ])>
+                                    'px-2 group cursor-pointer flex rounded-lg w-full items-center outline-none transition duration-75 hover:bg-gray-100 focus:bg-gray-100 dark:hover:bg-white/5 dark:focus:bg-white/5 space-x-1',
+                                    'bg-gray-100 dark:bg-white/5' =>
+                                        $selectedConversation === $conversation['sid'],
+                                ])>
                                     <a
-                                            @class([
-                                                'fi-sidebar-item-button relative flex flex-1 items-center justify-center gap-x-3 rounded-lg py-2 text-sm',
-                                            ])
-                                            wire:click="selectConversation('{{ $conversation['sid'] }}')"
+                                        @class([
+                                            'fi-sidebar-item-button relative flex flex-1 items-center justify-center gap-x-3 rounded-lg py-2 text-sm',
+                                        ])
+                                        wire:click="selectConversation('{{ $conversation['sid'] }}')"
                                     >
-                                    <span @class([
-                                        'fi-sidebar-item-label flex-1 truncate',
-                                        'text-gray-700 dark:text-gray-200' => !$selectedConversation === $conversation['sid'],
-                                        'text-primary-600 dark:text-primary-400' => $selectedConversation === $conversation['sid'],
-                                    ])>
-                                        {{ $conversation->participants()->where('user_id', '!=', auth()->id())->first()->name }}
-                                    </span>
+                                        <span @class([
+                                            'fi-sidebar-item-label flex-1 truncate',
+                                            'text-gray-700 dark:text-gray-200' =>
+                                                !$selectedConversation === $conversation['sid'],
+                                            'text-primary-600 dark:text-primary-400' =>
+                                                $selectedConversation === $conversation['sid'],
+                                        ])>
+                                            {{ $conversation->participants()->where('user_id', '!=', auth()->id())->first()->name }}
+                                        </span>
                                     </a>
                                     {{-- TODO: Will look into this later --}}
-                                    {{--                                <div>--}}
-                                    {{--                                    {{ ($this->deleteChatAction)(['chat' => $chatItem->id]) }}--}}
-                                    {{--                                </div>--}}
+                                    {{--                                <div> --}}
+                                    {{--                                    {{ ($this->deleteChatAction)(['chat' => $chatItem->id]) }} --}}
+                                    {{--                                </div> --}}
                                 </li>
                             @endforeach
                         </ul>
@@ -54,35 +54,56 @@ use Assist\InAppCommunication\Models\TwilioConversation;
                 </div>
             </div>
 
-            @if(!is_null($selectedConversation))
+            @if (!is_null($selectedConversation))
                 <div
+                    class="col-span-1 flex h-full flex-col gap-2 overflow-hidden md:col-span-3"
                     x-data="userToUserChat(`{{ $selectedConversation }}`)"
                     wire:key="conversation-{{ $selectedConversation }}"
-                    class="col-span-1 flex h-full flex-col gap-2 overflow-hidden md:col-span-3"
                 >
-                    <div x-show="loading" x-transition.delay.800ms class="self-center flex flex-col items-center">
+                    <div
+                        class="flex flex-col items-center self-center"
+                        x-show="loading"
+                        x-transition.delay.800ms
+                    >
                         <x-filament::loading-indicator class="h-12 w-12 text-primary-500" />
-                        <p class="text-center" x-text="loadingMessage"></p>
+                        <p
+                            class="text-center"
+                            x-text="loadingMessage"
+                        ></p>
                     </div>
                     <template x-if="!loading && error">
-                        <div class="self-center flex flex-col items-center">
-                            <x-filament::icon class="h-12 w-12 text-primary-500" icon="heroicon-m-exclamation-triangle" />
+                        <div class="flex flex-col items-center self-center">
+                            <x-filament::icon
+                                class="h-12 w-12 text-primary-500"
+                                icon="heroicon-m-exclamation-triangle"
+                            />
                             <p class="text-center">Something went wrong...</p>
-                            <p class="text-center" x-text="errorMessage"></p>
-                            <x-filament::button class="mt-2" x-on:click="errorRetry">Retry</x-filament::button>
+                            <p
+                                class="text-center"
+                                x-text="errorMessage"
+                            ></p>
+                            <x-filament::button
+                                class="mt-2"
+                                x-on:click="errorRetry"
+                            >Retry</x-filament::button>
                         </div>
                     </template>
-                    <div x-show="!loading && !error" x-transition.delay.850ms class="col-span-1 flex h-full flex-col gap-2 overflow-hidden md:col-span-3">
+                    <div
+                        class="col-span-1 flex h-full flex-col gap-2 overflow-hidden md:col-span-3"
+                        x-show="!loading && !error"
+                        x-transition.delay.850ms
+                    >
                         <div
-                            class="flex max-h-[calc(100vh-24rem)] flex-1 flex-col-reverse overflow-y-scroll rounded-xl border border-gray-950/5 text-sm shadow-sm dark:border-white/10 dark:bg-gray-800"
-                        >
+                            class="flex max-h-[calc(100vh-24rem)] flex-1 flex-col-reverse overflow-y-scroll rounded-xl border border-gray-950/5 text-sm shadow-sm dark:border-white/10 dark:bg-gray-800">
                             <div class="divide-y dark:divide-gray-700">
-                                <template x-for="message in messages" :key="message.message.index">
+                                <template
+                                    x-for="message in messages"
+                                    :key="message.message.index"
+                                >
                                     <div class="group w-full dark:bg-gray-800">
                                         <div class="m-auto justify-center p-4 text-base md:gap-6 md:py-6">
                                             <div
-                                                class="mx-auto flex flex-1 gap-4 text-base md:max-w-2xl md:gap-6 lg:max-w-[38rem] xl:max-w-3xl"
-                                            >
+                                                class="mx-auto flex flex-1 gap-4 text-base md:max-w-2xl md:gap-6 lg:max-w-[38rem] xl:max-w-3xl">
                                                 <div class="relative flex flex-shrink-0 flex-col items-end">
                                                     <div>
                                                         <x-filament::avatar
@@ -93,10 +114,10 @@ use Assist\InAppCommunication\Models\TwilioConversation;
                                                     </div>
                                                 </div>
                                                 <div
-                                                        class="relative flex w-[calc(100%-50px)] flex-col gap-1 md:gap-3 lg:w-[calc(100%-115px)]">
+                                                    class="relative flex w-[calc(100%-50px)] flex-col gap-1 md:gap-3 lg:w-[calc(100%-115px)]">
                                                     <div class="flex max-w-full flex-grow flex-col gap-3">
                                                         <div
-                                                                class="flex min-h-[20px] flex-col items-start gap-3 overflow-x-auto break-words">
+                                                            class="flex min-h-[20px] flex-col items-start gap-3 overflow-x-auto break-words">
                                                             <div x-text="message.message.body"></div>
                                                         </div>
                                                     </div>
@@ -107,20 +128,26 @@ use Assist\InAppCommunication\Models\TwilioConversation;
                                 </template>
                             </div>
                             <template x-if="messagePaginator?.hasPrevPage">
-                                <div x-on:click="loadPreviousMessages" class="flex justify-center text-center mb-auto p-3 text-primary-500 cursor-pointer bg-white dark:bg-gray-700">
+                                <div
+                                    class="mb-auto flex cursor-pointer justify-center bg-white p-3 text-center text-primary-500 dark:bg-gray-700"
+                                    x-on:click="loadPreviousMessages"
+                                >
                                     <p>Load previous messages...</p>
-                                    <x-filament::loading-indicator x-show="loadingPreviousMessages" class="ml-2 h-4 w-4 text-primary-500" />
+                                    <x-filament::loading-indicator
+                                        class="ml-2 h-4 w-4 text-primary-500"
+                                        x-show="loadingPreviousMessages"
+                                    />
                                 </div>
                             </template>
                         </div>
 
                         <form @submit.prevent="submit">
                             <div
-                                    class="w-full overflow-hidden rounded-xl border border-gray-950/5 bg-gray-50 shadow-sm dark:border-white/10 dark:bg-gray-700">
+                                class="w-full overflow-hidden rounded-xl border border-gray-950/5 bg-gray-50 shadow-sm dark:border-white/10 dark:bg-gray-700">
                                 <div class="bg-white dark:bg-gray-800">
                                     <label
-                                            class="sr-only"
-                                            for="message_input"
+                                        class="sr-only"
+                                        for="message_input"
                                     >Type here</label>
                                     <textarea
                                         class="w-full resize-none border-0 bg-white p-4 text-sm text-gray-900 focus:ring-0 dark:bg-gray-800 dark:text-white dark:placeholder-gray-400"
@@ -138,25 +165,27 @@ use Assist\InAppCommunication\Models\TwilioConversation;
                                         <x-filament::button type="submit">
                                             Post
                                         </x-filament::button>
-                                        <div x-show="usersTyping.length" class="flex relative justify-center h-6 items-center gap-0.5">
+                                        <div
+                                            class="relative flex h-6 items-center justify-center gap-0.5"
+                                            x-show="usersTyping.length"
+                                        >
                                             <template x-for="user in usersTyping">
                                                 <x-filament::avatar
-                                                    size="w-4 h-4"
                                                     alt="User Avatar"
+                                                    size="w-4 h-4"
                                                     x-bind:src="user.avatar"
                                                 />
                                             </template>
-                                            <span class="w-2 h-2 rounded-full bg-primary-500 text-primary-500 animate-bounce animation-delay-100"></span>
-                                            <span class="w-2 h-2 rounded-full bg-primary-500 text-primary-500 animate-bounce animation-delay-200"></span>
-                                            <span class="w-2 h-2 rounded-full bg-primary-500 text-primary-500 animate-bounce animation-delay-300"></span>
+                                            <span
+                                                class="h-2 w-2 animate-bounce rounded-full bg-primary-500 text-primary-500 animation-delay-100"
+                                            ></span>
+                                            <span
+                                                class="h-2 w-2 animate-bounce rounded-full bg-primary-500 text-primary-500 animation-delay-200"
+                                            ></span>
+                                            <span
+                                                class="h-2 w-2 animate-bounce rounded-full bg-primary-500 text-primary-500 animation-delay-300"
+                                            ></span>
                                         </div>
-
-                                        {{--                                <div--}}
-                                        {{--                                        class="py-2"--}}
-                                        {{--                                        wire:loading--}}
-                                        {{--                                >--}}
-                                        {{--                                    <x-filament::loading-indicator class="h-5 w-5 text-primary-500" />--}}
-                                        {{--                                </div>--}}
                                     </div>
                                 </div>
                             </div>
@@ -165,7 +194,7 @@ use Assist\InAppCommunication\Models\TwilioConversation;
                 </div>
             @else
                 <div class="col-span-1 flex h-full flex-col gap-2 overflow-hidden md:col-span-3">
-                    <p class="text-xl text-center">Select or create a new Chat</p>
+                    <p class="text-center text-xl">Select or create a new Chat</p>
                 </div>
             @endif
         </div>
