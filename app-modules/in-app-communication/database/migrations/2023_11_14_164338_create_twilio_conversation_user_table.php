a7103c17bba62ebb0973bc0ae7ab0ef0e466942e
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-11-16 11:48:23 -0500

    Add proper constraints
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- app-modules/in-app-communication/database/migrations/2023_11_14_164338_create_twilio_conversation_user_table.php
+++ app-modules/in-app-communication/database/migrations/2023_11_14_164338_create_twilio_conversation_user_table.php
@@ -12,9 +12,11 @@ return new class () extends Migration {
     {
         Schema::create('twilio_conversation_user', function (Blueprint $table) {
             $table->string('conversation_sid');
-            $table->string('user_id');
+            $table->foreignUuid('user_id');
             $table->string('participant_sid');
             $table->timestamps();
+
+            $table->foreign('conversation_sid')->references('sid')->on('twilio_conversations')->cascadeOnDelete();
         });
     }
 };
