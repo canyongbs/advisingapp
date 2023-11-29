f95b2486a73fa7134443b6e90370b118774e19e2
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-11-29 16:26:50 -0500

    Change footer text
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- resources/views/assist/filament/footer.blade.php
+++ resources/views/assist/filament/footer.blade.php
@@ -48,13 +48,7 @@
 
     <div class="flex w-full justify-center pb-4 pt-2">
         <span class="w-11/12 text-center text-xs lg:w-3/4 xl:w-7/12">
-            © 2023 Canyon GBS LLC. All Rights Reserved. Canyon GBS™, Advanced Student Support & Interaction Servicing
-            Technology™, ASSIST by Canyon GBS™ are trademarks of Canyon GBS LLC. For more information or inquiries,
-            visit
-            our website at <a
-                class="text-blue-600 underline dark:text-blue-400"
-                href="https://canyongbs.com/"
-            >https://canyongbs.com/.</a>
+            © 2016-2023 Canyon GBS LLC. All Rights Reserved. Canyon GBS™ and Advising App™ are trademarks of Canyon GBS LLC. For more information or inquiries, please visit our website at <a class="text-blue-600 underline dark:text-blue-400" href="https://canyongbs.com/">https://canyongbs.com/</a>.
         </span>
     </div>
 
