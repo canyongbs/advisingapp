86c473387d451e07e2a0d19fd610aa8a4f5ae9ac
Dan Harrin <git@danharrin.com>
2023-11-30 19:11:20 +0000

    Update CreateEngagementBatch.php

--- app-modules/engagement/src/Actions/CreateEngagementBatch.php
+++ app-modules/engagement/src/Actions/CreateEngagementBatch.php
@@ -77,7 +77,7 @@ class CreateEngagementBatch implements ShouldQueue
                 'recipient_type' => $record->getMorphClass(),
                 'subject' => $this->data->subject,
                 'body' => $this->data->body,
-                'body_json' => $this->data->body_json,
+                'body_json' => $this->data->bodyJson,
                 'scheduled' => false,
             ]);
 
