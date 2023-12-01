73c57b515bb32ff0d3151ff4c3af53b0ae4390bc
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-12-01 17:11:44 -0500

    Fix linting
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- app/Support/FeatureAccessResponse.php
+++ app/Support/FeatureAccessResponse.php
@@ -8,6 +8,11 @@ class FeatureAccessResponse extends Response
 {
     protected $message = 'Feature Access Denied';
 
+    final public function __construct($allowed, $message = '', $code = null)
+    {
+        parent::__construct($allowed, $message, $code);
+    }
+
     public static function deny($message = null, $code = null): FeatureAccessResponse|static
     {
         return new static(false, $message, $code);
