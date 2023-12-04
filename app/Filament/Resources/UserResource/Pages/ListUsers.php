b552c78000426fb4c7ce346ef05fdc96f9fb25f9
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-12-04 18:34:21 -0500

    Add a label to display the seats used
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- app/Filament/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -36,13 +36,24 @@
 
 namespace App\Filament\Resources\UserResource\Pages;
 
+use App\Models\User;
+use App\Settings\LicenseSettings;
 use Filament\Actions\CreateAction;
 use App\Filament\Resources\UserResource;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Contracts\Support\Htmlable;
 
 class ListUsers extends ListRecords
 {
     protected static string $resource = UserResource::class;
+
+    public function getSubheading(): string | Htmlable | null
+    {
+        $count = User::count();
+        $max = app(LicenseSettings::class)->data->limits->crmSeats;
+
+        return "{$count}/{$max} CRM Seats Used";
+    }
 
     protected function getHeaderActions(): array
     {
