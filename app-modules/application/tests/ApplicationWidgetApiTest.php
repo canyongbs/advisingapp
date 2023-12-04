bb4a0bf20378e1999da5c91150d47a73bae09c0b
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-12-04 15:19:35 -0500

    Feature access test for application.request-authentication
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- app-modules/application/tests/ApplicationWidgetApiTest.php
+++ app-modules/application/tests/ApplicationWidgetApiTest.php
@@ -1,9 +1,11 @@
 <?php
 
 use function Pest\Laravel\get;
+use function Pest\Laravel\post;
 
 use App\Settings\LicenseSettings;
 use Illuminate\Support\Facades\URL;
+use Assist\Prospect\Models\Prospect;
 use Assist\Application\Models\Application;
 
 use function Pest\Laravel\withoutMiddleware;
@@ -32,5 +34,32 @@ test('define is protected with proper feature access control', function () {
     $settings->save();
 
     get(URL::signedRoute('applications.define', ['application' => $application]))
+        ->assertSuccessful();
+});
+
+test('request-authentication is protected with proper feature access control', function () {
+    withoutMiddleware([EnsureSubmissibleIsEmbeddableAndAuthorized::class]);
+
+    $settings = app(LicenseSettings::class);
+
+    $settings->data->addons->onlineAdmissions = false;
+
+    $settings->save();
+
+    $application = Application::factory()->create();
+
+    $prospect = Prospect::factory()->create();
+
+    post(URL::signedRoute('applications.request-authentication', ['application' => $application, 'email' => $prospect->email]))
+        ->assertForbidden()
+        ->assertJson([
+            'error' => 'Online Admissions is not enabled.',
+        ]);
+
+    $settings->data->addons->onlineAdmissions = true;
+
+    $settings->save();
+
+    post(URL::signedRoute('applications.request-authentication', ['application' => $application, 'email' => $prospect->email]))
         ->assertSuccessful();
 });
