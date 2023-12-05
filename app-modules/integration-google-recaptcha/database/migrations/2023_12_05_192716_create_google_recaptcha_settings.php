6ea7f8fe1b0b85c4ec3a87f4a2894623c35a7606
Derek Goetz <derek@kirschbaumdevelopment.com>
2023-12-05 15:02:02 -0500

    Create module for recaptcha integration.

---
+++ app-modules/integration-google-recaptcha/database/migrations/2023_12_05_192716_create_google_recaptcha_settings.php
@@ -0,0 +1,18 @@
+<?php
+
+use Spatie\LaravelSettings\Migrations\SettingsBlueprint;
+use Spatie\LaravelSettings\Migrations\SettingsMigration;
+
+return new class () extends SettingsMigration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $this->migrator->inGroup('google-recaptcha', function (SettingsBlueprint $blueprint): void {
+            $blueprint->add('is_enabled', false);
+            $blueprint->add('site_key');
+            $blueprint->addEncrypted('secret_key');
+        });
+    }
+};
