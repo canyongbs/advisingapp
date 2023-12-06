e1921209afb80e9e5801e4bca09928e6fa891e2f
joelicatajr <joelicatajr@users.noreply.github.com>
2023-12-06 15:53:01 +0000

    chore: fix code style

--- resources/views/crm-seats.blade.php
+++ resources/views/crm-seats.blade.php
@@ -31,20 +31,16 @@
 
 </COPYRIGHT>
 --}}
-@props([
-    'count',
-    'max',
-])
+@props(['count', 'max'])
 
 <?php
-    $colorClass = 'text-green-500';
+$colorClass = 'text-green-500';
 
-    if ($count > $max) {
-        $colorClass = 'text-red-500';
-    } elseif ($count === $max) {
-        $colorClass = 'text-yellow-500';
-    }
+if ($count > $max) {
+    $colorClass = 'text-red-500';
+} elseif ($count === $max) {
+    $colorClass = 'text-yellow-500';
+}
 ?>
-
 
 <span class='{{ $colorClass }}'>{{ $count }}/{{ $max }}</span> CRM Seats Used
