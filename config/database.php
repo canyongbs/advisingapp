8e004843d4ee39c17d3aa868b44e0eb8f6b30cb2
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-12-20 19:13:24 -0500

    Uncluster
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- config/database.php
+++ config/database.php
@@ -173,29 +173,23 @@ return [
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
-        'clusters' => [
-            'default' => [
-                [
-                    'scheme' => env('REDIS_SCHEME', 'tcp'),
-                    'host' => env('REDIS_HOST', '127.0.0.1'),
-                    'username' => env('REDIS_USERNAME'),
-                    'password' => env('REDIS_PASSWORD'),
-                    'port' => env('REDIS_PORT', '6379'),
-                    'database' => env('REDIS_DB', '0'),
-                ],
-            ],
+        'default' => [
+            'scheme' => env('REDIS_SCHEME', 'tcp'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'username' => env('REDIS_USERNAME'),
+            'password' => env('REDIS_PASSWORD'),
+            'port' => env('REDIS_PORT', '6379'),
+            'database' => env('REDIS_DB', '0'),
+        ],
 
-            'cache' => [
-                [
-                    'scheme' => env('REDIS_SCHEME', 'tcp'),
-                    'url' => env('REDIS_URL'),
-                    'host' => env('REDIS_HOST', '127.0.0.1'),
-                    'username' => env('REDIS_USERNAME'),
-                    'password' => env('REDIS_PASSWORD'),
-                    'port' => env('REDIS_PORT', '6379'),
-                    'database' => env('REDIS_CACHE_DB', '1'),
-                ],
-            ],
+        'cache' => [
+            'scheme' => env('REDIS_SCHEME', 'tcp'),
+            'url' => env('REDIS_URL'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'username' => env('REDIS_USERNAME'),
+            'password' => env('REDIS_PASSWORD'),
+            'port' => env('REDIS_PORT', '6379'),
+            'database' => env('REDIS_CACHE_DB', '1'),
         ],
     ],
 
