a08c669a85617c5b9064c055a60561d05efcfd0f
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-12-20 19:02:33 -0500

    Change cache to cluster
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- config/database.php
+++ config/database.php
@@ -184,16 +184,18 @@ return [
                     'database' => env('REDIS_DB', '0'),
                 ],
             ],
-        ],
 
-        'cache' => [
-            'scheme' => env('REDIS_SCHEME', 'tcp'),
-            'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'username' => env('REDIS_USERNAME'),
-            'password' => env('REDIS_PASSWORD'),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_CACHE_DB', '1'),
+            'cache' => [
+                [
+                    'scheme' => env('REDIS_SCHEME', 'tcp'),
+                    'url' => env('REDIS_URL'),
+                    'host' => env('REDIS_HOST', '127.0.0.1'),
+                    'username' => env('REDIS_USERNAME'),
+                    'password' => env('REDIS_PASSWORD'),
+                    'port' => env('REDIS_PORT', '6379'),
+                    'database' => env('REDIS_CACHE_DB', '1'),
+                ],
+            ],
         ],
     ],
 
