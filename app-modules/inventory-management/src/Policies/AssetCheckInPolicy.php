b336e7e2629e1d81d30a5bd4dd15a9ade0ca9630
Derek Goetz <derek@kirschbaumdevelopment.com>
2024-01-03 08:29:11 -0500

    Update policies, roles.

--- app-modules/inventory-management/src/Policies/AssetCheckInPolicy.php
+++ app-modules/inventory-management/src/Policies/AssetCheckInPolicy.php
@@ -38,63 +38,63 @@ namespace AdvisingApp\InventoryManagement\Policies;
 
 use App\Models\Authenticatable;
 use Illuminate\Auth\Access\Response;
-use AdvisingApp\InventoryManagement\Models\AssetExchange;
+use AdvisingApp\InventoryManagement\Models\AssetCheckIn;
 
-class AssetExchangePolicy
+class AssetCheckInPolicy
 {
     public function viewAny(Authenticatable $authenticatable): Response
     {
         return $authenticatable->canOrElse(
-            abilities: 'asset_exchange.view-any',
-            denyResponse: 'You do not have permission to view asset exchanges.'
+            abilities: 'asset_check_in.view-any',
+            denyResponse: 'You do not have permission to view asset check ins.'
         );
     }
 
-    public function view(Authenticatable $authenticatable, AssetExchange $assetExchange): Response
+    public function view(Authenticatable $authenticatable, AssetCheckIn $assetCheckIn): Response
     {
         return $authenticatable->canOrElse(
-            abilities: ['asset_exchange.*.view', "asset_exchange.{$assetExchange->id}.view"],
-            denyResponse: 'You do not have permission to view this asset exchange.'
+            abilities: ['asset_check_in.*.view', "asset_check_in.{$assetCheckIn->id}.view"],
+            denyResponse: 'You do not have permission to view this asset check in.'
         );
     }
 
     public function create(Authenticatable $authenticatable): Response
     {
         return $authenticatable->canOrElse(
-            abilities: 'asset_exchange.create',
-            denyResponse: 'You do not have permission to create asset exchanges.'
+            abilities: 'asset_check_in.create',
+            denyResponse: 'You do not have permission to create asset check ins.'
         );
     }
 
-    public function update(Authenticatable $authenticatable, AssetExchange $assetExchange): Response
+    public function update(Authenticatable $authenticatable, AssetCheckIn $assetCheckIn): Response
     {
         return $authenticatable->canOrElse(
-            abilities: ['asset_exchange.*.update', "asset_exchange.{$assetExchange->id}.update"],
-            denyResponse: 'You do not have permission to update this asset exchange.'
+            abilities: ['asset_check_in.*.update', "asset_check_in.{$assetCheckIn->id}.update"],
+            denyResponse: 'You do not have permission to update this asset check in.'
         );
     }
 
-    public function delete(Authenticatable $authenticatable, AssetExchange $assetExchange): Response
+    public function delete(Authenticatable $authenticatable, AssetCheckIn $assetCheckIn): Response
     {
         return $authenticatable->canOrElse(
-            abilities: ['asset_exchange.*.delete', "asset_exchange.{$assetExchange->id}.delete"],
-            denyResponse: 'You do not have permission to delete this asset exchange.'
+            abilities: ['asset_check_in.*.delete', "asset_check_in.{$assetCheckIn->id}.delete"],
+            denyResponse: 'You do not have permission to delete this asset check in.'
         );
     }
 
-    public function restore(Authenticatable $authenticatable, AssetExchange $assetExchange): Response
+    public function restore(Authenticatable $authenticatable, AssetCheckIn $assetCheckIn): Response
     {
         return $authenticatable->canOrElse(
-            abilities: ['asset_exchange.*.restore', "asset_exchange.{$assetExchange->id}.restore"],
-            denyResponse: 'You do not have permission to restore this asset exchange.'
+            abilities: ['asset_check_in.*.restore', "asset_check_in.{$assetCheckIn->id}.restore"],
+            denyResponse: 'You do not have permission to restore this asset check in.'
         );
     }
 
-    public function forceDelete(Authenticatable $authenticatable, AssetExchange $assetExchange): Response
+    public function forceDelete(Authenticatable $authenticatable, AssetCheckIn $assetCheckIn): Response
     {
         return $authenticatable->canOrElse(
-            abilities: ['asset_exchange.*.force-delete', "asset_exchange.{$assetExchange->id}.force-delete"],
-            denyResponse: 'You do not have permission to permanently delete this asset exchange.'
+            abilities: ['asset_check_in.*.force-delete', "asset_check_in.{$assetCheckIn->id}.force-delete"],
+            denyResponse: 'You do not have permission to permanently delete this asset check in.'
         );
     }
 }
