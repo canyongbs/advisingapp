f79cf945b6f98edaa2ae3ed2caccf5a7c9f90640
Kevin Ullyott <ullyott.kevin@gmail.com>
2024-01-08 10:53:21 -0500

    Return channels directly
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- app-modules/notification/src/Notifications/BaseNotification.php
+++ app-modules/notification/src/Notifications/BaseNotification.php
@@ -64,7 +64,7 @@ abstract class BaseNotification extends Notification implements ShouldQueue
     {
         $traits = collect(class_uses_recursive(static::class));
 
-        $channels = $traits
+        return $traits
             ->filter(function ($traitName) {
                 return in_array(ChannelTrait::class, class_uses($traitName));
             })
@@ -82,8 +82,6 @@ abstract class BaseNotification extends Notification implements ShouldQueue
             ->unique()
             ->values()
             ->toArray();
-
-        return $channels;
     }
 
     public function beforeSend(object $notifiable, string $channel): OutboundDeliverable|false
