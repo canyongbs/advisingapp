3b4ec9bf1d6e1adbc6086fa757a25544d7df5f7a
Dan Harrin <git@danharrin.com>
2024-01-11 13:19:48 +0000

    [ADVAPP-184]: Unlicensed user should see a message on the dashboard that indicates they do not have an active license

---
+++ app-modules/authorization/resources/views/filament/widgets/unlicensed-notice.blade.php
@@ -0,0 +1,16 @@
+<x-filament-widgets::widget>
+    <x-filament::section class="!bg-warning-50 dark:!bg-gray-900">
+        <div class="flex gap-3">
+            <div class="flex-shrink-0">
+                <x-heroicon-o-exclamation-triangle
+                    class="h-5 w-5 text-warning-400"
+                    aria-hidden="true"
+                />
+            </div>
+
+            <h3 class="text-sm font-medium text-warning-800 dark:text-warning-200">
+                Your account currently does not have a license to access features in Advising App.
+            </h3>
+        </div>
+    </x-filament::section>
+</x-filament-widgets::widget>
---
+++ app-modules/authorization/src/Filament/Widgets/UnlicensedNotice.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AdvisingApp\Authorization\Filament\Widgets;
+
+use Filament\Widgets\Widget;
+use App\Models\Authenticatable;
+use AdvisingApp\Authorization\Enums\LicenseType;
+
+class UnlicensedNotice extends Widget
+{
+    protected static string $view = 'authorization::filament.widgets.unlicensed-notice';
+
+    protected int | string | array $columnSpan = 'full';
+
+    public static function canView(): bool
+    {
+        /** @var Authenticatable $user */
+        $user = auth()->user();
+
+        return ! $user->hasAnyLicense(LicenseType::cases());
+    }
+}
