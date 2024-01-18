4126fe702b5d0d7162f32196310241f5f3d41245
Kevin Ullyott <ullyott.kevin@gmail.com>
2024-01-18 11:02:38 -0500

    Fix google calendar settings migration
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- app-modules/meeting-center/database/migrations/2024_01_18_152802_create_google_calendar_settings.php
+++ app-modules/meeting-center/database/migrations/2024_01_18_152802_create_google_calendar_settings.php
@@ -6,7 +6,7 @@ use Spatie\LaravelSettings\Migrations\SettingsMigration;
 return new class () extends SettingsMigration {
     public function up(): void
     {
-        $this->migrator->inGroup('azure_calendar', function (SettingsBlueprint $blueprint): void {
+        $this->migrator->inGroup('google_calendar', function (SettingsBlueprint $blueprint): void {
             $blueprint->add('is_enabled', false);
             $blueprint->addEncrypted('client_id');
             $blueprint->addEncrypted('client_secret');
