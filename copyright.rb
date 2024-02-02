98560b1b2071e0cbd24a23ce6e752780c5545735
Matthew Myers <mxm1070@users.noreply.github.com>
2024-02-02 18:55:22 -0500

    Update copyright script start year.

--- copyright.rb
+++ copyright.rb
@@ -4,7 +4,7 @@ YEAR = Time.now.year;
 
 COPYRIGHT = <<EOM
 
-    Copyright © 2022-#{YEAR}, Canyon GBS LLC. All rights reserved.
+    Copyright © 2016-#{YEAR}, Canyon GBS LLC. All rights reserved.
 
     Advising App™ is licensed under the Elastic License 2.0. For more details,
     see https://github.com/canyongbs/advisingapp/blob/main/LICENSE.
