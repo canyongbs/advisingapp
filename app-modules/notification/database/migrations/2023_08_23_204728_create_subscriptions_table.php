051a785235042691fd7da88343b89a2d6db9949c
Dan Harrin <git@danharrin.com>
2024-02-12 13:19:09 +0000

    Clean up migrations

--- app-modules/notification/database/migrations/2023_08_23_204728_create_subscriptions_table.php
+++ app-modules/notification/database/migrations/2023_08_23_204728_create_subscriptions_table.php
@@ -39,9 +39,6 @@ use Tpetry\PostgresqlEnhanced\Schema\Blueprint;
 use Tpetry\PostgresqlEnhanced\Support\Facades\Schema;
 
 return new class () extends Migration {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('subscriptions', function (Blueprint $table) {
