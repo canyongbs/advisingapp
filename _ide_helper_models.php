a7559e8b954e76613a4029adb19b6ce69f134b77
Matthew Myers <mxm1070@users.noreply.github.com>
2024-02-13 14:17:15 -0500

    Regen helpers.

--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -1,39 +1,5 @@
 <?php
 
-/*
-<COPYRIGHT>
-
-    Copyright © 2016-2024, Canyon GBS LLC. All rights reserved.
-
-    Advising App™ is licensed under the Elastic License 2.0. For more details,
-    see https://github.com/canyongbs/advisingapp/blob/main/LICENSE.
-
-    Notice:
-
-    - You may not provide the software to third parties as a hosted or managed
-      service, where the service provides users with access to any substantial set of
-      the features or functionality of the software.
-    - You may not move, change, disable, or circumvent the license key functionality
-      in the software, and you may not remove or obscure any functionality in the
-      software that is protected by the license key.
-    - You may not alter, remove, or obscure any licensing, copyright, or other notices
-      of the licensor in the software. Any use of the licensor’s trademarks is subject
-      to applicable law.
-    - Canyon GBS LLC respects the intellectual property rights of others and expects the
-      same in return. Canyon GBS™ and Advising App™ are registered trademarks of
-      Canyon GBS LLC, and we are committed to enforcing and protecting our trademarks
-      vigorously.
-    - The software solution, including services, infrastructure, and code, is offered as a
-      Software as a Service (SaaS) by Canyon GBS LLC.
-    - Use of this software implies agreement to the license terms and conditions as stated
-      in the Elastic License 2.0.
-
-    For more information or inquiries please visit our website at
-    https://www.canyongbs.com or contact us via email at legal@canyongbs.com.
-
-</COPYRIGHT>
-*/
-
 // @formatter:off
 // phpcs:ignoreFile
 /**
@@ -2706,6 +2672,7 @@ namespace AdvisingApp\KnowledgeBase\Models{
  * @property string $id
  * @property string $name
  * @property string|null $description
+ * @property string|null $icon
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property \Illuminate\Support\Carbon|null $deleted_at
@@ -2721,6 +2688,7 @@ namespace AdvisingApp\KnowledgeBase\Models{
  * @method static \Illuminate\Database\Eloquent\Builder|KnowledgeBaseCategory whereCreatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|KnowledgeBaseCategory whereDeletedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|KnowledgeBaseCategory whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|KnowledgeBaseCategory whereIcon($value)
  * @method static \Illuminate\Database\Eloquent\Builder|KnowledgeBaseCategory whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|KnowledgeBaseCategory whereName($value)
  * @method static \Illuminate\Database\Eloquent\Builder|KnowledgeBaseCategory whereUpdatedAt($value)
@@ -3538,7 +3506,6 @@ namespace AdvisingApp\ServiceManagement\Models{
  * @property string $id
  * @property string $name
  * @property \AdvisingApp\ServiceManagement\Enums\SystemChangeRequestClassification $classification
- * @property string|null $archived_at
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property \Illuminate\Support\Carbon|null $deleted_at
@@ -3546,14 +3513,11 @@ namespace AdvisingApp\ServiceManagement\Models{
  * @property-read int|null $audits_count
  * @property-read \Illuminate\Database\Eloquent\Collection<int, \AdvisingApp\ServiceManagement\Models\ChangeRequest> $changeRequests
  * @property-read int|null $change_requests_count
- * @method static \Illuminate\Database\Eloquent\Builder|ChangeRequestStatus archived()
  * @method static \AdvisingApp\ServiceManagement\Database\Factories\ChangeRequestStatusFactory factory($count = null, $state = [])
  * @method static \Illuminate\Database\Eloquent\Builder|ChangeRequestStatus newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|ChangeRequestStatus newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|ChangeRequestStatus onlyTrashed()
  * @method static \Illuminate\Database\Eloquent\Builder|ChangeRequestStatus query()
- * @method static \Illuminate\Database\Eloquent\Builder|ChangeRequestStatus unarchived()
- * @method static \Illuminate\Database\Eloquent\Builder|ChangeRequestStatus whereArchivedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|ChangeRequestStatus whereClassification($value)
  * @method static \Illuminate\Database\Eloquent\Builder|ChangeRequestStatus whereCreatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|ChangeRequestStatus whereDeletedAt($value)
@@ -3575,7 +3539,6 @@ namespace AdvisingApp\ServiceManagement\Models{
  * @property string $id
  * @property string $name
  * @property int $number_of_required_approvals
- * @property string|null $archived_at
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property \Illuminate\Support\Carbon|null $deleted_at
@@ -3585,14 +3548,11 @@ namespace AdvisingApp\ServiceManagement\Models{
  * @property-read int|null $change_requests_count
  * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\User> $userApprovers
  * @property-read int|null $user_approvers_count
- * @method static \Illuminate\Database\Eloquent\Builder|ChangeRequestType archived()
  * @method static \AdvisingApp\ServiceManagement\Database\Factories\ChangeRequestTypeFactory factory($count = null, $state = [])
  * @method static \Illuminate\Database\Eloquent\Builder|ChangeRequestType newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|ChangeRequestType newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|ChangeRequestType onlyTrashed()
  * @method static \Illuminate\Database\Eloquent\Builder|ChangeRequestType query()
- * @method static \Illuminate\Database\Eloquent\Builder|ChangeRequestType unarchived()
- * @method static \Illuminate\Database\Eloquent\Builder|ChangeRequestType whereArchivedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|ChangeRequestType whereCreatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|ChangeRequestType whereDeletedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|ChangeRequestType whereId($value)
@@ -3981,7 +3941,6 @@ namespace AdvisingApp\ServiceManagement\Models{
  * @property \AdvisingApp\ServiceManagement\Enums\SystemServiceRequestClassification $classification
  * @property string $name
  * @property \AdvisingApp\ServiceManagement\Enums\ColumnColorOptions $color
- * @property string|null $archived_at
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property \Illuminate\Support\Carbon|null $deleted_at
@@ -3989,14 +3948,11 @@ namespace AdvisingApp\ServiceManagement\Models{
  * @property-read int|null $audits_count
  * @property-read \Illuminate\Database\Eloquent\Collection<int, \AdvisingApp\ServiceManagement\Models\ServiceRequest> $serviceRequests
  * @property-read int|null $service_requests_count
- * @method static \Illuminate\Database\Eloquent\Builder|ServiceRequestStatus archived()
  * @method static \AdvisingApp\ServiceManagement\Database\Factories\ServiceRequestStatusFactory factory($count = null, $state = [])
  * @method static \Illuminate\Database\Eloquent\Builder|ServiceRequestStatus newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|ServiceRequestStatus newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|ServiceRequestStatus onlyTrashed()
  * @method static \Illuminate\Database\Eloquent\Builder|ServiceRequestStatus query()
- * @method static \Illuminate\Database\Eloquent\Builder|ServiceRequestStatus unarchived()
- * @method static \Illuminate\Database\Eloquent\Builder|ServiceRequestStatus whereArchivedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|ServiceRequestStatus whereClassification($value)
  * @method static \Illuminate\Database\Eloquent\Builder|ServiceRequestStatus whereColor($value)
  * @method static \Illuminate\Database\Eloquent\Builder|ServiceRequestStatus whereCreatedAt($value)
@@ -4021,7 +3977,6 @@ namespace AdvisingApp\ServiceManagement\Models{
  * @property bool $has_enabled_feedback_collection
  * @property bool $has_enabled_csat
  * @property bool $has_enabled_nps
- * @property string|null $archived_at
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property \Illuminate\Support\Carbon|null $deleted_at
@@ -4032,14 +3987,11 @@ namespace AdvisingApp\ServiceManagement\Models{
  * @property-read int|null $priorities_count
  * @property-read \Illuminate\Database\Eloquent\Collection<int, \AdvisingApp\ServiceManagement\Models\ServiceRequest> $serviceRequests
  * @property-read int|null $service_requests_count
- * @method static \Illuminate\Database\Eloquent\Builder|ServiceRequestType archived()
  * @method static \AdvisingApp\ServiceManagement\Database\Factories\ServiceRequestTypeFactory factory($count = null, $state = [])
  * @method static \Illuminate\Database\Eloquent\Builder|ServiceRequestType newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|ServiceRequestType newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|ServiceRequestType onlyTrashed()
  * @method static \Illuminate\Database\Eloquent\Builder|ServiceRequestType query()
- * @method static \Illuminate\Database\Eloquent\Builder|ServiceRequestType unarchived()
- * @method static \Illuminate\Database\Eloquent\Builder|ServiceRequestType whereArchivedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|ServiceRequestType whereCreatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|ServiceRequestType whereDeletedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|ServiceRequestType whereHasEnabledCsat($value)
