28e0f927bdbff31f74d0fec573e3988826248fb6
joelicatajr <joelicatajr@users.noreply.github.com>
2024-02-16 13:34:59 +0000

    chore: fix code style

--- app-modules/report/resources/views/filament/resources/reports/pages/edit-report.blade.php
+++ app-modules/report/resources/views/filament/resources/reports/pages/edit-report.blade.php
@@ -39,11 +39,11 @@
     @capture($form)
     <x-filament-panels::form
     :wire:key="$this->getId() . '.forms.' . $this->getFormStatePath()" wire:submit="save">
-                        {{ $this->form }}
+                            {{ $this->form }}
 
-                        <x-filament-panels::form.actions
+                            <x-filament-panels::form.actions
     :actions="$this->getCachedFormActions()" :full-width="$this->hasFullWidthFormActions()"/>
-                    </x-filament-panels::form>
+                        </x-filament-panels::form>
 @endcapture
 
     @php
