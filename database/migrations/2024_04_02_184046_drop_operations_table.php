3b515c7ed838bc173c634596071c89d2cda49852
Kevin Ullyott <kevin@kirschbaumdevelopment.com>
2024-04-02 14:46:34 -0400

    Restore migrations and create drop migrations
    Signed-off-by: Kevin Ullyott <kevin@kirschbaumdevelopment.com>

---
+++ database/migrations/2024_04_02_184046_drop_operations_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class () extends Migration {
+    public function up(): void
+    {
+        Schema::dropIfExists('operations');
+    }
+
+    public function down(): void
+    {
+        Schema::create('operations', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name')->unique();
+            $table->string('dispatched');
+            $table->timestamp('processed_at')->nullable();
+            $table->timestamp('completed_at')->nullable();
+        });
+    }
+};
