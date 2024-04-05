f5f35131c38ffaab0b2a49249085ebe8cbdf7c74
Kevin Ullyott <kevin@kirschbaumdevelopment.com>
2024-04-05 17:45:25 -0400

    Add the feature activation migration
    Signed-off-by: Kevin Ullyott <kevin@kirschbaumdevelopment.com>

---
+++ database/migrations/2024_04_05_214332_data_active_new_enrollments_columns_feature.php
@@ -0,0 +1,16 @@
+<?php
+
+use Laravel\Pennant\Feature;
+use Illuminate\Database\Migrations\Migration;
+
+return new class () extends Migration {
+    public function up(): void
+    {
+        Feature::activate('new-enrollments-columns');
+    }
+
+    public function down(): void
+    {
+        Feature::deactivate('new-enrollments-columns');
+    }
+};
