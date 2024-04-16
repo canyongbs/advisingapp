2b872bcabf799db8516fe9d8e4120420c584b8bd
dgoetzit <dgoetzit@users.noreply.github.com>
2024-04-16 00:46:21 +0000

    chore: fix enforcement of copyright on all files

--- portals/knowledge-management/src/Services/Consumer.js
+++ portals/knowledge-management/src/Services/Consumer.js
@@ -3,8 +3,8 @@
 
     Copyright © 2016-2024, Canyon GBS LLC. All rights reserved.
 
-    Aiding App™ is licensed under the Elastic License 2.0. For more details,
-    see <https://github.com/canyongbs/aidingapp/blob/main/LICENSE.>
+    Advising App™ is licensed under the Elastic License 2.0. For more details,
+    see https://github.com/canyongbs/advisingapp/blob/main/LICENSE.
 
     Notice:
 
@@ -18,7 +18,7 @@
       of the licensor in the software. Any use of the licensor’s trademarks is subject
       to applicable law.
     - Canyon GBS LLC respects the intellectual property rights of others and expects the
-      same in return. Canyon GBS™ and Aiding App™ are registered trademarks of
+      same in return. Canyon GBS™ and Advising App™ are registered trademarks of
       Canyon GBS LLC, and we are committed to enforcing and protecting our trademarks
       vigorously.
     - The software solution, including services, infrastructure, and code, is offered as a
@@ -27,7 +27,7 @@
       in the Elastic License 2.0.
 
     For more information or inquiries please visit our website at
-    <https://www.canyongbs.com> or contact us via email at legal@canyongbs.com.
+    https://www.canyongbs.com or contact us via email at legal@canyongbs.com.
 
 </COPYRIGHT>
 */
