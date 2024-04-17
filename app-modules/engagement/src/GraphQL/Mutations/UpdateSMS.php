07713574d29c1d67cf49c25591d1e87193013e83
Matthew Myers <mxm1070@users.noreply.github.com>
2024-04-16 21:34:37 -0400

    wip

--- app-modules/engagement/src/GraphQL/Mutations/UpdateSMS.php
+++ app-modules/engagement/src/GraphQL/Mutations/UpdateSMS.php
@@ -2,11 +2,9 @@
 
 namespace AdvisingApp\Engagement\GraphQL\Mutations;
 
-use Illuminate\Database\Eloquent\Model;
 use Nuwave\Lighthouse\Execution\ResolveInfo;
 use AdvisingApp\Engagement\Models\Engagement;
 use AdvisingApp\StudentDataModel\Models\Student;
-use Illuminate\Database\Eloquent\Relations\Relation;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 use AdvisingApp\Engagement\Actions\GenerateTipTapBodyJson;
 use AdvisingApp\Engagement\Enums\EngagementDeliveryMethod;
@@ -16,11 +14,9 @@ class UpdateSMS
 {
     public function __invoke(mixed $root, array $args, GraphQLContext $context, ResolveInfo $resolveInfo): Engagement
     {
-        /** @var Model $morph */
-        $morph = Relation::getMorphedModel($args['recipient_type']);
-        $recipient = $morph::find($args['recipient_id']);
+        $engagement = Engagement::findOrFail($args['id']);
 
-        $mergeTags = match ($recipient::class) {
+        $mergeTags = match ($engagement->recipient::class) {
             Student::class => [
                 '{{ student full name }}',
                 '{{ student email }}',
@@ -29,9 +25,7 @@ class UpdateSMS
         };
 
         $args['body'] = app(GenerateTipTapBodyJson::class)(body: $args['body'], mergeData: $mergeTags);
-
-        $engagement = Engagement::findOrFail($args['id']);
-        $engagement->fill($args);
+        $engagement->update($args);
 
         $engagement->deliverable->delete();
         app(CreateEngagementDeliverable::class)(engagement: $engagement, deliveryMethod: EngagementDeliveryMethod::Sms->value);
