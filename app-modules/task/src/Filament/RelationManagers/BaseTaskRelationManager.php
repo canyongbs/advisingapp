8745fe9ecd35f7c718dffff5073dfb35939578e5
ankit-canyon <ankit.kabra@canyongbs.com>
2024-04-26 11:58:45 +0000

    Added DissociateBulkAction Changes

--- app-modules/task/src/Filament/RelationManagers/BaseTaskRelationManager.php
+++ app-modules/task/src/Filament/RelationManagers/BaseTaskRelationManager.php
@@ -54,10 +54,11 @@ use Filament\Tables\Actions\CreateAction;
 use Filament\Tables\Filters\SelectFilter;
 use Illuminate\Database\Eloquent\Builder;
 use Filament\Tables\Actions\BulkActionGroup;
+use Illuminate\Database\Eloquent\Collection;
 use Filament\Forms\Components\DateTimePicker;
-use Filament\Tables\Actions\DetachBulkAction;
 use Filament\Tables\Actions\DissociateAction;
 use AdvisingApp\StudentDataModel\Models\Student;
+use Filament\Tables\Actions\DissociateBulkAction;
 use Filament\Resources\Pages\ManageRelatedRecords;
 use AdvisingApp\Prospect\Filament\Resources\ProspectResource;
 use AdvisingApp\StudentDataModel\Filament\Resources\StudentResource;
@@ -172,7 +173,12 @@ abstract class BaseTaskRelationManager extends ManageRelatedRecords
             ->recordUrl(null)
             ->bulkActions([
                 BulkActionGroup::make([
-                    DetachBulkAction::make(),
+                    DissociateBulkAction::make()
+                        ->using(function (Collection $selectedRecords) {
+                            $selectedRecords->each(
+                                fn (Task $selectedRecord) => $selectedRecord->concern()->dissociate()->save()
+                            );
+                        }),
                 ]),
             ]);
     }
