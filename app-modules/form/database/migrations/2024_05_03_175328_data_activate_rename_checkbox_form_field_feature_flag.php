a4db654c48601d7c288c8a496bdbfbc8c3d0af89
Matthew Myers <mxm1070@users.noreply.github.com>
2024-05-03 15:37:53 -0400

    fix

--- app-modules/form/database/migrations/2024_05_03_175328_data_activate_rename_checkbox_form_field_feature_flag.php
+++ app-modules/form/database/migrations/2024_05_03_175328_data_activate_rename_checkbox_form_field_feature_flag.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Migrations\Migration;
 return new class () extends Migration {
     public function up(): void
     {
-        Feature::active('rename-checkbox-form-field');
+        Feature::activate('rename-checkbox-form-field');
     }
 
     public function down(): void
