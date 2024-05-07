04d9c0e1eaccaf3cde329c2f3ace0186868cc37e
Kevin Ullyott <kevin@kirschbaumdevelopment.com>
2024-05-07 11:10:56 -0700

    Add new s3 permission
    Signed-off-by: Kevin Ullyott <kevin@kirschbaumdevelopment.com>

--- database/migrations/2024_05_07_143413_seed_permissions_add_manage_s3_settings.php
+++ database/migrations/2024_05_07_143413_seed_permissions_add_manage_s3_settings.php
@@ -1,19 +1,41 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Migrations\Migration;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     public function up(): void
     {
-        DB::table('permissions')->insert('test');
+        $groupId = (string) Str::orderedUuid();
+
+        DB::table('permission_groups')
+            ->insert(
+                [
+                    'id' => $groupId,
+                    'name' => 'Amazon S3',
+                    'created_at' => now(),
+                ]
+            );
+
+        DB::table('permissions')->insert(
+            [
+                'id' => (string) Str::orderedUuid(),
+                'group_id' => $groupId,
+                'guard_name' => 'web',
+                'name' => 'amazon-s3.manage_s3_settings',
+                'created_at' => now(),
+            ]
+        );
     }
 
     public function down(): void
     {
-        //
+        DB::table('permissions')
+            ->where('name', 'amazon-s3.manage_s3_settings')
+            ->delete();
+
+        DB::table('permission_groups')
+            ->where('name', 'Amazon S3')
+            ->delete();
     }
 };
