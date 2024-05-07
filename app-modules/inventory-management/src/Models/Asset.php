ae41530a54da28f723dd06d12975a26d73fb87eb
joelicatajr <joelicatajr@users.noreply.github.com>
2024-05-07 19:29:31 +0000

    chore: fix code style

--- app-modules/inventory-management/src/Models/Asset.php
+++ app-modules/inventory-management/src/Models/Asset.php
@@ -50,6 +50,7 @@ use AdvisingApp\InventoryManagement\Enums\SystemAssetStatusClassification;
 
 /**
  * @property-read string $purchase_age
+ *
  * @mixin IdeHelperAsset
  */
 class Asset extends BaseModel implements Auditable
