423364ac6ac4eee7f7bc0ac819ecdabbb41d180e
Dan Harrin <git@danharrin.com>
2024-05-22 12:29:54 +0100

    Fix remaining test

--- app-modules/ai/src/Policies/LegacyAiMessageLogPolicy.php
+++ app-modules/ai/src/Policies/LegacyAiMessageLogPolicy.php
@@ -36,15 +36,12 @@
 
 namespace AdvisingApp\Ai\Policies;
 
-use App\Enums\Feature;
 use App\Models\Authenticatable;
 use Illuminate\Auth\Access\Response;
-use Illuminate\Support\Facades\Gate;
-use AdvisingApp\Ai\Models\AiAssistant;
 use App\Concerns\PerformsLicenseChecks;
 use AdvisingApp\Authorization\Enums\LicenseType;
 
-class AiAssistantPolicy
+class LegacyAiMessageLogPolicy
 {
     use PerformsLicenseChecks;
 
@@ -59,52 +56,9 @@ class AiAssistantPolicy
 
     public function viewAny(Authenticatable $authenticatable): Response
     {
-        if (! Gate::check(Feature::CustomAiAssistants->getGateName())) {
-            return Response::deny('AI Assistants are not enabled.');
-        }
-
         return $authenticatable->canOrElse(
-            abilities: 'ai_assistant.view-any',
-            denyResponse: 'You do not have permission to view AI Assistants.'
+            abilities: 'assistant_chat_message_log.view-any',
+            denyResponse: 'You do not have permission to view AI message logs.'
         );
-    }
-
-    public function view(Authenticatable $authenticatable, AiAssistant $aiAssistant): Response
-    {
-        return $authenticatable->canOrElse(
-            abilities: ['ai_assistant.*.view', "ai_assistant.{$aiAssistant->id}.view"],
-            denyResponse: 'You do not have permission to view this AI Assistant.'
-        );
-    }
-
-    public function create(Authenticatable $authenticatable): Response
-    {
-        return $authenticatable->canOrElse(
-            abilities: 'ai_assistant.create',
-            denyResponse: 'You do not have permission to create AI Assistants.'
-        );
-    }
-
-    public function update(Authenticatable $authenticatable, AiAssistant $aiAssistant): Response
-    {
-        return $authenticatable->canOrElse(
-            abilities: ['ai_assistant.*.update', "ai_assistant.{$aiAssistant->id}.update"],
-            denyResponse: 'You do not have permission to update this AI Assistant.'
-        );
-    }
-
-    public function delete(Authenticatable $authenticatable, AiAssistant $aiAssistant): Response
-    {
-        return Response::deny('AI Assistants cannot be deleted.');
-    }
-
-    public function restore(Authenticatable $authenticatable, AiAssistant $aiAssistant): Response
-    {
-        return Response::deny('AI Assistants cannot be restored.');
-    }
-
-    public function forceDelete(Authenticatable $authenticatable, AiAssistant $aiAssistant): Response
-    {
-        return Response::deny('AI Assistants cannot be permanently deleted.');
     }
 }
