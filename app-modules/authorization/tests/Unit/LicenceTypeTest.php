ce8045709b1053125c5b03bf765ae49369bb86c7
default <kevin.ullyott@canyongbs.com>
2024-05-24 18:35:28 +0000

    Test

--- app-modules/authorization/tests/Unit/LicenceTypeTest.php
+++ app-modules/authorization/tests/Unit/LicenceTypeTest.php
@@ -83,6 +83,8 @@ test('Users with a Super Admin role are not counted in the available seats', fun
 
     assertEquals(0, $licenseType->getSeatsInUse());
     assertEquals($totalSeats, $licenseType->getAvailableSeats());
+
+    // This is a test
 })
     ->with([
         LicenseType::ConversationalAi,
