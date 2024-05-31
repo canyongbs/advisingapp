6af9bcca7e208a630bb48f473cfe98a0d3313043
default <ankit.kabra@canyongbs.com>
2024-05-31 09:43:03 +0000

    [ADVAPP-611] Regression: Mobile view fails to display correctly for AI Assistant

--- app-modules/ai/resources/views/components/assistant.blade.php
+++ app-modules/ai/resources/views/components/assistant.blade.php
@@ -417,33 +417,30 @@
                                 required
                             ></textarea>
                         </div>
-                        <div class="flex items-center justify-between border-t px-3 py-2 dark:border-gray-600">
-                            <div class="flex w-full items-center gap-3">
-                                <x-filament::button type="submit">
+                        <div class="flex flex-col items-center border-t px-3 py-2 dark:border-gray-600 sm:flex-row sm:justify-between">
+                            <div class="w-full flex flex-col gap-3 sm:flex-row sm:w-auto sm:items-center">
+                                <x-filament::button type="submit" class="w-full sm:w-auto">
                                     Send
                                 </x-filament::button>
-
+                        
                                 {{ $this->insertFromPromptLibraryAction }}
-
-                                <div
-                                    class="py-2"
-                                    x-show="isSendingMessage"
-                                >
+                        
+                                <div class="py-2" x-show="isSendingMessage">
                                     <x-filament::loading-indicator class="h-5 w-5 text-primary-500" />
                                 </div>
                             </div>
-
+                        
                             @if (blank($this->thread->name))
-                                <div class="flex pl-0 sm:pl-2">
+                                <div class="flex w-full justify-center pt-3 sm:pl-2 sm:pt-0 sm:w-auto">
                                     {{ $this->saveThreadAction }}
                                 </div>
                             @else
-                                <div class="flex gap-3">
+                                <div class="flex w-full justify-center gap-1.5 pt-3 sm:w-auto sm:pt-0">
                                     {{ ($this->cloneThreadAction)(['thread' => $this->thread->id]) }}
                                     {{ ($this->emailThreadAction)(['thread' => $this->thread->id]) }}
                                 </div>
                             @endif
-                        </div>
+                        </div>                        
                     </div>
                 </form>
             </div>
