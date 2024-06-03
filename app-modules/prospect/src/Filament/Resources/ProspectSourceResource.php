45f515d34d038eb9e238f7d56e1a67567bc39dca
ketan-canyon <ketan-canyon@users.noreply.github.com>
2024-06-03 20:31:05 +0000

    chore: fix code style

--- app-modules/prospect/src/Filament/Resources/ProspectSourceResource.php
+++ app-modules/prospect/src/Filament/Resources/ProspectSourceResource.php
@@ -38,11 +38,11 @@ namespace AdvisingApp\Prospect\Filament\Resources;
 
 use Filament\Resources\Resource;
 use AdvisingApp\Prospect\Models\ProspectSource;
+use App\Filament\Clusters\ConstituentManagement;
 use AdvisingApp\Prospect\Filament\Resources\ProspectSourceResource\Pages\EditProspectSource;
 use AdvisingApp\Prospect\Filament\Resources\ProspectSourceResource\Pages\ViewProspectSource;
 use AdvisingApp\Prospect\Filament\Resources\ProspectSourceResource\Pages\ListProspectSources;
 use AdvisingApp\Prospect\Filament\Resources\ProspectSourceResource\Pages\CreateProspectSource;
-use App\Filament\Clusters\ConstituentManagement;
 
 class ProspectSourceResource extends Resource
 {
