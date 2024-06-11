8430f7bb8c50677791c1eb05480f95f0923d034f
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2024-06-11 20:29:50 +0000

    Create a command to create permission migrations
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

---
+++ app/Overrides/Laravel/PermissionMigrationCreator.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Overrides\Laravel;
+
+use Illuminate\Database\Migrations\MigrationCreator;
+
+class PermissionMigrationCreator extends MigrationCreator
+{
+    protected function getStub($table, $create)
+    {
+        return $this->files->get(
+            $this->files->exists($customPath = $this->customStubPath . '/permission-migration.stub')
+                            ? $customPath
+                            : $this->stubPath() . '/migration.stub'
+        );
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -50,6 +50,7 @@ use Laravel\Octane\Commands\ReloadCommand;
 use Filament\Actions\Imports\Jobs\ImportCsv;
 use Filament\Actions\Exports\Jobs\PrepareCsvExport;
 use Illuminate\Database\Eloquent\Relations\Relation;
+use App\Overrides\Laravel\PermissionMigrationCreator;
 use OpenSearch\Migrations\Filesystem\MigrationStorage;
 use App\Overrides\Filament\Actions\Imports\Jobs\ImportCsvOverride;
 use App\Overrides\Filament\Actions\Imports\Jobs\PrepareCsvExportOverride;
@@ -106,6 +107,10 @@ class AppServiceProvider extends ServiceProvider
             configureScope(function (Scope $scope): void {
                 $scope->removeUser();
             });
+        });
+
+        $this->app->singleton(PermissionMigrationCreator::class, function ($app) {
+            return new PermissionMigrationCreator($app['files'], $app->basePath('stubs'));
         });
     }
 }
