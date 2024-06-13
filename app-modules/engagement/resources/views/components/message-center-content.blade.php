22f2da46dc9d8487613f8b5486c5d0f042f584f7
jayushi-canyon <jayushi-canyon@users.noreply.github.com>
2024-06-13 11:45:23 +0000

    chore: fix code style

--- app-modules/engagement/resources/views/components/message-center-content.blade.php
+++ app-modules/engagement/resources/views/components/message-center-content.blade.php
@@ -48,7 +48,7 @@
                 <div
                     class="sticky top-0 z-[5] flex h-12 w-full items-center justify-between bg-gray-100 px-4 dark:bg-gray-700">
                     <h1 class="ml-2"> <a
-                        href="{{ $educatable->filamentResource()::getUrl('view', ['record' => $educatable->identifier()]) }}"
+                            href="{{ $educatable->filamentResource()::getUrl('view', ['record' => $educatable->identifier()]) }}"
                         >{{ $educatable->display_name }} </a></h1>
                     <x-filament::button wire:click="engage('{{ $educatable }}')">
                         Engage
