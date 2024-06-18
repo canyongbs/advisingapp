0e77743f941b513553b44445f1b7624fb1605916
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2024-06-18 23:13:16 +0000

    Fix the recovery codes copy
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- app-modules/multifactor-authentication/resources/views/filament/actions/recovery-codes-modal.blade.php
+++ app-modules/multifactor-authentication/resources/views/filament/actions/recovery-codes-modal.blade.php
@@ -6,14 +6,43 @@
     @endforeach
     </div>
     <div class="inline-block text-xs">
-        <a
+        {{-- <span
             x-data="{}"
             :x-on:click.prevent="window.navigator.clipboard.writeText(@js($codes->join(',')));$tooltip('Copied!');"
-            href="#"
-            class="flex items-center"
+            class="flex items-center cursor-pointer"
         >
             @svg('heroicon-s-clipboard-document', 'w-4 mr-2')
             <span class="">Copy to clipboard</span>
-        </a>
+        </span> --}}
+        <div
+            class="visible mt-2 flex justify-center gap-2 self-end text-gray-400 md:gap-3"
+            x-data="{
+                messageCopied: false,
+                copyMessage: function() {
+                    navigator.clipboard.writeText(@js($codes->join(',')))
+            
+                    this.messageCopied = true
+            
+                    setTimeout(() => { this.messageCopied = false }, 2000)
+                }
+            }"
+        >
+            <span
+                x-on:click="copyMessage"
+                class="flex items-center cursor-pointer text-xs hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-200 disabled:dark:hover:text-gray-400 p-1 rounded-md"
+            >
+                <x-filament::icon
+                    class="ml-auto flex h-6 w-6 cursor-pointer items-center gap-2"
+                    icon="heroicon-o-clipboard-document-check"
+                    x-show="messageCopied"
+                />
+                <x-filament::icon
+                    class="ml-auto flex h-6 w-6 cursor-pointer items-center gap-2"
+                    icon="heroicon-o-clipboard"
+                    x-show="! messageCopied"
+                />
+                <span class="">Copy to clipboard</span>
+            </span>
+        </div>
     </div>
 </div>
