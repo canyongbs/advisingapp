26ee4b2a1a2a6d1346828606b1a5735d85c56452
payal-canyon <payal.baldaniya@canyongbs.com>
2024-06-19 08:54:11 +0000

    [ADVAPP-663]: Enhance the order of Reporting navigation in the application

--- app-modules/report/src/Filament/Pages/ReportLibrary.php
+++ app-modules/report/src/Filament/Pages/ReportLibrary.php
@@ -46,7 +46,7 @@ class ReportLibrary extends Page
 
     protected static ?string $navigationGroup = 'Reporting';
 
-    protected static ?int $navigationSort = 50;
+    protected static ?int $navigationSort = 10;
 
     public static function canAccess(): bool
     {
