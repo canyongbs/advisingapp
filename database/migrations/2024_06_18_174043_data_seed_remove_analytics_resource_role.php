38769accc4a40e00a21a4f627cbd1c62951cb2e3
ketan-canyon <ketan.barevadiya@canyongbs.com>
2024-06-19 12:20:52 +0000

    change migration as required

--- database/migrations/2024_06_18_174043_data_seed_remove_analytics_resource_role.php
+++ database/migrations/2024_06_18_174043_data_seed_remove_analytics_resource_role.php
@@ -7,23 +7,33 @@ use Illuminate\Database\Migrations\Migration;
 return new class () extends Migration {
     public function up(): void
     {
-        $role_details = DB::table('roles')->where('name', 'analytics.analytics_management')->first();
+        $role_details = DB::table('roles')->where('name', 'analytics.analytics_management')->whereIn('guard_name', ['web', 'api'])->get();
 
         if (! empty($role_details)) {
-            DB::table('model_has_roles')->where('role_id', $role_details->id)->delete();
-            DB::table('role_has_permissions')->where('role_id', $role_details->id)->delete();
-            DB::table('roles')->where('name', 'analytics.analytics_management')->delete();
+            foreach ($role_details as $role) {
+                DB::table('model_has_roles')->where('role_id', $role->id)->delete();
+                DB::table('role_has_permissions')->where('role_id', $role->id)->delete();
+                DB::table('roles')->where('name', $role->name)->delete();
+            }
         }
     }
 
     public function down(): void
     {
-        $analytics_role = [
-            'id' => (string) Str::orderedUuid(),
-            'name' => 'analytics.analytics_management',
-            'guard_name' => 'web',
-            'created_at' => now(),
+        $analytics_roles = [
+            [
+                'id' => (string) Str::orderedUuid(),
+                'name' => 'analytics.analytics_management',
+                'guard_name' => 'web',
+                'created_at' => now(),
+            ],
+            [
+                'id' => (string) Str::orderedUuid(),
+                'name' => 'analytics.analytics_management',
+                'guard_name' => 'api',
+                'created_at' => now(),
+            ]
         ];
-        DB::table('roles')->insert($analytics_role);
+        DB::table('roles')->insert($analytics_roles);
     }
 };
