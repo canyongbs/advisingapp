d16782c43b63580b272059be7b47eff895b56199
Orrison <Orrison@users.noreply.github.com>
2024-07-09 20:48:52 +0000

    chore: fix code style

--- database/migrations/2024_06_25_174131_seed_permissions_remove_caseload_permissions.php
+++ database/migrations/2024_06_25_174131_seed_permissions_remove_caseload_permissions.php
@@ -35,8 +35,6 @@
 */
 
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 use Database\Migrations\Concerns\CanModifyPermissions;
 
