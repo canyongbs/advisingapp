4187528881ffcf3c57a21d60353189654f500cf9
Dan Harrin <git@danharrin.com>
2024-07-09 17:26:57 +0100

    Update OpenAiGptTestServiceTest.php

--- app-modules/integration-open-ai/tests/Feature/Services/OpenAiGptTestServiceTest.php
+++ app-modules/integration-open-ai/tests/Feature/Services/OpenAiGptTestServiceTest.php
@@ -456,7 +456,7 @@ it('can complete a prompt', function () {
     ]);
 
     $aiSettings = app(AiSettings::class);
-    $aiSettings->temperature = $temperature = rand(0, 10) / 10;
+    $aiSettings->temperature = $temperature = random_int(1, 10) / 10;
     $aiSettings->save();
 
     expect($service->complete($prompt = Str::random(), $content = Str::random()))
