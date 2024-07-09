fa56d188b976794793c829750a0fafb02c0e49a2
jayushi-canyon <jayushi.jain@canyongbs.com>
2024-07-09 11:22:48 +0000

    create prospect overview report

---
+++ app-modules/prospect/src/Filament/Pages/ProspectReport.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace AdvisingApp\Prospect\Filament\Pages;
+
+use AdvisingApp\Report\Filament\Widgets\ProspectReportStats;
+use AdvisingApp\Report\Filament\Widgets\RefreshWidget;
+use Filament\Pages\Dashboard;
+use Filament\Pages\Page;
+
+class ProspectReport extends Dashboard
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static ?string $navigationGroup = 'Recruitment CRM';
+
+    protected static string $routePath = 'prospect-report';
+
+    protected static ?string $title = 'Prospects (Overview)';
+
+    // protected static string $view = 'advising-prospect.filament.pages.prospect-report';
+
+    protected $cacheTag = 'prospect-report-cache';
+
+    public function getWidgets(): array
+    {
+        return [
+            RefreshWidget::make(['cacheTag' => $this->cacheTag]),
+            ProspectReportStats::make(['cacheTag' => $this->cacheTag]),
+        ];
+    }
+
+    public function getColumns(): int | string | array
+    {
+        return [
+            'sm' => 2,
+            'md' => 4,
+            'lg' => 4,
+        ];
+    }
+}
---
+++ app-modules/prospect/src/Filament/Widgets/ProspectReportStats.php
@@ -0,0 +1,92 @@
+<?php
+
+/*
+<COPYRIGHT>
+
+    Copyright © 2016-2024, Canyon GBS LLC. All rights reserved.
+
+    Advising App™ is licensed under the Elastic License 2.0. For more details,
+    see https://github.com/canyongbs/advisingapp/blob/main/LICENSE.
+
+    Notice:
+
+    - You may not provide the software to third parties as a hosted or managed
+      service, where the service provides users with access to any substantial set of
+      the features or functionality of the software.
+    - You may not move, change, disable, or circumvent the license key functionality
+      in the software, and you may not remove or obscure any functionality in the
+      software that is protected by the license key.
+    - You may not alter, remove, or obscure any licensing, copyright, or other notices
+      of the licensor in the software. Any use of the licensor’s trademarks is subject
+      to applicable law.
+    - Canyon GBS LLC respects the intellectual property rights of others and expects the
+      same in return. Canyon GBS™ and Advising App™ are registered trademarks of
+      Canyon GBS LLC, and we are committed to enforcing and protecting our trademarks
+      vigorously.
+    - The software solution, including services, infrastructure, and code, is offered as a
+      Software as a Service (SaaS) by Canyon GBS LLC.
+    - Use of this software implies agreement to the license terms and conditions as stated
+      in the Elastic License 2.0.
+
+    For more information or inquiries please visit our website at
+    https://www.canyongbs.com or contact us via email at legal@canyongbs.com.
+
+</COPYRIGHT>
+*/
+
+namespace AdvisingApp\Report\Filament\Widgets;
+
+use Illuminate\Support\Number;
+use AdvisingApp\Ai\Models\PromptUse;
+use Illuminate\Support\Facades\Cache;
+use AdvisingApp\Ai\Models\PromptUpvote;
+use AdvisingApp\Alert\Models\Alert;
+use AdvisingApp\Authorization\Models\License;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use AdvisingApp\Authorization\Enums\LicenseType;
+use AdvisingApp\Engagement\Models\EngagementDeliverable;
+use AdvisingApp\Prospect\Models\Prospect;
+use AdvisingApp\Task\Models\Task;
+
+class ProspectReportStats extends StatsOverviewReportWidget
+{
+    protected int | string | array $columnSpan = [
+        'sm' => 2,
+        'md' => 4,
+        'lg' => 4,
+    ];
+
+    protected function getStats(): array
+    {
+        return [
+            Stat::make('Total Prospects', Number::abbreviate(
+                Cache::tags([$this->cacheTag])->remember('prospects-count', now()->addHours(24), function (): int {
+                    return Prospect::count();
+                }),
+                maxPrecision: 2,
+            )),
+            Stat::make('Total Alerts', Number::abbreviate(
+                Cache::tags([$this->cacheTag])->remember('alert-count', now()->addHours(24), function (): int {
+                    return Alert::whereHasMorph('concern', Prospect::class)->count();
+                }),
+                maxPrecision: 2,
+            )),
+            Stat::make('Total Segment', Number::abbreviate(
+                Cache::tags([$this->cacheTag])->remember('prompts-insertions-count', now()->addHours(24), function (): int {
+                    return EngagementDeliverable::whereHas('engagement', function ($q) {
+                        return $q->whereHasMorph('recipient', Prospect::class);
+                    })
+                        ->where('delivery_status', 'successful')
+                        ->count();
+                }),
+                maxPrecision: 2,
+            )),
+            Stat::make('Total Tasks', Number::abbreviate(
+                Cache::tags([$this->cacheTag])->remember('alert-count', now()->addHours(24), function (): int {
+                    return Task::whereHasMorph('concern', Prospect::class)->count();
+                }),
+                maxPrecision: 2,
+            )),
+        ];
+    }
+}
