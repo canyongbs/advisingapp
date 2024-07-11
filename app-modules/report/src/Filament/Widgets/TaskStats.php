c8d41e8de8a066377d298f2c1399e9b69e1ea3a1
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2024-07-11 22:15:43 +0000

    Fix TaskStats
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- app-modules/report/src/Filament/Widgets/TaskStats.php
+++ app-modules/report/src/Filament/Widgets/TaskStats.php
@@ -37,18 +37,14 @@
 namespace AdvisingApp\Report\Filament\Widgets;
 
 use Illuminate\Support\Number;
-use AdvisingApp\Ai\Models\PromptUse;
 use Illuminate\Support\Facades\Cache;
-use AdvisingApp\Ai\Models\PromptUpvote;
-use AdvisingApp\Authorization\Models\License;
 use Filament\Widgets\StatsOverviewWidget\Stat;
-use AdvisingApp\Authorization\Enums\LicenseType;
 use AdvisingApp\Prospect\Models\Prospect;
 use AdvisingApp\StudentDataModel\Models\Student;
 use AdvisingApp\Task\Enums\TaskStatus;
 use AdvisingApp\Task\Models\Task;
 use App\Models\User;
-use Illuminate\Database\Query\Builder;
+use Illuminate\Database\Eloquent\Builder;
 
 class TaskStats extends StatsOverviewReportWidget
 {
