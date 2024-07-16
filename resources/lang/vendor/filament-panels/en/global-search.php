df6097feca098141b68f9b65cc37b644585b8fad
jayushi-canyon <jayushi.jain@canyongbs.com>
2024-07-16 14:39:22 +0000

    Change global search placeholder

---
+++ resources/lang/vendor/filament-panels/en/global-search.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'field' => [
+        'label' => 'Global search',
+        'placeholder' => 'Global Search',
+    ],
+
+    'no_results_message' => 'No search results found.',
+
+];
