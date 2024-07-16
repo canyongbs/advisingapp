3004df32a08b853ccfdc7986fa13b2a8928d7f4b
ankit-canyon <ankit.kabra@canyongbs.com>
2024-07-16 16:33:09 +0000

    fix headings

--- resources/views/livewire/noWidgetData.blade.php
+++ resources/views/livewire/noWidgetData.blade.php
@@ -1,7 +1,17 @@
+@php
+    use Filament\Support\Facades\FilamentView;
+
+    $color = $this->getColor();
+    $heading = $this->getHeading();
+@endphp
 <x-filament-widgets::widget class="fi-wi-chart">
-    <x-filament::section class="h-full flex items-center justify-center">
-        <div>
+    <x-filament::section class="h-full relative pt-20" :heading="$heading">
+        <div class="lg:absolute inset-0 flex items-center justify-center">
             Insufficient Data
         </div>
     </x-filament::section>
 </x-filament-widgets::widget>
+
+
+
+
