08aa82ce690b04c401228df56603904fda847022
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2024-07-17 19:40:05 +0000

    Create AiMessageFileDeleted
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

---
+++ app-modules/ai/src/Events/AiMessageFileDeleted.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AdvisingApp\Ai\Events;
+
+use AdvisingApp\Ai\Models\AiMessageFile;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+
+class AiMessageFileDeleted
+{
+    use Dispatchable;
+    use InteractsWithSockets;
+
+    public function __construct(AiMessageFile $aiMessageFile) {}
+}
