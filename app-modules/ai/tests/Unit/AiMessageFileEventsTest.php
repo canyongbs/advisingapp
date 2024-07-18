a381d27263240fc3ecde73f9b7dad39b5f045478
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2024-07-18 00:26:53 +0000

    Finis hthe AiMessageFileEventsTest
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- app-modules/ai/tests/Unit/AiMessageFileEventsTest.php
+++ app-modules/ai/tests/Unit/AiMessageFileEventsTest.php
@@ -1,8 +1,8 @@
 <?php
 
-use AdvisingApp\Ai\Events\AiMessageFileDeleted;
-use AdvisingApp\Ai\Models\AiMessageFile;
 use Illuminate\Support\Facades\Event;
+use AdvisingApp\Ai\Models\AiMessageFile;
+use AdvisingApp\Ai\Events\AiMessageFileDeleted;
 
 it('dispatches the AiMessageFileDeleted event when an AiMessageFile is deleted', function () {
     $aiMessageFile = AiMessageFile::factory()->create();
@@ -10,6 +10,18 @@ it('dispatches the AiMessageFileDeleted event when an AiMessageFile is deleted',
     Event::fake();
 
     $aiMessageFile->delete();
+
+    Event::assertDispatched(AiMessageFileDeleted::class, function ($event) use ($aiMessageFile) {
+        return $event->aiMessageFile->is($aiMessageFile);
+    });
+});
+
+it('dispatches the AiMessageFileDeleted event when an AiMessageFile is force deleted', function () {
+    $aiMessageFile = AiMessageFile::factory()->create();
+
+    Event::fake();
+
+    $aiMessageFile->forceDelete();
 
     Event::assertDispatched(AiMessageFileDeleted::class, function ($event) use ($aiMessageFile) {
         return $event->aiMessageFile->is($aiMessageFile);
