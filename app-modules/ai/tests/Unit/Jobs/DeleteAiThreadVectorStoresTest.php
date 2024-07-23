96434a2318066e58ad542f631a8631757d91d83a
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2024-07-23 06:01:55 +0000

    Test for DeleteAiThreadVectorStores
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

---
+++ app-modules/ai/tests/Unit/Jobs/DeleteAiThreadVectorStoresTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use AdvisingApp\Ai\Enums\AiModel;
+use Mockery\MockInterface;
+use AdvisingApp\Ai\Models\AiThread;
+use AdvisingApp\Ai\Models\AiAssistant;
+use AdvisingApp\Ai\Jobs\DeleteAiThreadVectorStores;
+use AdvisingApp\IntegrationOpenAi\DataTransferObjects\Threads\ThreadsDataTransferObject;
+
+it('deletes vector stores for a thread', function () {
+    $aiThread = AiThread::factory()
+        ->for(
+            factory: AiAssistant::factory()->state(['model' => AiModel::OpenAiGpt4o]),
+            relationship: 'assistant',
+        )
+        ->create();
+
+    /** @phpstan-ignore-next-line */
+    $this->mock(
+        $aiThread->assistant->model->getService()::class,
+        fn (MockInterface $mock) => $mock
+            ->shouldReceive('supportsMessageFileUploads')->once()->andReturn(true)
+            ->shouldReceive('retrieveThread')->once()->andReturn(ThreadsDataTransferObject::from([
+                'id' => 1,
+                'vectorStoreIds' => [1, 2, 3],
+            ]))
+            ->shouldReceive('deleteVectorStore')->times(3),
+    );
+
+    (new DeleteAiThreadVectorStores($aiThread))->handle();
+});
