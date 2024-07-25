0041c96b6c15868dc2c06560af47cedd608f7ad7
jayushi-canyon <jayushi-canyon@users.noreply.github.com>
2024-07-25 12:02:04 +0000

    chore: fix code style

--- app-modules/report/src/Filament/Pages/ArtificialIntelligence.php
+++ app-modules/report/src/Filament/Pages/ArtificialIntelligence.php
@@ -47,48 +47,48 @@ use AdvisingApp\Report\Filament\Widgets\PromptsByCategoryDoughnutChart;
 
 class ArtificialIntelligence extends Dashboard
 {
-  protected static ?string $navigationIcon = 'heroicon-o-document-text';
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
-  protected static ?string $cluster = ReportLibrary::class;
+    protected static ?string $cluster = ReportLibrary::class;
 
-  protected static ?string $navigationGroup = 'Artificial Intelligence';
+    protected static ?string $navigationGroup = 'Artificial Intelligence';
 
-  protected static ?string $navigationLabel = 'Artificial Intelligence';
+    protected static ?string $navigationLabel = 'Artificial Intelligence';
 
-  protected static ?string $title = 'Artificial Intelligence';
+    protected static ?string $title = 'Artificial Intelligence';
 
-  protected static string $routePath = 'artificial-intelligence';
+    protected static string $routePath = 'artificial-intelligence';
 
-  protected static ?int $navigationSort = 3;
+    protected static ?int $navigationSort = 3;
 
-  protected $cacheTag = 'report-artificial-intelligence';
+    protected $cacheTag = 'report-artificial-intelligence';
 
-  public static function canAccess(): bool
-  {
-    /** @var User $user */
-    $user = auth()->user();
+    public static function canAccess(): bool
+    {
+        /** @var User $user */
+        $user = auth()->user();
 
-    return $user->can('report-library.view-any');
-  }
+        return $user->can('report-library.view-any');
+    }
 
-  public function getWidgets(): array
-  {
-    return [
-      RefreshWidget::make(['cacheTag' => $this->cacheTag]),
-      AiStats::make(['cacheTag' => $this->cacheTag]),
-      SavedConversationsLineChart::make(['cacheTag' => $this->cacheTag]),
-      SpecialActionsDoughnutChart::make(['cacheTag' => $this->cacheTag]),
-      PromptsByCategoryDoughnutChart::make(['cacheTag' => $this->cacheTag]),
-      PromptsCreatedLineChart::make(['cacheTag' => $this->cacheTag]),
-    ];
-  }
+    public function getWidgets(): array
+    {
+        return [
+            RefreshWidget::make(['cacheTag' => $this->cacheTag]),
+            AiStats::make(['cacheTag' => $this->cacheTag]),
+            SavedConversationsLineChart::make(['cacheTag' => $this->cacheTag]),
+            SpecialActionsDoughnutChart::make(['cacheTag' => $this->cacheTag]),
+            PromptsByCategoryDoughnutChart::make(['cacheTag' => $this->cacheTag]),
+            PromptsCreatedLineChart::make(['cacheTag' => $this->cacheTag]),
+        ];
+    }
 
-  public function getColumns(): int | string | array
-  {
-    return [
-      'sm' => 12,
-      'md' => 12,
-      'lg' => 12,
-    ];
-  }
+    public function getColumns(): int | string | array
+    {
+        return [
+            'sm' => 12,
+            'md' => 12,
+            'lg' => 12,
+        ];
+    }
 }
