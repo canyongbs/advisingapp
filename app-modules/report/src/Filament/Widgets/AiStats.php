273d48fdb070a589c4276a6ff9289a663388cd5d
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2024-08-02 20:38:02 +0000

    Change AI Users to Licensed Users in AI report
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- app-modules/report/src/Filament/Widgets/AiStats.php
+++ app-modules/report/src/Filament/Widgets/AiStats.php
@@ -40,7 +40,6 @@ use Illuminate\Support\Number;
 use AdvisingApp\Ai\Models\PromptUse;
 use Illuminate\Support\Facades\Cache;
 use AdvisingApp\Ai\Models\PromptUpvote;
-use AdvisingApp\Authorization\Models\License;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use AdvisingApp\Authorization\Enums\LicenseType;
 
@@ -51,9 +50,9 @@ class AiStats extends StatsOverviewReportWidget
     protected function getStats(): array
     {
         return [
-            Stat::make('AI Users', Number::abbreviate(
-                Cache::tags([$this->cacheTag])->remember('ai-users-count', now()->addHours(24), function (): int {
-                    return License::where('type', LicenseType::ConversationalAi)->count();
+            Stat::make('Licensed Users', Number::abbreviate(
+                Cache::tags([$this->cacheTag])->remember('licensed-ai-users-count', now()->addHours(24), function (): int {
+                    return LicenseType::ConversationalAi->getSeatsInUse();
                 }),
                 maxPrecision: 2,
             )),
