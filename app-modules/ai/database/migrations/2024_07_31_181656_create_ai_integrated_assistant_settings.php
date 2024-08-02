7a4a9a6b42d3e8b6e2821d3c155ff1d79b63b410
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2024-08-02 19:07:27 +0000

    Update the migration to not reference classes outside of the approved
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- app-modules/ai/database/migrations/2024_07_31_181656_create_ai_integrated_assistant_settings.php
+++ app-modules/ai/database/migrations/2024_07_31_181656_create_ai_integrated_assistant_settings.php
@@ -40,7 +40,7 @@ use Spatie\LaravelSettings\Migrations\SettingsMigration;
 return new class () extends SettingsMigration {
     public function up(): void
     {
-        $this->migrator->add('ai-integrated-assistant.default_model', AiModel::OpenAiGpt4o);
+        $this->migrator->add('ai-integrated-assistant.default_model', 'openai_gpt_4o');
     }
 
     public function down(): void
