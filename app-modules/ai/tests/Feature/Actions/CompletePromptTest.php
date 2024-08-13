4a043b9689e047442e7f8dd1e3146f6aa3708372
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2024-08-13 21:44:22 +0000

    Test that CompletePromptTest makes the proper log
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- app-modules/ai/tests/Feature/Actions/CompletePromptTest.php
+++ app-modules/ai/tests/Feature/Actions/CompletePromptTest.php
@@ -1,11 +1,17 @@
 <?php
 
 use Mockery\MockInterface;
+
+use function Tests\asSuperAdmin;
+
 use AdvisingApp\Ai\Enums\AiModel;
+use AdvisingApp\Ai\Enums\AiFeature;
 use AdvisingApp\Ai\Actions\CompletePrompt;
 use AdvisingApp\Ai\Services\TestAiService;
 
-use function Tests\asSuperAdmin;
+use function Pest\Laravel\assertDatabaseHas;
+
+use AdvisingApp\Ai\Models\LegacyAiMessageLog;
 
 it('calls the passed AI Models service class complete method', function () {
     asSuperAdmin();
@@ -27,4 +33,38 @@ it('calls the passed AI Models service class complete method', function () {
     $completion = app(CompletePrompt::class)->execute($aiModel, $prompt, $content);
 
     expect($completion)->toBe('test-completion');
+});
+
+it('creates a new AI message log', function () {
+    asSuperAdmin();
+
+    $aiModel = AiModel::Test;
+    $prompt = 'test-prompt';
+    $content = 'test-content';
+
+    expect(LegacyAiMessageLog::count())->toBe(0);
+
+    /** @phpstan-ignore-next-line */
+    $this->mock(
+        TestAiService::class,
+        fn (MockInterface $mock) => $mock
+            ->shouldReceive('complete')
+            ->once()
+            ->with($prompt, $content)
+            ->andReturn('test-completion'),
+    );
+
+    $completion = app(CompletePrompt::class)->execute($aiModel, $prompt, $content);
+
+    expect(LegacyAiMessageLog::count())->toBe(1);
+
+    expect(LegacyAiMessageLog::first())
+        ->message->toBe($content)
+        ->metadata->toBe([
+            'prompt' => $prompt,
+            'completion' => 'test-completion',
+        ])
+        ->user_id->toBe(auth()->id())
+        ->ai_assistant_name->toBe('Institutional Assistant')
+        ->feature->toBe(AiFeature::DraftWithAi);
 });
