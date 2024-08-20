2ca72c49d32e0462e320f810ee3222856ce1dd0d
joelicatajr <joelicatajr@users.noreply.github.com>
2024-08-20 13:36:43 +0000

    chore: fix code style

--- resources/views/filament/resources/user-resource/mfa-badge.blade.php
+++ resources/views/filament/resources/user-resource/mfa-badge.blade.php
@@ -34,24 +34,24 @@
 @if (!$this->getRecord()->is_external)
     @if ($this->getRecord()->hasConfirmedMultifactor())
         <x-filament::badge
-            tooltip="MFA Enabled"
             data-identifier="mfa-enabled"
+            tooltip="MFA Enabled"
             color="success"
         >
             {{ __('MFA Enabled') }}
         </x-filament::badge>
     @elseif($this->getRecord()->hasEnabledMultifactor())
         <x-filament::badge
-            tooltip="MFA Enabled | Not Confirmed"
             data-identifier="mfa-not-confirmed"
+            tooltip="MFA Enabled | Not Confirmed"
             color="warning"
         >
             {{ __('MFA Enabled | Not Confirmed') }}
         </x-filament::badge>
     @else
         <x-filament::badge
-            tooltip="MFA Disabled"
             data-identifier="mfa-disabled"
+            tooltip="MFA Disabled"
             color="gray"
         >
             {{ __('MFA Disabled') }}
