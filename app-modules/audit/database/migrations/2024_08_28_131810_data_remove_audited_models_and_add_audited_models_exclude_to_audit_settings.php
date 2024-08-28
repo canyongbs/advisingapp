31b3fc73a45df2ddd3bb139248a330f0c1fb6098
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2024-08-28 16:08:26 +0000

    Fix the migration to handle if the default already filled the value
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- app-modules/audit/database/migrations/2024_08_28_131810_data_remove_audited_models_and_add_audited_models_exclude_to_audit_settings.php
+++ app-modules/audit/database/migrations/2024_08_28_131810_data_remove_audited_models_and_add_audited_models_exclude_to_audit_settings.php
@@ -1,11 +1,17 @@
 <?php
 
 use Spatie\LaravelSettings\Migrations\SettingsMigration;
+use Spatie\LaravelSettings\Exceptions\SettingAlreadyExists;
 
 return new class () extends SettingsMigration {
     public function up(): void
     {
-        $this->migrator->delete('audit.audited_models');
-        $this->migrator->add('audit.audited_models_exclude', []);
+        $this->migrator->deleteIfExists('audit.audited_models');
+
+        try {
+            $this->migrator->add('audit.audited_models_exclude', []);
+        } catch (SettingAlreadyExists $exception) {
+            // Ignore
+        }
     }
 };
