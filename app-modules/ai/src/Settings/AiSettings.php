b0ab20ea9d83c97bc64dde3328ea9679c479ed61
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2024-09-09 14:35:18 +0000

    Remove additional duplicate imports
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- app-modules/ai/src/Settings/AiSettings.php
+++ app-modules/ai/src/Settings/AiSettings.php
@@ -39,7 +39,6 @@ namespace AdvisingApp\Ai\Settings;
 use AdvisingApp\Ai\Enums\AiModel;
 use Spatie\LaravelSettings\Settings;
 use AdvisingApp\Ai\Enums\AiMaxTokens;
-use AdvisingApp\Ai\Enums\AiMaxTokens;
 
 class AiSettings extends Settings
 {
