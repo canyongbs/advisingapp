f389a93b906c4025a2310fcf84633dff7f340766
Dan Harrin <git@danharrin.com>
2024-09-11 21:53:45 +0100

    [ADVAPP-820]: Introduce a "branding bar" for institutions to integrate their brand name and brand color into the product experience for end users. (#1008)
    * [ADVAPP-820]: Introduce a "branding bar" for institutions to integrate their brand name and brand color into the product experience for end users.
    
    * chore: fix enforcement of copyright on all files
    
    * Update index.blade.php
    
    ---------
    
    Co-authored-by: danharrin <danharrin@users.noreply.github.com>

---
+++ resources/views/vendor/filament-panels/components/sidebar/index.blade.php
@@ -0,0 +1,214 @@
+{{--
+<COPYRIGHT>
+
+    Copyright © 2016-2024, Canyon GBS LLC. All rights reserved.
+
+    Advising App™ is licensed under the Elastic License 2.0. For more details,
+    see https://github.com/canyongbs/advisingapp/blob/main/LICENSE.
+
+    Notice:
+
+    - You may not provide the software to third parties as a hosted or managed
+      service, where the service provides users with access to any substantial set of
+      the features or functionality of the software.
+    - You may not move, change, disable, or circumvent the license key functionality
+      in the software, and you may not remove or obscure any functionality in the
+      software that is protected by the license key.
+    - You may not alter, remove, or obscure any licensing, copyright, or other notices
+      of the licensor in the software. Any use of the licensor’s trademarks is subject
+      to applicable law.
+    - Canyon GBS LLC respects the intellectual property rights of others and expects the
+      same in return. Canyon GBS™ and Advising App™ are registered trademarks of
+      Canyon GBS LLC, and we are committed to enforcing and protecting our trademarks
+      vigorously.
+    - The software solution, including services, infrastructure, and code, is offered as a
+      Software as a Service (SaaS) by Canyon GBS LLC.
+    - Use of this software implies agreement to the license terms and conditions as stated
+      in the Elastic License 2.0.
+
+    For more information or inquiries please visit our website at
+    https://www.canyongbs.com or contact us via email at legal@canyongbs.com.
+
+</COPYRIGHT>
+--}}
+@props([
+    'navigation',
+    'hasBrandingBar' => false,
+])
+
+@php
+    $openSidebarClasses = 'fi-sidebar-open w-[--sidebar-width] translate-x-0 shadow-xl ring-1 ring-gray-950/5 dark:ring-white/10 rtl:-translate-x-0';
+    $isRtl = __('filament-panels::layout.direction') === 'rtl';
+@endphp
+
+{{-- format-ignore-start --}}
+<aside
+    x-data="{}"
+    @if (filament()->isSidebarCollapsibleOnDesktop() && (! filament()->hasTopNavigation()))
+        x-cloak
+        x-bind:class="
+            $store.sidebar.isOpen
+                ? @js($openSidebarClasses . ' ' . 'lg:sticky')
+                : '-translate-x-full rtl:translate-x-full lg:sticky lg:translate-x-0 rtl:lg:-translate-x-0'
+        "
+    @else
+        @if (filament()->hasTopNavigation())
+            x-cloak
+            x-bind:class="$store.sidebar.isOpen ? @js($openSidebarClasses) : '-translate-x-full rtl:translate-x-full'"
+        @elseif (filament()->isSidebarFullyCollapsibleOnDesktop())
+            x-cloak
+            x-bind:class="$store.sidebar.isOpen ? @js($openSidebarClasses . ' ' . 'lg:sticky') : '-translate-x-full rtl:translate-x-full'"
+        @else
+            x-cloak="-lg"
+            x-bind:class="
+                $store.sidebar.isOpen
+                    ? @js($openSidebarClasses . ' ' . 'lg:sticky')
+                    : 'w-[--sidebar-width] -translate-x-full rtl:translate-x-full lg:sticky'
+            "
+        @endif
+    @endif
+    {{
+        $attributes->class([
+            'fi-sidebar fixed inset-y-0 start-0 z-30 flex flex-col h-screen content-start bg-white transition-all dark:bg-gray-900 lg:z-0 lg:bg-transparent lg:shadow-none lg:ring-0 lg:transition-none dark:lg:bg-transparent',
+            'lg:h-[calc(100dvh-4rem)] lg:top-[4rem]' => ! $hasBrandingBar,
+            'lg:h-[calc(100dvh-6.5rem)] lg:top-[6.5rem]' => $hasBrandingBar,
+            'lg:translate-x-0 rtl:lg:-translate-x-0' => ! (filament()->isSidebarCollapsibleOnDesktop() || filament()->isSidebarFullyCollapsibleOnDesktop() || filament()->hasTopNavigation()),
+            'lg:-translate-x-full rtl:lg:translate-x-full' => filament()->hasTopNavigation(),
+        ])
+    }}
+>
+    <div class="overflow-x-clip lg:hidden">
+        <header
+            class="fi-sidebar-header flex h-16 items-center bg-white px-6 ring-1 ring-gray-950/5 dark:bg-gray-900 dark:ring-white/10 lg:shadow-sm"
+        >
+            <div
+                @if (filament()->isSidebarCollapsibleOnDesktop())
+                    x-show="$store.sidebar.isOpen"
+                    x-transition:enter="lg:transition lg:delay-100"
+                    x-transition:enter-start="opacity-0"
+                    x-transition:enter-end="opacity-100"
+                @endif
+            >
+                @if ($homeUrl = filament()->getHomeUrl())
+                    <a {{ \Filament\Support\generate_href_html($homeUrl) }}>
+                        <x-filament-panels::logo />
+                    </a>
+                @else
+                    <x-filament-panels::logo />
+                @endif
+            </div>
+
+            @if (filament()->isSidebarCollapsibleOnDesktop())
+                <x-filament::icon-button
+                    color="gray"
+                    :icon="$isRtl ? 'heroicon-o-chevron-left' : 'heroicon-o-chevron-right'"
+                    {{-- @deprecated Use `panels::sidebar.expand-button.rtl` instead of `panels::sidebar.expand-button` for RTL. --}}
+                    :icon-alias="$isRtl ? ['panels::sidebar.expand-button.rtl', 'panels::sidebar.expand-button'] : 'panels::sidebar.expand-button'"
+                    icon-size="lg"
+                    :label="__('filament-panels::layout.actions.sidebar.expand.label')"
+                    x-cloak
+                    x-data="{}"
+                    x-on:click="$store.sidebar.open()"
+                    x-show="! $store.sidebar.isOpen"
+                    class="mx-auto"
+                />
+            @endif
+
+            @if (filament()->isSidebarCollapsibleOnDesktop() || filament()->isSidebarFullyCollapsibleOnDesktop())
+                <x-filament::icon-button
+                    color="gray"
+                    :icon="$isRtl ? 'heroicon-o-chevron-right' : 'heroicon-o-chevron-left'"
+                    {{-- @deprecated Use `panels::sidebar.collapse-button.rtl` instead of `panels::sidebar.collapse-button` for RTL. --}}
+                    :icon-alias="$isRtl ? ['panels::sidebar.collapse-button.rtl', 'panels::sidebar.collapse-button'] : 'panels::sidebar.collapse-button'"
+                    icon-size="lg"
+                    :label="__('filament-panels::layout.actions.sidebar.collapse.label')"
+                    x-cloak
+                    x-data="{}"
+                    x-on:click="$store.sidebar.close()"
+                    x-show="$store.sidebar.isOpen"
+                    class="ms-auto hidden lg:flex"
+                />
+            @endif
+        </header>
+    </div>
+
+    <nav
+        class="fi-sidebar-nav flex-grow flex flex-col gap-y-7 overflow-y-auto overflow-x-hidden px-6 py-8"
+        style="scrollbar-gutter: stable"
+    >
+        {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::SIDEBAR_NAV_START) }}
+
+        @if (filament()->hasTenancy() && filament()->hasTenantMenu())
+            <div
+                @class([
+                    'fi-sidebar-nav-tenant-menu-ctn',
+                    '-mx-2' => ! filament()->isSidebarCollapsibleOnDesktop(),
+                ])
+                @if (filament()->isSidebarCollapsibleOnDesktop())
+                    x-bind:class="$store.sidebar.isOpen ? '-mx-2' : '-mx-4'"
+                @endif
+            >
+                <x-filament-panels::tenant-menu />
+            </div>
+        @endif
+
+        <ul class="fi-sidebar-nav-groups -mx-2 flex flex-col gap-y-7">
+            @foreach ($navigation as $group)
+                <x-filament-panels::sidebar.group
+                    :active="$group->isActive()"
+                    :collapsible="$group->isCollapsible()"
+                    :icon="$group->getIcon()"
+                    :items="$group->getItems()"
+                    :label="$group->getLabel()"
+                    :attributes="\Filament\Support\prepare_inherited_attributes($group->getExtraSidebarAttributeBag())"
+                />
+            @endforeach
+        </ul>
+
+        <script>
+            var collapsedGroups = JSON.parse(
+                localStorage.getItem('collapsedGroups'),
+            )
+
+            if (collapsedGroups === null || collapsedGroups === 'null') {
+                localStorage.setItem(
+                    'collapsedGroups',
+                    JSON.stringify(@js(
+                        collect($navigation)
+                            ->filter(fn (\Filament\Navigation\NavigationGroup $group): bool => $group->isCollapsed())
+                            ->map(fn (\Filament\Navigation\NavigationGroup $group): string => $group->getLabel())
+                            ->values()
+                    )),
+                )
+            }
+
+            collapsedGroups = JSON.parse(
+                localStorage.getItem('collapsedGroups'),
+            )
+
+            document
+                .querySelectorAll('.fi-sidebar-group')
+                .forEach((group) => {
+                    if (
+                        !collapsedGroups.includes(group.dataset.groupLabel)
+                    ) {
+                        return
+                    }
+
+                    // Alpine.js loads too slow, so attempt to hide a
+                    // collapsed sidebar group earlier.
+                    group.querySelector(
+                        '.fi-sidebar-group-items',
+                    ).style.display = 'none'
+                    group
+                        .querySelector('.fi-sidebar-group-collapse-button')
+                        .classList.add('rotate-180')
+                })
+        </script>
+
+        {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::SIDEBAR_NAV_END) }}
+    </nav>
+
+    {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::SIDEBAR_FOOTER) }}
+</aside>
+{{-- format-ignore-end --}}
---
+++ resources/views/vendor/filament-panels/components/topbar/index.blade.php
@@ -0,0 +1,246 @@
+{{--
+<COPYRIGHT>
+
+    Copyright © 2016-2024, Canyon GBS LLC. All rights reserved.
+
+    Advising App™ is licensed under the Elastic License 2.0. For more details,
+    see https://github.com/canyongbs/advisingapp/blob/main/LICENSE.
+
+    Notice:
+
+    - You may not provide the software to third parties as a hosted or managed
+      service, where the service provides users with access to any substantial set of
+      the features or functionality of the software.
+    - You may not move, change, disable, or circumvent the license key functionality
+      in the software, and you may not remove or obscure any functionality in the
+      software that is protected by the license key.
+    - You may not alter, remove, or obscure any licensing, copyright, or other notices
+      of the licensor in the software. Any use of the licensor’s trademarks is subject
+      to applicable law.
+    - Canyon GBS LLC respects the intellectual property rights of others and expects the
+      same in return. Canyon GBS™ and Advising App™ are registered trademarks of
+      Canyon GBS LLC, and we are committed to enforcing and protecting our trademarks
+      vigorously.
+    - The software solution, including services, infrastructure, and code, is offered as a
+      Software as a Service (SaaS) by Canyon GBS LLC.
+    - Use of this software implies agreement to the license terms and conditions as stated
+      in the Elastic License 2.0.
+
+    For more information or inquiries please visit our website at
+    https://www.canyongbs.com or contact us via email at legal@canyongbs.com.
+
+</COPYRIGHT>
+--}}
+@props([
+    'navigation',
+])
+
+@php
+    $isRtl = __('filament-panels::layout.direction') === 'rtl';
+@endphp
+
+<div
+    {{
+        $attributes->class([
+            'fi-topbar sticky top-0 z-20 overflow-x-clip',
+            'fi-topbar-with-navigation' => filament()->hasTopNavigation(),
+        ])
+    }}
+>
+    <nav
+        class="flex h-16 items-center gap-x-4 bg-white px-4 shadow-sm ring-1 ring-gray-950/5 dark:bg-gray-900 dark:ring-white/10 md:px-6 lg:px-8"
+    >
+        {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::TOPBAR_START) }}
+
+        @if (filament()->hasNavigation())
+            <x-filament::icon-button
+                color="gray"
+                icon="heroicon-o-bars-3"
+                icon-alias="panels::topbar.open-sidebar-button"
+                icon-size="lg"
+                :label="__('filament-panels::layout.actions.sidebar.expand.label')"
+                x-cloak
+                x-data="{}"
+                x-on:click="$store.sidebar.open()"
+                x-show="! $store.sidebar.isOpen"
+                @class([
+                    'fi-topbar-open-sidebar-btn',
+                    'lg:hidden' => (! filament()->isSidebarFullyCollapsibleOnDesktop()) || filament()->isSidebarCollapsibleOnDesktop(),
+                ])
+            />
+
+            <x-filament::icon-button
+                color="gray"
+                icon="heroicon-o-x-mark"
+                icon-alias="panels::topbar.close-sidebar-button"
+                icon-size="lg"
+                :label="__('filament-panels::layout.actions.sidebar.collapse.label')"
+                x-cloak
+                x-data="{}"
+                x-on:click="$store.sidebar.close()"
+                x-show="$store.sidebar.isOpen"
+                class="fi-topbar-close-sidebar-btn lg:hidden"
+            />
+        @endif
+
+        <div class="me-6 hidden lg:flex items-center gap-3">
+            @if ($homeUrl = filament()->getHomeUrl())
+                <a {{ \Filament\Support\generate_href_html($homeUrl) }}>
+                    <x-filament-panels::logo />
+                </a>
+            @else
+                <x-filament-panels::logo />
+            @endif
+
+            @if (filament()->isSidebarCollapsibleOnDesktop())
+                <x-filament::icon-button
+                    color="gray"
+                    :icon="$isRtl ? 'heroicon-o-chevron-left' : 'heroicon-o-chevron-right'"
+                    {{-- @deprecated Use `panels::sidebar.expand-button.rtl` instead of `panels::sidebar.expand-button` for RTL. --}}
+                    :icon-alias="$isRtl ? ['panels::sidebar.expand-button.rtl', 'panels::sidebar.expand-button'] : 'panels::sidebar.expand-button'"
+                    icon-size="lg"
+                    :label="__('filament-panels::layout.actions.sidebar.expand.label')"
+                    x-cloak
+                    x-data="{}"
+                    x-on:click="$store.sidebar.open()"
+                    x-show="! $store.sidebar.isOpen"
+                    class="mx-auto"
+                />
+            @endif
+
+            @if (filament()->isSidebarCollapsibleOnDesktop() || filament()->isSidebarFullyCollapsibleOnDesktop())
+                <x-filament::icon-button
+                    color="gray"
+                    :icon="$isRtl ? 'heroicon-o-chevron-right' : 'heroicon-o-chevron-left'"
+                    {{-- @deprecated Use `panels::sidebar.collapse-button.rtl` instead of `panels::sidebar.collapse-button` for RTL. --}}
+                    :icon-alias="$isRtl ? ['panels::sidebar.collapse-button.rtl', 'panels::sidebar.collapse-button'] : 'panels::sidebar.collapse-button'"
+                    icon-size="lg"
+                    :label="__('filament-panels::layout.actions.sidebar.collapse.label')"
+                    x-cloak
+                    x-data="{}"
+                    x-on:click="$store.sidebar.close()"
+                    x-show="$store.sidebar.isOpen"
+                    class="ms-auto hidden lg:flex"
+                />
+            @endif
+        </div>
+
+        @if (filament()->hasTopNavigation() || (! filament()->hasNavigation()))
+            @if (filament()->hasTenancy() && filament()->hasTenantMenu())
+                <x-filament-panels::tenant-menu class="hidden lg:block" />
+            @endif
+
+            @if (filament()->hasNavigation())
+                <ul class="me-4 hidden items-center gap-x-4 lg:flex">
+                    @foreach ($navigation as $group)
+                        @if ($groupLabel = $group->getLabel())
+                            <x-filament::dropdown
+                                placement="bottom-start"
+                                teleport
+                                :attributes="\Filament\Support\prepare_inherited_attributes($group->getExtraTopbarAttributeBag())"
+                            >
+                                <x-slot name="trigger">
+                                    <x-filament-panels::topbar.item
+                                        :active="$group->isActive()"
+                                        :icon="$group->getIcon()"
+                                    >
+                                        {{ $groupLabel }}
+                                    </x-filament-panels::topbar.item>
+                                </x-slot>
+
+                                @php
+                                    $lists = [];
+
+                                    foreach ($group->getItems() as $item) {
+                                        if ($childItems = $item->getChildItems()) {
+                                            $lists[] = [
+                                                $item,
+                                                ...$childItems,
+                                            ];
+                                            $lists[] = [];
+
+                                            continue;
+                                        }
+
+                                        if (empty($lists)) {
+                                            $lists[] = [$item];
+
+                                            continue;
+                                        }
+
+                                        $lists[count($lists) - 1][] = $item;
+                                    }
+
+                                    if (empty($lists[count($lists) - 1])) {
+                                        array_pop($lists);
+                                    }
+                                @endphp
+
+                                @foreach ($lists as $list)
+                                    <x-filament::dropdown.list>
+                                        @foreach ($list as $item)
+                                            @php
+                                                $itemIsActive = $item->isActive();
+                                            @endphp
+
+                                            <x-filament::dropdown.list.item
+                                                :badge="$item->getBadge()"
+                                                :badge-color="$item->getBadgeColor()"
+                                                :badge-tooltip="$item->getBadgeTooltip()"
+                                                :color="$itemIsActive ? 'primary' : 'gray'"
+                                                :href="$item->getUrl()"
+                                                :icon="$itemIsActive ? ($item->getActiveIcon() ?? $item->getIcon()) : $item->getIcon()"
+                                                tag="a"
+                                                :target="$item->shouldOpenUrlInNewTab() ? '_blank' : null"
+                                            >
+                                                {{ $item->getLabel() }}
+                                            </x-filament::dropdown.list.item>
+                                        @endforeach
+                                    </x-filament::dropdown.list>
+                                @endforeach
+                            </x-filament::dropdown>
+                        @else
+                            @foreach ($group->getItems() as $item)
+                                <x-filament-panels::topbar.item
+                                    :active="$item->isActive()"
+                                    :active-icon="$item->getActiveIcon()"
+                                    :badge="$item->getBadge()"
+                                    :badge-color="$item->getBadgeColor()"
+                                    :badge-tooltip="$item->getBadgeTooltip()"
+                                    :icon="$item->getIcon()"
+                                    :should-open-url-in-new-tab="$item->shouldOpenUrlInNewTab()"
+                                    :url="$item->getUrl()"
+                                >
+                                    {{ $item->getLabel() }}
+                                </x-filament-panels::topbar.item>
+                            @endforeach
+                        @endif
+                    @endforeach
+                </ul>
+            @endif
+        @endif
+
+        <div
+            x-persist="topbar.end"
+            class="ms-auto flex items-center gap-x-4"
+        >
+            {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::GLOBAL_SEARCH_BEFORE) }}
+
+            @if (filament()->isGlobalSearchEnabled())
+                @livewire(Filament\Livewire\GlobalSearch::class)
+            @endif
+
+            {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::GLOBAL_SEARCH_AFTER) }}
+
+            @if (filament()->auth()->check())
+                @if (filament()->hasDatabaseNotifications())
+                    @livewire(Filament\Livewire\DatabaseNotifications::class, ['lazy' => true])
+                @endif
+
+                <x-filament-panels::user-menu />
+            @endif
+        </div>
+
+        {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::TOPBAR_END) }}
+    </nav>
+</div>
