2aea07c165ebd4649657ceea99f7ede624c99754
jayushi-canyon <jayushi-canyon@users.noreply.github.com>
2024-09-13 08:20:49 +0000

    chore: fix code style

--- app-modules/knowledge-base/database/migrations/2024_09_10_171824_add_portal_view_count_in_knowledge_base_articles_table.php
+++ app-modules/knowledge-base/database/migrations/2024_09_10_171824_add_portal_view_count_in_knowledge_base_articles_table.php
@@ -38,18 +38,18 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-return new class() extends Migration {
-  public function up(): void
-  {
-    Schema::table('knowledge_base_articles', function (Blueprint $table) {
-      $table->integer('portal_view_count')->default(0);
-    });
-  }
+return new class () extends Migration {
+    public function up(): void
+    {
+        Schema::table('knowledge_base_articles', function (Blueprint $table) {
+            $table->integer('portal_view_count')->default(0);
+        });
+    }
 
-  public function down(): void
-  {
-    Schema::table('knowledge_base_articles', function (Blueprint $table) {
-      $table->dropColumn('portal_view_count');
-    });
-  }
+    public function down(): void
+    {
+        Schema::table('knowledge_base_articles', function (Blueprint $table) {
+            $table->dropColumn('portal_view_count');
+        });
+    }
 };
