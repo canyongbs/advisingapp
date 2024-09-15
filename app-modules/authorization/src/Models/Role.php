7d46b5bfd324611efe1d4bd570516d1be5e70be7
Dan Harrin <git@danharrin.com>
2024-09-15 10:25:44 +0100

    [ADVAPP-870]: Fix performance issues with Roles

--- app-modules/authorization/src/Models/Role.php
+++ app-modules/authorization/src/Models/Role.php
@@ -62,7 +62,7 @@ class Role extends SpatieRole implements Auditable
             getModelForGuard($this->attributes['guard_name'] ?? config('auth.defaults.guard')),
             'model',
             config('permission.table_names.model_has_roles'),
-            PermissionRegistrar::$pivotRole,
+            app(PermissionRegistrar::class)->pivotRole,
             config('permission.column_names.model_morph_key')
         );
     }
