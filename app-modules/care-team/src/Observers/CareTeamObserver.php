36a8b11a04c2eceef3977f76732d664507891683
Payal Baldaniya <payal.baldaniya@canyongbs.com>
2024-09-26 01:52:31 +0530

    [ADVAPP-886]: Improve the notifications received when a user is added to a care team (#1034)
    * [ADVAPP-885]: Improve the relationship between care team members and students and prospects by auto subscribing users
    
    * worked on suggestions
    
    * rewrite the testcase
    
    * created the unit testcase
    
    * chore: fix enforcement of copyright on all files
    
    * [ADVAPP-886]: Improve the notifications received when a user is added to a care team
    
    ---------
    
    Co-authored-by: payal-canyon <payal-canyon@users.noreply.github.com>

--- app-modules/care-team/src/Observers/CareTeamObserver.php
+++ app-modules/care-team/src/Observers/CareTeamObserver.php
@@ -37,8 +37,14 @@
 namespace AdvisingApp\CareTeam\Observers;
 
 use App\Models\User;
+use Illuminate\Support\HtmlString;
+use Filament\Notifications\Notification;
 use AdvisingApp\CareTeam\Models\CareTeam;
+use AdvisingApp\Prospect\Models\Prospect;
+use AdvisingApp\StudentDataModel\Models\Student;
+use AdvisingApp\Prospect\Filament\Resources\ProspectResource;
 use AdvisingApp\Notification\Events\TriggeredAutoSubscription;
+use AdvisingApp\StudentDataModel\Filament\Resources\StudentResource;
 
 class CareTeamObserver
 {
@@ -48,6 +54,57 @@ class CareTeamObserver
 
         if ($user instanceof User) {
             TriggeredAutoSubscription::dispatch($user, $careTeam);
+
+            $educatable = $careTeam->educatable;
+
+            $name = $educatable->{$educatable->displayNameKey()};
+
+            $target = match ($educatable::class) {
+                Prospect::class => ProspectResource::class,
+                Student::class => StudentResource::class,
+            };
+
+            $url = $target::getUrl('view', ['record' => $educatable]);
+
+            $link = new HtmlString("<a href='{$url}' target='_blank' class='underline'>{$name}</a>");
+
+            $morph = str($educatable->getMorphClass());
+
+            $user->notify(
+                Notification::make()
+                    ->success()
+                    ->title("You have been added as a care team member to {$morph} {$link}")
+                    ->toDatabase(),
+            );
+        }
+    }
+
+    public function deleted(CareTeam $careTeam): void
+    {
+        $user = $careTeam->user;
+
+        if ($user instanceof User) {
+            $educatable = $careTeam->educatable;
+
+            $name = $educatable->{$educatable->displayNameKey()};
+
+            $target = match ($educatable::class) {
+                Prospect::class => ProspectResource::class,
+                Student::class => StudentResource::class,
+            };
+
+            $url = $target::getUrl('view', ['record' => $educatable]);
+
+            $link = new HtmlString("<a href='{$url}' target='_blank' class='underline'>{$name}</a>");
+
+            $morph = str($educatable->getMorphClass());
+
+            $user->notify(
+                Notification::make()
+                    ->danger()
+                    ->title("You have been removed from the care team for {$morph} {$link}")
+                    ->toDatabase(),
+            );
         }
     }
 }
