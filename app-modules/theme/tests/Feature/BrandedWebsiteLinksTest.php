225f68c55a023c94ea70d6e8fe0715da725ad4ef
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2024-09-25 16:08:58 +0000

    Finish tests
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- app-modules/theme/tests/Feature/BrandedWebsiteLinksTest.php
+++ app-modules/theme/tests/Feature/BrandedWebsiteLinksTest.php
@@ -2,14 +2,14 @@
 
 namespace AdvisingApp\Theme\Tests\Feature;
 
-use AdvisingApp\Theme\Settings\ThemeSettings;
-use AdvisingApp\Theme\Tests\RequestFactories\BrandedWebsiteLinksRequestFactory;
-
+use App\Models\Tenant;
 use App\Http\Middleware\CheckOlympusKey;
 
-use App\Models\Tenant;
 use function Pest\Laravel\withoutMiddleware;
 
+use AdvisingApp\Theme\Settings\ThemeSettings;
+use Worksome\RequestFactories\RequestFactory;
+use AdvisingApp\Theme\Tests\RequestFactories\BrandedWebsiteLinksRequestFactory;
 
 test('Branded theme api test', function () {
     $tenant = Tenant::current();
@@ -38,3 +38,75 @@ test('Branded theme api test', function () {
         expect($settings->custom_link_url)->toEqual($data['custom_link_url']);
     });
 });
+
+it('validates the inputs', function (RequestFactory $requestFactory, $errors) {
+    $tenant = Tenant::current();
+
+    $data = $requestFactory->create();
+
+    $request = withoutMiddleware(CheckOlympusKey::class)
+        ->post(
+            route('landlord.api.brandedWebsiteLinks.update'),
+            $data
+        )
+        ->assertSessionHasErrors($errors);
+})->with(
+    [
+        'is_support_url_enabled boolean' => [
+            BrandedWebsiteLinksRequestFactory::new()->state(['is_support_url_enabled' => 'blah']),
+            ['is_support_url_enabled'],
+        ],
+        'support_url string' => [
+            BrandedWebsiteLinksRequestFactory::new()->state(['support_url' => 123]),
+            ['support_url'],
+        ],
+        'support_url url' => [
+            BrandedWebsiteLinksRequestFactory::new()->state(['support_url' => 'blah']),
+            ['support_url'],
+        ],
+        'is_recent_updates_url_enabled boolean' => [
+            BrandedWebsiteLinksRequestFactory::new()->state(['is_recent_updates_url_enabled' => 'blah']),
+            ['is_recent_updates_url_enabled'],
+        ],
+        'recent_updates_url string' => [
+            BrandedWebsiteLinksRequestFactory::new()->state(['recent_updates_url' => 123]),
+            ['recent_updates_url'],
+        ],
+        'recent_updates_url url' => [
+            BrandedWebsiteLinksRequestFactory::new()->state(['recent_updates_url' => 'blah']),
+            ['recent_updates_url'],
+        ],
+        'is_custom_link_url_enabled boolean' => [
+            BrandedWebsiteLinksRequestFactory::new()->state(['is_custom_link_url_enabled' => 'blah']),
+            ['is_custom_link_url_enabled'],
+        ],
+        'custom_link_label string' => [
+            BrandedWebsiteLinksRequestFactory::new()->state(['custom_link_label' => 123]),
+            ['custom_link_label'],
+        ],
+        'custom_link_url string' => [
+            BrandedWebsiteLinksRequestFactory::new()->state(['custom_link_url' => 123]),
+            ['custom_link_url'],
+        ],
+        'custom_link_url url' => [
+            BrandedWebsiteLinksRequestFactory::new()->state(['custom_link_url' => 'blah']),
+            ['custom_link_url'],
+        ],
+        'tenant_id string' => [
+            BrandedWebsiteLinksRequestFactory::new()->state(['tenant_id' => 123]),
+            ['tenant_id'],
+        ],
+        'tenant_id required and missing' => [
+            BrandedWebsiteLinksRequestFactory::new()->without(['tenant_id']),
+            ['tenant_id'],
+        ],
+        'tenant_id required and null' => [
+            BrandedWebsiteLinksRequestFactory::new()->state(['tenant_id' => null]),
+            ['tenant_id'],
+        ],
+        'tenant_id required and empty string' => [
+            BrandedWebsiteLinksRequestFactory::new()->state(['tenant_id' => '']),
+            ['tenant_id'],
+        ],
+    ]
+);
