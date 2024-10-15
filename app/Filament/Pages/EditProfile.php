4c5c54641d8569354d9473103edfc2064d54c1d4
Ketan Barevadiya <ketan.barevadiya@canyongbs.com>
2024-10-15 22:12:30 +0530

    [ADVAPP-869]: Enable the branding bar to be dismissible by end users (#1053)
    * feat: Enable the branding bar to be dismissible by end users (WIP)
    
    * Worked on remaining changes
    
    * chore: fix enforcement of copyright on all files
    
    * chore: fix code style
    
    * worked on PR Changes
    
    * chore: fix code style
    
    * worked on PR review changes
    
    * chore: fix code style
    
    * worked on sonar cloud issue
    
    * Worked on PR Review changes
    
    * Fix infinate loop issues
    
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>
    
    ---------
    
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>
    Co-authored-by: ketan-canyon <ketan-canyon@users.noreply.github.com>
    Co-authored-by: joelicatajr <joelicatajr@users.noreply.github.com>
    Co-authored-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- app/Filament/Pages/EditProfile.php
+++ app/Filament/Pages/EditProfile.php
@@ -45,6 +45,7 @@ use Filament\Actions\Action;
 use Filament\Facades\Filament;
 use App\Settings\DisplaySettings;
 use Filament\Actions\ActionGroup;
+use App\Features\EnableBrandingBar;
 use Filament\Forms\Components\Grid;
 use Illuminate\Support\Facades\Hash;
 use Filament\Forms\Components\Select;
@@ -58,6 +59,7 @@ use Illuminate\Database\Eloquent\Model;
 use Filament\Forms\Components\Component;
 use Filament\Forms\Components\TextInput;
 use Filament\Notifications\Notification;
+use App\Settings\CollegeBrandingSettings;
 use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\TimePicker;
 use Illuminate\Validation\Rules\Password;
@@ -262,6 +264,12 @@ class EditProfile extends Page
                                 return "Default: {$timezone}";
                             }),
                     ]),
+                Section::make('Disable Branding Bar')
+                    ->aside()
+                    ->schema([
+                        Toggle::make('is_branding_bar_dismissed'),
+                    ])
+                    ->visible(fn (CollegeBrandingSettings $settings) => EnableBrandingBar::active() && $settings->dismissible),
                 Section::make('Connected Accounts')
                     ->description('Disconnect your external accounts.')
                     ->aside()
@@ -374,6 +382,8 @@ class EditProfile extends Page
         $this->data['passwordConfirmation'] = null;
 
         $this->getSavedNotification()?->send();
+
+        $this->dispatch('refresh-branding-bar');
 
         if ($redirectUrl = $this->getRedirectUrl()) {
             $this->redirect($redirectUrl);
--- resources/views/vendor/filament-panels/components/branding-bar.blade.php
+++ resources/views/vendor/filament-panels/components/branding-bar.blade.php
@@ -1,6 +1,4 @@
-<?php
-
-/*
+{{--
 <COPYRIGHT>
 
     Copyright © 2016-2024, Canyon GBS LLC. All rights reserved.
@@ -32,24 +30,31 @@
     https://www.canyongbs.com or contact us via email at legal@canyongbs.com.
 
 </COPYRIGHT>
-*/
+--}}
+            
+@php
+    use Filament\Support\Colors\Color;
+@endphp
 
-namespace App\Settings;
+<div  
+    x-data="{ isVisible: @entangle('isVisible') }" 
+    x-show="isVisible"
+    wire:loading.remove
+    style="display: none; --c-600: {{ Color::all()[$color][600] }}"
+    class="sticky top-16 z-10 bg-custom-600 text-sm font-medium text-white px-6 py-2 flex items-center h-10"
+>
+    {{ $brandingBarText}}
+    
+    <button  
+          x-data="{ isDismissible: @entangle('dismissible') }" 
+          x-show="isDismissible" 
+          @click="$wire.dismiss()" 
+          class="hover:text-gray-400 ml-auto"
+    >
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+        </svg>
+    </button>
+</div>
 
-use Spatie\LaravelSettings\Settings;
-
-class CollegeBrandingSettings extends Settings
-{
-    public bool $is_enabled = false;
-
-    public ?string $college_text = null;
-
-    public ?string $color = null;
-
-    public bool $dismissible = false;
-
-    public static function group(): string
-    {
-        return 'college_branding';
-    }
-}
+          
