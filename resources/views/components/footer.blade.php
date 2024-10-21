1f7551827c8459d51593bca2da6450314ac5c8ff
amit-canyon <amit.solanki@canyongbs.com>
2024-10-21 09:19:00 +0000

    Fix format

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -48,9 +48,8 @@
 
     <div class="flex w-full justify-center pb-4">
         <span class="w-11/12 text-center text-xs lg:w-3/4 xl:w-7/12">
-            © 2016-{{ date('Y') }} Canyon GBS LLC. All Rights Reserved. Canyon GBS&#174; and Advising App&#174; are trademarks
-            of Canyon GBS
-            LLC. For more information or inquiries, please visit our website at
+            © 2016-{{ date('Y') }} Canyon GBS LLC. All Rights Reserved. Canyon GBS&#174; and Advising App&#174; are
+            trademarks of Canyon GBS LLC. For more information or inquiries, please visit our website at
             <a
                 class="text-blue-600 underline dark:text-blue-400"
                 href="https://canyongbs.com/"
