e201fb52d48c7607fb1b25e8b74d20be5f437a01
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2024-10-21 19:04:32 +0000

    Finish Enrollment import
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- app-modules/student-data-model/src/Filament/Imports/EnrollmentImporter.php
+++ app-modules/student-data-model/src/Filament/Imports/EnrollmentImporter.php
@@ -51,55 +51,135 @@ class EnrollmentImporter extends Importer
             ImportColumn::make('sisid')
                 ->label('Student ID')
                 ->requiredMapping()
-                ->example('########'),
+                ->example('########')
+                ->rules([
+                    'required',
+                    'string',
+                    'max:255',
+                ]),
             ImportColumn::make('division')
                 ->example('ABC01')
-                ->label('Division'),
+                ->label('Division')
+                ->rules([
+                    'nullable',
+                    'string',
+                    'max:255',
+                ]),
             ImportColumn::make('class_nbr')
                 ->label('Class NBR')
-                ->example('19309'),
+                ->example('19309')
+                ->rules([
+                    'nullable',
+                    'string',
+                    'max:255',
+                ]),
             ImportColumn::make('crse_grade_off')
                 ->example('A')
-                ->label('CRSE grade off'),
+                ->label('CRSE grade off')
+                ->rules([
+                    'nullable',
+                    'string',
+                    'max:255',
+                ]),
             ImportColumn::make('unt_taken')
                 ->label('UNT taken')
                 ->example('1')
-                ->numeric(),
+                ->numeric()
+                ->rules([
+                    'nullable',
+                    'numeric',
+                ]),
             ImportColumn::make('unt_earned')
                 ->label('UNT earned')
                 ->example('1')
-                ->numeric(),
+                ->numeric()
+                ->rules([
+                    'nullable',
+                    'numeric',
+                ]),
             ImportColumn::make('last_upd_dt_stmp')
                 ->label('Last UPD date STMP')
-                ->example('1995-02-11 14:01:12+00'),
+                ->example('1995-02-11 14:01:12+00')
+                ->rules([
+                    'nullable',
+                    'date',
+                ]),
             ImportColumn::make('section')
                 ->label('Section')
-                ->example('7661'),
+                ->example('7661')
+                ->rules([
+                    'nullable',
+                    'string',
+                    'max:255',
+                ]),
             ImportColumn::make('name')
                 ->label('Name')
-                ->example('Introduction to Mathematics'),
+                ->example('Introduction to Mathematics')
+                ->rules([
+                    'nullable',
+                    'string',
+                    'max:255',
+                ]),
             ImportColumn::make('department')
                 ->label('Department')
-                ->example('Business'),
+                ->example('Business')
+                ->rules([
+                    'nullable',
+                    'string',
+                    'max:255',
+                ]),
             ImportColumn::make('faculty_name')
                 ->label('Faculty name')
-                ->example('Keyon Metz'),
+                ->example('Keyon Metz')
+                ->rules([
+                    'nullable',
+                    'string',
+                    'max:255',
+                ]),
             ImportColumn::make('faculty_email')
                 ->label('Faculty email')
-                ->example('jerry72@example.net'),
+                ->example('jerry72@example.net')
+                ->rules([
+                    'nullable',
+                    'email',
+                    'max:255',
+                ]),
             ImportColumn::make('semester_code')
                 ->label('Semester code')
-                ->example('4209'),
+                ->example('4209')
+                ->rules([
+                    'nullable',
+                    'string',
+                    'max:255',
+                ]),
             ImportColumn::make('semester_name')
                 ->label('Semester name')
-                ->example('Fall 2006'),
+                ->example('Fall 2006')
+                ->rules([
+                    'nullable',
+                    'string',
+                    'max:255',
+                ]),
             ImportColumn::make('start_date')
                 ->label('Start date')
-                ->example('2001-09-30 19:55:54'),
+                ->example('2001-09-30 19:55:54')
+                ->rules([
+                    'nullable',
+                    'date',
+                ]),
             ImportColumn::make('end_date')
                 ->label('End date')
-                ->example('2001-09-30 19:55:54'),
+                ->example('2001-09-30 19:55:54')
+                ->rules([
+                    'nullable',
+                    'date',
+                ]),
         ];
+    }
+
+    public function resolveRecord(): ?Enrollment
+    {
+        return new Enrollment();
     }
 
     public static function getCompletedNotificationBody(Import $import): string
