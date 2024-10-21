4955a32fd661a64384e84a3c6e0f1955e73249c6
joelicatajr <joelicatajr@users.noreply.github.com>
2024-10-21 17:46:52 +0000

    chore: fix code style

--- app-modules/student-data-model/database/migrations/2024_10_21_173037_add_id_column_to_programs_table.php
+++ app-modules/student-data-model/database/migrations/2024_10_21_173037_add_id_column_to_programs_table.php
@@ -39,8 +39,7 @@ use Illuminate\Database\Migrations\Migration;
 use Tpetry\PostgresqlEnhanced\Schema\Blueprint;
 use Tpetry\PostgresqlEnhanced\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     public function up(): void
     {
         Schema::table('programs', function (Blueprint $table) {
