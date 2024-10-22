f4a0fa8c993c21974e8070b80bbda3ccfdf09d78
Ankit Kabra <164159992+ankit-canyon@users.noreply.github.com>
2024-10-22 16:18:06 +0530

    [ADVAPP-950]: Login page fails to preserve the padding in the top header when responding to screen size changes (#1077)
    * Fix cotent overlapping & logo padding issue
    
    * chore: fix enforcement of copyright on all files
    
    ---------
    
    Co-authored-by: ankit-canyon <ankit-canyon@users.noreply.github.com>

--- resources/views/vendor/filament-panels/components/layout/login.blade.php
+++ resources/views/vendor/filament-panels/components/layout/login.blade.php
@@ -31,17 +31,17 @@
 
 </COPYRIGHT>
 --}}
-
 <x-filament-panels::layout.base :livewire="$livewire">
-    <div class="fi-layout flex h-full w-full flex-row-reverse overflow-x-clip">
-        <div class="fi-main-ctn w-screen flex flex-col lg:h-screen md:h-auto">
-            <div class="flex justify-center items-center w-full h-32 border-b border-gray-200 mb-4">
+    <div class="fi-layout flex h-screen w-full flex-col lg:flex-row-reverse">
+        <div class="fi-main-ctn w-full flex flex-col h-full">
+            <div class="flex justify-center items-center w-full border-b border-gray-200 mb-4 flex-shrink-0 p-4">
                 <x-filament-panels::logo />
             </div>
-            <main class="fi-main mx-auto flex justify-center items-center h-full w-full px-4 md:px-6 lg:px-8 max-w-screen-lg">
+            
+            <main class="fi-main mx-auto flex-grow flex justify-center items-center w-full px-4 md:px-6 lg:px-8 max-w-screen-lg mb-4">
                 {{ $slot }}
             </main>
-            <div class="mt-3 mb-6 inline-block w-full">
+            <div class="mt-auto mb-4 lg:mb-4 inline-block w-full h-16">
                 <x-footer class="footer" />
             </div>
         </div>
