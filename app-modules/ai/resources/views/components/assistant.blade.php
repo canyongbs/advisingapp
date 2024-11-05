887875ffbc9522a27e7d0df4ae009fa8758e927a
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2024-11-05 23:48:40 +0000

    Fix formatting
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- app-modules/ai/resources/views/components/assistant.blade.php
+++ app-modules/ai/resources/views/components/assistant.blade.php
@@ -124,10 +124,8 @@
                                             x-on:dragend="end"
                                             :class="{
                                                 'flex items-center cursor-move': true,
-                                                'text-gray-700 dark:text-gray-200': thread.id !== $wire
-                                                    .selectedThreadId,
-                                                'text-primary-600 dark:text-primary-400': thread.id === $wire
-                                                    .selectedThreadId
+                                                'text-gray-700 dark:text-gray-200': thread.id !== $wire.selectedThreadId,
+                                                'text-primary-600 dark:text-primary-400': thread.id === $wire.selectedThreadId
                                             }"
                                         >
                                             <template x-if="loading.type !== 'thread' || loading.identifier !== thread.id">
@@ -358,10 +356,8 @@
                                                 x-on:dragend="end"
                                                 :class="{
                                                     'flex items-center cursor-move': true,
-                                                    'text-gray-700 dark:text-gray-200': thread.id !== $wire
-                                                        .selectedThreadId,
-                                                    'text-primary-600 dark:text-primary-400': thread.id === $wire
-                                                        .selectedThreadId
+                                                    'text-gray-700 dark:text-gray-200': thread.id !== $wire.selectedThreadId,
+                                                    'text-primary-600 dark:text-primary-400': thread.id === $wire.selectedThreadId
                                                 }"
                                             >
                                                 <template
