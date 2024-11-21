1535ddf279f247455add620ecb2b643fa0986988
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2024-11-21 19:11:56 +0000

    Fix none renamed variable
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- resources/views/vendor/filament-panels/components/login-resource-portal-card.blade.php
+++ resources/views/vendor/filament-panels/components/login-resource-portal-card.blade.php
@@ -36,7 +36,7 @@
         <div>
             <p class="text-gray-800 text-sm font-semibold">Resource Portal</p>
             <p class="text-gray-700 text-sm my-4">Visit our resource portal for answers to frequently asked product and service questions.</p>
-            <a href="{{ $productKnowledgebaseUrl }}"  target="_blank" class="border-2 border-gray-700 px-4 py-2 text-gray-700 rounded-xl text-sm font-semibold inline-block">Visit Resource Portal</a>
+            <a href="{{ $productResourcehubUrl }}"  target="_blank" class="border-2 border-gray-700 px-4 py-2 text-gray-700 rounded-xl text-sm font-semibold inline-block">Visit Resource Portal</a>
         </div>
         <img src="{{ Vite::asset('resources/images/changelog_image.svg') }}" class="block max-h-32" />
     </div>
