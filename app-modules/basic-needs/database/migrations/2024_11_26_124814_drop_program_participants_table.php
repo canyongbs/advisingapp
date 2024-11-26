f99a1d4041b28035a6d7aaaf13787846f3f859b0
kandarp-canyon <kandarp.pandya@canyongbs.com>
2024-11-26 15:37:00 +0000

    down merhod added

--- app-modules/basic-needs/database/migrations/2024_11_26_124814_drop_program_participants_table.php
+++ app-modules/basic-needs/database/migrations/2024_11_26_124814_drop_program_participants_table.php
@@ -35,11 +35,24 @@
 */
 
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 return new class () extends Migration {
     public function up(): void
     {
         Schema::dropIfExists('program_participants');
+    }
+
+    public function down(): void
+    {
+        Schema::create('program_participants', function (Blueprint $table) {
+            $table->foreignUuid('basic_needs_program_id')->constrained('basic_needs_programs')->cascadeOnDelete();
+            $table->string('program_participants_type');
+            $table->string('program_participants_id');
+            $table->timestamps();
+
+            $table->index(['program_participants_type', 'program_participants_id']);
+        });
     }
 };
