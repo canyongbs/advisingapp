8fc6f21b0bf341e851332249dc119b102358c4f0
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2024-12-03 20:46:27 +0000

    Fix the tag factory
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -41,7 +41,7 @@ use App\Enums\TagType;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tag>
+ * @extends Factory<Tag>
  */
 class TagFactory extends Factory
 {
