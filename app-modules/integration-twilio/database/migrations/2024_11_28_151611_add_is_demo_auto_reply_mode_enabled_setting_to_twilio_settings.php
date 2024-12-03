65c4bf7d34f0a1863ce84c0e4683168be5a42455
danharrin <dan.harrin@canyongbs.com>
2024-12-03 12:39:53 +0000

    Catch SettingAlreadyExists exception

--- app-modules/integration-twilio/database/migrations/2024_11_28_151611_add_is_demo_auto_reply_mode_enabled_setting_to_twilio_settings.php
+++ app-modules/integration-twilio/database/migrations/2024_11_28_151611_add_is_demo_auto_reply_mode_enabled_setting_to_twilio_settings.php
@@ -36,13 +36,17 @@
 
 use Spatie\LaravelSettings\Migrations\SettingsBlueprint;
 use Spatie\LaravelSettings\Migrations\SettingsMigration;
+use Spatie\LaravelSettings\Exceptions\SettingAlreadyExists;
 
 return new class () extends SettingsMigration {
     public function up(): void
     {
-        $this->migrator->inGroup('twilio', function (SettingsBlueprint $blueprint): void {
-            $blueprint->add('is_demo_auto_reply_mode_enabled', false);
-        });
+        try {
+            $this->migrator->inGroup('twilio', function (SettingsBlueprint $blueprint): void {
+                $blueprint->add('is_demo_auto_reply_mode_enabled', false);
+            });
+        } catch (SettingAlreadyExists) {
+        }
     }
 
     public function down(): void
