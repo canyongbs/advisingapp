05786af1fd4257afb6bf6c35a66276449dc4d1ff
Jayushi Jain <jayushi.jain@canyongbs.com>
2024-12-11 15:00:17 +0000

    change request files

--- app-modules/portal/src/Http/Middleware/EnsureResourceHubPortalIsEmbeddableAndAuthorized.php
+++ app-modules/portal/src/Http/Middleware/EnsureResourceHubPortalIsEmbeddableAndAuthorized.php
@@ -43,28 +43,28 @@ use AdvisingApp\Portal\Settings\PortalSettings;
 
 class EnsureResourceHubPortalIsEmbeddableAndAuthorized
 {
-    public function handle(Request $request, Closure $next): Response
-    {
-        $referer = $request->headers->get('referer');
+  public function handle(Request $request, Closure $next): Response
+  {
+    $referer = $request->headers->get('referer');
 
-        if (parse_url($request->url())['host'] === parse_url(config('app.url'))['host']) {
-            return $next($request);
-        }
-
-        if (! $referer) {
-            return response()->json(['error' => 'Missing referer header.'], 400);
-        }
-
-        $referer = parse_url($referer)['host'];
-
-        $settings = resolve(PortalSettings::class);
-
-        if ($referer != parse_url(config('app.url'))['host']) {
-            if (parse_url($settings->knowledge_management_portal_authorized_domain)['host'] !== $referer) {
-                return response()->json(['error' => 'Referer not allowed. Domain must be added to allowed domains list'], 403);
-            }
-        }
-
-        return $next($request);
+    if (parse_url($request->url())['host'] === parse_url(config('app.url'))['host']) {
+      return $next($request);
     }
+
+    if (! $referer) {
+      return response()->json(['error' => 'Missing referer header.'], 400);
+    }
+
+    $referer = parse_url($referer)['host'];
+
+    $settings = resolve(PortalSettings::class);
+
+    if ($referer != parse_url(config('app.url'))['host']) {
+      if (parse_url($settings->resource_hub_portal_authorized_domain)['host'] !== $referer) {
+        return response()->json(['error' => 'Referer not allowed. Domain must be added to allowed domains list'], 403);
+      }
+    }
+
+    return $next($request);
+  }
 }
