f73487638e2835aecd5250cfa36ff0c47599acbd
amit-canyon <amit-canyon@users.noreply.github.com>
2024-12-17 16:59:27 +0000

    chore: fix code style

--- app-modules/interaction/resources/views/components/interaction-timeline-item.blade.php
+++ app-modules/interaction/resources/views/components/interaction-timeline-item.blade.php
@@ -34,20 +34,20 @@
 @use('App\Filament\Resources\UserResource')
 <div>
     @if ($record->user)
-    <div class="flex flex-row justify-between">
-        <h3 class="mb-1 flex items-center text-lg font-semibold text-gray-500 dark:text-gray-100">
-            <a
-                class="font-medium underline"
-                href="{{ UserResource::getUrl('view', ['record' => $record->user]) }}"
-            >
-                {{ $record->user->name }}
-            </a>
-        </h3>
+        <div class="flex flex-row justify-between">
+            <h3 class="mb-1 flex items-center text-lg font-semibold text-gray-500 dark:text-gray-100">
+                <a
+                    class="font-medium underline"
+                    href="{{ UserResource::getUrl('view', ['record' => $record->user]) }}"
+                >
+                    {{ $record->user->name }}
+                </a>
+            </h3>
 
-        <div>
-            {{ $viewRecordIcon }}
+            <div>
+                {{ $viewRecordIcon }}
+            </div>
         </div>
-    </div>
     @endif
 
     <time class="mb-2 block text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
