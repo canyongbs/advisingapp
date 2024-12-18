d18fb39dbab73e565500ac4eb3cd1b01d202707d
Jayushi Jain <jayushi.jain@canyongbs.com>
2024-12-18 13:32:30 +0000

    make table report

--- app-modules/report/src/Filament/Pages/Users.php
+++ app-modules/report/src/Filament/Pages/Users.php
@@ -40,42 +40,43 @@ use App\Filament\Clusters\ReportLibrary;
 use AdvisingApp\Report\Abstract\UserReport;
 use AdvisingApp\Report\Filament\Widgets\UsersStats;
 use AdvisingApp\Report\Filament\Widgets\RefreshWidget;
+use AdvisingApp\Report\Filament\Widgets\UsersLoginCountTable;
 use AdvisingApp\Report\Filament\Widgets\UserUniqueLoginCountLineChart;
 
 class Users extends UserReport
 {
-    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+  protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
-    protected static ?string $cluster = ReportLibrary::class;
+  protected static ?string $cluster = ReportLibrary::class;
 
-    protected static ?string $navigationGroup = 'Users';
+  protected static ?string $navigationGroup = 'Users';
 
-    protected static ?string $navigationLabel = 'Overview';
+  protected static ?string $navigationLabel = 'Overview';
 
-    protected static ?string $title = 'Users (Overview)';
+  protected static ?string $title = 'Users (Overview)';
 
-    protected static string $routePath = 'Users';
+  protected static string $routePath = 'Users';
 
-    protected static ?int $navigationSort = 50;
+  protected static ?int $navigationSort = 50;
 
-    protected $cacheTag = 'report-users';
+  protected $cacheTag = 'report-users';
 
-    public function getWidgets(): array
-    {
-        return [
-            RefreshWidget::make(['cacheTag' => $this->cacheTag]),
-            UsersStats::make(['cacheTag' => $this->cacheTag]),
-            UserUniqueLoginCountLineChart::make(['cacheTag' => $this->cacheTag]),
-            // MostRecentUsersTable::make(['cacheTag' => $this->cacheTag]),
-        ];
-    }
+  public function getWidgets(): array
+  {
+    return [
+      RefreshWidget::make(['cacheTag' => $this->cacheTag]),
+      UsersStats::make(['cacheTag' => $this->cacheTag]),
+      UserUniqueLoginCountLineChart::make(['cacheTag' => $this->cacheTag]),
+      UsersLoginCountTable::make(['cacheTag' => $this->cacheTag]),
+    ];
+  }
 
-    public function getColumns(): int | string | array
-    {
-        return [
-            'sm' => 2,
-            'md' => 4,
-            'lg' => 4,
-        ];
-    }
+  public function getColumns(): int | string | array
+  {
+    return [
+      'sm' => 2,
+      'md' => 4,
+      'lg' => 4,
+    ];
+  }
 }
