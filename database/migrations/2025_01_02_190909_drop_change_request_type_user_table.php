00f92c901a3ab0f950ec071f08a474be2e09f4ba
joelicatajr <joelicatajr@users.noreply.github.com>
2025-01-02 20:07:35 +0000

    chore: fix code style

--- database/migrations/2025_01_02_190909_drop_change_request_type_user_table.php
+++ database/migrations/2025_01_02_190909_drop_change_request_type_user_table.php
@@ -35,11 +35,9 @@
 */
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     public function up(): void
     {
         Schema::dropIfExists('change_request_type_user');
