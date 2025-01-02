d5d89505b7c1e2a4d2940afacc812eda0424c3cc
Joe Licata <80057330+joelicatajr@users.noreply.github.com>
2025-01-02 06:05:20 -0700

    chore: update yearly copyright (#1221)
    Co-authored-by: Orrison <6799341+Orrison@users.noreply.github.com>

--- app-modules/case-management/tests/Notifications/ChangeRequestAwaitingApprovalTest.php
+++ app-modules/case-management/tests/Notifications/ChangeRequestAwaitingApprovalTest.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Notification;
 /*
 <COPYRIGHT>
 
-    Copyright © 2016-2024, Canyon GBS LLC. All rights reserved.
+    Copyright © 2016-2025, Canyon GBS LLC. All rights reserved.
 
     Advising App™ is licensed under the Elastic License 2.0. For more details,
     see https://github.com/canyongbs/advisingapp/blob/main/LICENSE.
