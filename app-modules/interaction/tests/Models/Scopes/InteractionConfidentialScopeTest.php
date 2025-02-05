aed22a61024a23e4b9a7291d39e5c75d2601d71a
Amit Solanki <amit.solanki@canyongbs.com>
2025-02-05 14:44:11 +0000

    refactor: simplify InteractionConfidentialScopeTest by removing redundant code and updating test descriptions

--- app-modules/interaction/tests/Models/Scopes/InteractionConfidentialScopeTest.php
+++ app-modules/interaction/tests/Models/Scopes/InteractionConfidentialScopeTest.php
@@ -37,152 +37,90 @@
 use AdvisingApp\Authorization\Enums\LicenseType;
 use AdvisingApp\Interaction\Models\Interaction;
 use AdvisingApp\Interaction\Models\Scopes\InteractionConfidentialScope;
-use AdvisingApp\StudentDataModel\Filament\Resources\StudentResource\Pages\ViewStudent;
-use AdvisingApp\StudentDataModel\Filament\Resources\StudentResource\RelationManagers\InteractionsRelationManager;
-use AdvisingApp\StudentDataModel\Models\Student;
 use AdvisingApp\Team\Models\Team;
 use App\Models\User;
 
 use function Pest\Laravel\actingAs;
-use function Pest\Livewire\livewire;
 use function Tests\asSuperAdmin;
 
 test('Interaction model has applied global scope', function () {
-    $interaction = Interaction::factory()->create();
+    Interaction::factory()->create();
 
-    expect($interaction->hasGlobalScope(InteractionConfidentialScope::class))->toBeTrue();
+    expect(Interaction::hasGlobalScope(InteractionConfidentialScope::class))->toBeTrue();
 });
 
-test('InteractionsRelationManager with display data for created user', function () {
+test('Interactions model with fetch data count for created user', function () {
     $user = User::factory()->licensed(LicenseType::cases())->create();
     $user->givePermissionTo('interaction.view-any');
 
     actingAs($user);
-    $confidentialInteraction = Interaction::factory()->count(10)->create([
+    Interaction::factory()->count(10)->create([
         'is_confidential' => true,
         'user_id' => $user,
     ]);
 
-    $otherConfidentialInteraction = Interaction::factory()->count(10)->create([
+    Interaction::factory()->count(10)->create([
         'is_confidential' => true,
     ]);
 
-    $nonConfidentialInteraction = Interaction::factory()->count(10)->create([
+    Interaction::factory()->count(10)->create([
         'is_confidential' => false,
     ]);
 
-    $student = Student::factory()
-        ->create();
-
-    $student->interactions()->saveMany($confidentialInteraction->merge($otherConfidentialInteraction)->merge($nonConfidentialInteraction));
-    $student->refresh();
-
-    livewire(
-        InteractionsRelationManager::class,
-        [
-            'ownerRecord' => $student,
-            'pageClass' => ViewStudent::class,
-        ]
-    )
-        ->set('tableRecordsPerPage', 20)
-        ->assertCanSeeTableRecords($confidentialInteraction->merge($nonConfidentialInteraction))
-        ->assertCanNotSeeTableRecords($otherConfidentialInteraction);
+    expect(Interaction::all()->count())->toBe(20);
 });
 
-test('InteractionsRelationManager with display data for team user', function () {
+test('Interactions model with fetch data count for team user', function () {
     $teamUser = User::factory()->licensed(LicenseType::cases())->create();
     $teamUser->givePermissionTo('interaction.view-any');
 
     $team = Team::factory()->hasAttached($teamUser, [], 'users')->create();
 
     actingAs($teamUser);
-    $confidentialInteraction = Interaction::factory()->hasAttached($team, [], 'confidentialAccessTeams')->count(10)->create([
+    Interaction::factory()->hasAttached($team, [], 'confidentialAccessTeams')->count(10)->create([
         'is_confidential' => true,
     ]);
 
-    $otherConfidentialInteraction = Interaction::factory()->count(10)->create([
+    Interaction::factory()->count(10)->create([
         'is_confidential' => true,
     ]);
-    $nonConfidentialInteraction = Interaction::factory()->count(10)->create([
+
+    Interaction::factory()->count(10)->create([
         'is_confidential' => false,
     ]);
 
-    $student = Student::factory()
-        ->create();
-
-    $student->interactions()->saveMany($confidentialInteraction->merge($otherConfidentialInteraction)->merge($nonConfidentialInteraction));
-    $student->refresh();
-
-    livewire(
-        InteractionsRelationManager::class,
-        [
-            'ownerRecord' => $student,
-            'pageClass' => ViewStudent::class,
-        ]
-    )
-        ->set('tableRecordsPerPage', 20)
-        ->assertCanSeeTableRecords($confidentialInteraction->merge($nonConfidentialInteraction))
-        ->assertCanNotSeeTableRecords($otherConfidentialInteraction);
+    expect(Interaction::all()->count())->toBe(20);
 });
 
-test('InteractionsRelationManager with display data for assigned user', function () {
+test('Interactions model with fetch data count for assigned user', function () {
     $user = User::factory()->licensed(LicenseType::cases())->create();
     $user->givePermissionTo('interaction.view-any');
 
     actingAs($user);
-    $confidentialInteraction = Interaction::factory()->hasAttached($user, [], 'confidentialAccessUsers')->count(10)->create([
+    Interaction::factory()->hasAttached($user, [], 'confidentialAccessUsers')->count(10)->create([
         'is_confidential' => true,
     ]);
 
-    $otherConfidentialInteraction = Interaction::factory()->count(10)->create([
+    Interaction::factory()->count(10)->create([
         'is_confidential' => true,
     ]);
 
-    $nonConfidentialInteraction = Interaction::factory()->count(10)->create([
+    Interaction::factory()->count(10)->create([
         'is_confidential' => false,
     ]);
 
-    $student = Student::factory()
-        ->create();
-
-    $student->interactions()->saveMany($confidentialInteraction->merge($otherConfidentialInteraction)->merge($nonConfidentialInteraction));
-    $student->refresh();
-
-    livewire(
-        InteractionsRelationManager::class,
-        [
-            'ownerRecord' => $student,
-            'pageClass' => ViewStudent::class,
-        ]
-    )
-        ->set('tableRecordsPerPage', 20)
-        ->assertCanSeeTableRecords($confidentialInteraction->merge($nonConfidentialInteraction))
-        ->assertCanNotSeeTableRecords($otherConfidentialInteraction);
+    expect(Interaction::all()->count())->toBe(20);
 });
 
-test('InteractionsRelationManager with display all data for superadmin user', function () {
+test('Interactions model with fetch data count for superadmin user', function () {
     asSuperAdmin();
-    $confidentialInteraction = Interaction::factory()->count(10)->create([
+    Interaction::factory()->count(10)->create([
         'is_confidential' => true,
     ]);
 
-    $nonConfidentialInteraction = Interaction::factory()->count(10)->create([
+    Interaction::factory()->count(10)->create([
         'is_confidential' => false,
     ]);
 
-    $student = Student::factory()
-        ->create();
-
-    $student->interactions()->saveMany($confidentialInteraction->merge($nonConfidentialInteraction));
-    $student->refresh();
-
-    livewire(
-        InteractionsRelationManager::class,
-        [
-            'ownerRecord' => $student,
-            'pageClass' => ViewStudent::class,
-        ]
-    )
-        ->set('tableRecordsPerPage', 20)
-        ->assertCanSeeTableRecords($confidentialInteraction->merge($nonConfidentialInteraction));
+    expect(Interaction::all()->count())->toBe(20);
 });
