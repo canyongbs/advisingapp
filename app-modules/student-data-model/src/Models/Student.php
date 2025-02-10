d3ed1d27df20f0f1679d3cb14ea49cb91ca822cf
Ankit Kabra <ankit.kabra@canyongbs.com>
2025-02-10 17:39:42 +0000

    feat: add primaryEmail and primaryPhone relationships to Student model

--- app-modules/student-data-model/src/Models/Student.php
+++ app-modules/student-data-model/src/Models/Student.php
@@ -359,6 +359,16 @@ class Student extends BaseAuthenticatable implements Auditable, Subscribable, Ed
         return $this->belongsTo(StudentAddress::class, 'primary_address_id', 'id');
     }
 
+    public function primaryEmail()
+    {
+        return $this->belongsTo(StudentEmailAddress::class, 'primary_email');
+    }
+
+    public function primaryPhone()
+    {
+        return $this->belongsTo(StudentPhoneNumber::class, 'primary_phone');
+    }
+
     public static function getLabel(): string
     {
         return 'student';
