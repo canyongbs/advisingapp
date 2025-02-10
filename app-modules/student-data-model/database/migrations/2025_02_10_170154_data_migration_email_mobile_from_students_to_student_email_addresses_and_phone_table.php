ee8eb2a534e2a231f754bc52f29136805af4fcab
Ankit Kabra <ankit.kabra@canyongbs.com>
2025-02-10 17:36:45 +0000

    feat: add 'order' field to address, phone number, and email models for prospects and students

---
+++ app-modules/student-data-model/database/migrations/2025_02_10_170154_data_migration_email_mobile_from_students_to_student_email_addresses_and_phone_table.php
@@ -0,0 +1,119 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+return new class () extends Migration {
+    public function up(): void
+    {
+        DB::beginTransaction();
+
+        DB::table('students')
+            ->select('sisid', 'email', 'email_2', 'mobile', 'phone', 'address', 'address2', 'address3', 'city', 'state', 'postal')
+            ->orderBy('sisid', 'asc')
+            ->chunk(100, function ($students) {
+                foreach ($students as $student) {
+                    DB::table('student_email_addresses')
+                        ->insert([
+                            'id' => (string) Str::orderedUuid(),
+                            'sisid' => $student->sisid,
+                            'address' => $student->email,
+                            'type' => 'Personal',
+                            'order' => 1
+                        ]);
+
+
+                    if(!blank($student->email_2)) {
+                        DB::table('student_email_addresses')
+                        ->insert([
+                            'id' => (string) Str::orderedUuid(),
+                            'sisid' => $student->sisid,
+                            'address' => $student->email_2,
+                            'type' => 'Other',
+                            'order' => 2
+                        ]);
+                    }
+
+                    if (! blank($student->mobile)) {
+                        DB::table('student_phone_numbers')
+                            ->insert([
+                                'id' => (string) Str::orderedUuid(),
+                                'sisid' => $student->sisid,
+                                'number' => $student->mobile,
+                                'can_recieve_sms' => true,
+                                'type' => 'Mobile',
+                                'order' => 1
+                            ]);
+                    }
+
+                    if (! blank($student->phone)) {
+                        DB::table('student_phone_numbers')
+                            ->insert([
+                                'id' => (string) Str::orderedUuid(),
+                                'sisid' => $student->sisid,
+                                'number' => $student->phone,
+                                'can_recieve_sms' => false,
+                                'type' => 'Phone',
+                                'order' => 2
+                            ]);
+                    }
+
+                    if (! blank($student->address)) {
+                        DB::table('student_addresses')
+                            ->insert([
+                                'id' => (string) Str::orderedUuid(),
+                                'sisid' => $student->sisid,
+                                'line_1' => $student->address,
+                                'line_2' => $student->address2,
+                                'line_3' => $student->address3,
+                                'city' => $student->city,
+                                'state' => $student->state,
+                                'postal' => $student->postal,
+                                'order' => 1
+                            ]);
+                    }
+
+                    $primaryEmail = DB::table('student_email_addresses')
+                        ->where('sisid', $student->sisid)
+                        ->first();
+
+                    $primaryPhone = DB::table('student_phone_numbers')
+                        ->where('sisid', $student->sisid)
+                        ->first();
+
+                    DB::table('students')
+                        ->where('sisid', $student->sisid)
+                        ->update([
+                            'primary_email' => $primaryEmail->id ?? null,
+                            'primary_phone' => $primaryPhone->id ?? null,
+                        ]);
+                }
+            });
+
+        DB::commit();
+    }
+
+    public function down(): void
+    {
+        DB::beginTransaction();
+
+        DB::table('students')
+            ->update([
+                'primary_email' => null,
+                'primary_phone' => null,
+            ]);
+
+        DB::table('student_email_addresses')
+            ->delete();
+
+        DB::table('student_phone_numbers')
+            ->delete();
+
+        DB::table('student_addresses')
+            ->delete();
+
+        DB::commit();
+    }
+};
+
