f288f4b4c5bfb5e2a170e7e5e20c1106e2070fe5
Ankit Kabra <ankit.kabra@canyongbs.com>
2025-02-14 14:37:07 +0000

    Fix event attendee factory

--- app-modules/meeting-center/database/factories/EventAttendeeFactory.php
+++ app-modules/meeting-center/database/factories/EventAttendeeFactory.php
@@ -57,8 +57,8 @@ class EventAttendeeFactory extends Factory
             'status' => fake()->randomElement(EventAttendeeStatus::class),
             'email' => fake()->unique()->randomElement([
                 fake()->email(),
-                Student::factory()->create()->value('email'),
-                Prospect::factory()->create()->value('email'),
+                Student::factory()->create()->primaryEmail?->address,
+                Prospect::factory()->create()->primaryEmail?->address
             ]),
             'event_id' => Event::inRandomOrder()->first() ?? Event::factory()->create(),
         ];
