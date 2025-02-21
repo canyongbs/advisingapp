47ea8d369fd2db8540a5cc476fa6cfd458dde782
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2025-02-21 22:05:06 +0000

    Copy over InboundEmail to job from test
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- app-modules/engagement/src/Jobs/ProcessSesS3InboundEmail.php
+++ app-modules/engagement/src/Jobs/ProcessSesS3InboundEmail.php
@@ -2,14 +2,22 @@
 
 namespace AdvisingApp\Engagement\Jobs;
 
+use AdvisingApp\Engagement\Enums\EngagementResponseType;
+use AdvisingApp\Prospect\Models\Prospect;
+use AdvisingApp\StudentDataModel\Models\Student;
 use App\Features\InboundEmailsUpdates;
+use App\Models\Tenant;
 use Aws\Crypto\KmsMaterialsProviderV2;
 use Aws\Kms\KmsClient;
 use Aws\S3\Crypto\S3EncryptionClientV2;
 use Aws\S3\S3Client;
+use Exception;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use PhpMimeMailParser\Attachment;
 use PhpMimeMailParser\Parser;
 use Spatie\Multitenancy\Jobs\NotTenantAware;
 
@@ -81,10 +89,92 @@ class ProcessSesS3InboundEmail implements ShouldQueue, ShouldBeUnique, NotTenant
         $parser = (new Parser())
             ->setText($content);
 
-        $arrayHeaderTo = $parser->getAddresses('to');
+        $matchedTenants = collect($parser->getAddresses('to'))
+            ->pluck('address')
+            ->map(function (string $address) {
+                $localPart = filter_var($address, FILTER_VALIDATE_EMAIL) ? explode('@', $address)[0] : null;
 
-        // $text = $parser->getMessageBody('htmlEmbedded');
+                if ($localPart === null) {
+                    return null;
+                }
 
-        var_dump($parser->getHeaders());
+                return Tenant::query()
+                    ->where(
+                        DB::raw('LOWER(domain)'),
+                        'like',
+                        strtolower("{$localPart}.%")
+                    )
+                    ->first();
+            })
+            ->filter();
+
+        // TODO: Move the email to a failed folder and throw a custom exceptions saying that a Tenant match could not be found.
+        throw_if($matchedTenants->isEmpty(), new Exception('No matching tenants found'));
+
+        $sender = $parser->getAddresses('from')[0]['address'];
+
+        $matchedTenants->each(function (Tenant $tenant) use ($parser, $content, $sender) {
+            $tenant->execute(function () use ($parser, $content, $sender) {
+                $students = Student::query()
+                    // This will need to be changed when we refactor email into a different table
+                    ->where('email', $sender)
+                    ->get();
+
+                if ($students->isNotEmpty()) {
+                    $students->each(function (Student $student) use ($parser, $content) {
+                        /** @var EngagementResponse $engagementResponse */
+                        $engagementResponse = $student->engagementResponses()
+                            ->create([
+                                'subject' => $parser->getHeader('subject'),
+                                'content' => $parser->getMessageBody('htmlEmbedded'),
+                                'sent_at' => $parser->getHeader('date'),
+                                'type' => EngagementResponseType::Email,
+                                'raw' => $content,
+                            ]);
+
+                        collect($parser->getAttachments())->each(function (Attachment $attachment) use ($engagementResponse) {
+                            $engagementResponse->addMediaFromStream($attachment->getStream())
+                                ->toMediaCollection('attachments');
+                        });
+                    });
+
+                    Storage::disk('s3-inbound-email')->delete($this->emailFilePath);
+
+                    // If we found students and added records, we can stop here as Student records take final precedence over Prospect records.
+                    return;
+                }
+
+                $prospects = Prospect::query()
+                    // This will need to be changed when we refactor email into a different table
+                    ->where('email', $sender)
+                    ->get();
+
+                if ($prospects->isEmpty()) {
+                    // TODO: Move the email to a failed folder and throw a custom exceptions saying that no matching Educatable record could be found.
+                }
+
+                $prospects->each(function (Prospect $prospect) use ($parser, $content) {
+                    /** @var EngagementResponse $engagementResponse */
+                    $engagementResponse = $prospect->engagementResponses()
+                        ->create([
+                            'subject' => $parser->getHeader('subject'),
+                            'content' => $parser->getMessageBody('htmlEmbedded'),
+                            'sent_at' => $parser->getHeader('date'),
+                            'type' => EngagementResponseType::Email,
+                            'raw' => $content,
+                        ]);
+
+                    collect($parser->getAttachments())->each(function (Attachment $attachment) use ($engagementResponse) {
+                        $engagementResponse->addMediaFromStream($attachment->getStream())
+                            ->setName($attachment->getFilename())
+                            ->setFileName($attachment->getFilename())
+                            ->toMediaCollection('attachments');
+                    });
+                });
+
+                // Throw an error if file failed to delete?
+                Storage::disk('s3-inbound-email')->delete($this->emailFilePath);
+            });
+        });
     }
 }
