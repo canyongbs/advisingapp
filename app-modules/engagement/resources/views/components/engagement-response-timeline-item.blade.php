645a30d9179ff9e326567168e4f064ce6e829796
Orrison <Orrison@users.noreply.github.com>
2025-02-25 21:46:34 +0000

    chore: fix code style

--- app-modules/engagement/resources/views/components/engagement-response-timeline-item.blade.php
+++ app-modules/engagement/resources/views/components/engagement-response-timeline-item.blade.php
@@ -55,7 +55,7 @@
     <div
         class="my-4 rounded-lg border-2 border-gray-200 p-2 text-base font-normal text-gray-500 dark:border-gray-800 dark:text-gray-400">
         <div class="flex flex-col">
-            @if (! empty($record->subject))
+            @if (!empty($record->subject))
                 <p class="text-xs text-gray-400 dark:text-gray-500">Subject:</p>
                 <p>{{ $record->subject }}</p>
             @endif
