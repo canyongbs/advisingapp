cbb7a308cd7a682153032ce5dff68029d3d7cb8d
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2025-02-28 21:14:41 +0000

    Add cssFileName property to widget configurations
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- widgets/form/vite.config.js
+++ widgets/form/vite.config.js
@@ -43,6 +43,7 @@ export default defineConfig({
             entry: resolve(__dirname, 'src/widget.js'),
             name: 'AdvisingAppFormWidget',
             fileName: 'advising-app-form-widget',
+            cssFileName: 'style',
             formats: ['es'],
         },
         outDir: resolve(__dirname, '../../public/js/widgets/form'),
