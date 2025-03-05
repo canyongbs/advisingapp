92ff9596d929f6f70218b56e70e18db2fcb09f60
Dan Harrin <dan.harrin@canyongbs.com>
2025-03-05 14:45:38 +0000

    Import cancellations
    Signed-off-by: Dan Harrin <dan.harrin@canyongbs.com>

--- app-modules/student-data-model/src/Filament/Imports/StudentImporter.php
+++ app-modules/student-data-model/src/Filament/Imports/StudentImporter.php
@@ -248,4 +248,9 @@ class StudentImporter extends Importer
 
         return $body;
     }
+
+    public function getJobBatchName(): ?string
+    {
+        return "student-import-{$this->getImport()->getKey()}";
+    }
 }
