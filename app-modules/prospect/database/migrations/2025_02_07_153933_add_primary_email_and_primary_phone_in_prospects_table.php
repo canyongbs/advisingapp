8aefa9136de7712e29d1e8685f832675eeff9d33
Dan Harrin <dan.harrin@canyongbs.com>
2025-03-10 17:53:14 +0000

    Apply suggestions
    Signed-off-by: Dan Harrin <dan.harrin@canyongbs.com>

--- app-modules/prospect/database/migrations/2025_02_07_153933_add_primary_email_and_primary_phone_in_prospects_table.php
+++ app-modules/prospect/database/migrations/2025_02_07_153933_add_primary_email_and_primary_phone_in_prospects_table.php
@@ -42,16 +42,18 @@ return new class () extends Migration {
     public function up(): void
     {
         Schema::table('prospects', function (Blueprint $table) {
-            $table->foreignUuid('primary_email_id')->nullable()->constrained('prospect_email_addresses')->cascadeOnDelete();
-            $table->foreignUuid('primary_phone_id')->nullable()->constrained('prospect_phone_numbers')->cascadeOnDelete();
-            $table->foreignUuid('primary_address_id')->nullable()->constrained('prospect_addresses')->cascadeOnDelete();
+            $table->foreignUuid('primary_email_id')->nullable()->constrained('prospect_email_addresses')->nullOnDelete();
+            $table->foreignUuid('primary_phone_id')->nullable()->constrained('prospect_phone_numbers')->nullOnDelete();
+            $table->foreignUuid('primary_address_id')->nullable()->constrained('prospect_addresses')->nullOnDelete();
         });
     }
 
     public function down(): void
     {
         Schema::table('prospects', function (Blueprint $table) {
-            $table->dropColumn(['primary_email_id', 'primary_phone_id', 'primary_address_id']);
+            $table->dropConstrainedForeignId('primary_email_id');
+            $table->dropConstrainedForeignId('primary_phone_id');
+            $table->dropConstrainedForeignId('primary_address_id');
         });
     }
 };
