afe9bdad02ff6809cfde51f0c3728e2b6932d8e4
Orrison <Orrison@users.noreply.github.com>
2025-03-12 18:46:48 +0000

    chore: fix code style

--- app-modules/ai/database/migrations/2025_03_12_183223_data_activate_o1_mini_and_o3_mini_feature.php
+++ app-modules/ai/database/migrations/2025_03_12_183223_data_activate_o1_mini_and_o3_mini_feature.php
@@ -37,8 +37,7 @@
 use App\Features\O1MiniAndO3MiniFeature;
 use Illuminate\Database\Migrations\Migration;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     public function up(): void
     {
         O1MiniAndO3MiniFeature::activate();
