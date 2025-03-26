d605f7d3ddf286006070ea747c8220e19bc55f1f
Orrison <Orrison@users.noreply.github.com>
2025-03-26 12:55:37 +0000

    chore: fix code style

--- app-modules/student-data-model/src/Models/Student.php
+++ app-modules/student-data-model/src/Models/Student.php
@@ -74,6 +74,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
