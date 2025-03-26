65508c7cebe4a16de7fd0353facf3f1418537e7a
Orrison <Orrison@users.noreply.github.com>
2025-03-26 05:26:05 +0000

    chore: fix code style

--- app-modules/care-team/database/migrations/2025_03_26_051339_data_activate_care_team_role_feature.php
+++ app-modules/care-team/database/migrations/2025_03_26_051339_data_activate_care_team_role_feature.php
@@ -37,8 +37,7 @@
 use App\Features\CareTeamRoleFeature;
 use Illuminate\Database\Migrations\Migration;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     public function up(): void
     {
         CareTeamRoleFeature::activate();
