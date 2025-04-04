58c2f04d1b9f79a82eae4c6d6446d1f91bc4d41b
Orrison <Orrison@users.noreply.github.com>
2025-04-04 14:52:26 +0000

    chore: fix code style

--- app-modules/student-data-model/resources/views/filament/resources/student-resource/view-student.blade.php
+++ app-modules/student-data-model/resources/views/filament/resources/student-resource/view-student.blade.php
@@ -138,7 +138,7 @@
                         .title('Error while loading page')
                         .body(
                             'There was an error rendering some information on the holistic student profile page. We are tracking this error on the back end and will work on getting this fixed.'
-                            )
+                        )
                         .danger()
                         .send()
                 })
