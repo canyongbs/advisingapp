2514c36db78f547b6e483193893c69b1cea99e46
Dan Harrin <dan.harrin@canyongbs.com>
2025-04-17 12:57:36 +0000

    [ADVAPP-1346]: When creating emails but canceling send, the emails persist unxpectedly
    Signed-off-by: Dan Harrin <dan.harrin@canyongbs.com>

--- app-modules/engagement/src/Filament/Actions/SendEngagementAction.php
+++ app-modules/engagement/src/Filament/Actions/SendEngagementAction.php
@@ -70,6 +70,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Query\Expression;
 use Illuminate\Support\Carbon;
+use Livewire\Component;
 use Livewire\Features\SupportFileUploads\TemporaryUploadedFile;
 
 class SendEngagementAction extends Action
@@ -343,7 +344,7 @@ class SendEngagementAction extends Action
                     ->color('gray')
                     ->cancelParentActions()
                     ->requiresConfirmation()
-                    ->action(fn () => null)
+                    ->action(fn (Component $livewire) => $livewire->js('$store.previous = {}')) // This fixes an issue where the TipTap editor inside this modal is persisted after the modal is closed, and the old content is restored to the editor. This can be removed when the app is upgraded to Filament v4.
                     ->modalSubmitAction(fn (StaticAction $action) => $action->color('danger')),
             ]);
     }
