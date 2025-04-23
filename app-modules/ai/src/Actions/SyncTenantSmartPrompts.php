0e8f45be4d31b6dc732b0ff96ad49bc82f6f7b1c
Dan Harrin <dan.harrin@canyongbs.com>
2025-04-23 16:53:51 +0000

    Ignore PHPStan errors
    Signed-off-by: Dan Harrin <dan.harrin@canyongbs.com>

--- app-modules/ai/src/Actions/SyncTenantSmartPrompts.php
+++ app-modules/ai/src/Actions/SyncTenantSmartPrompts.php
@@ -56,13 +56,15 @@ class SyncTenantSmartPrompts
                     );
 
                 foreach ($smartPromptCategory['smart_prompts'] ?? [] as $smartPrompt) {
+                    /** @var Prompt $prompt */
                     $prompt = Prompt::find($smartPrompt['id']) ?? new Prompt();
+
                     $prompt->id = $smartPrompt['id'];
-                    $prompt->title = $smartPrompt['title'];
-                    $prompt->description = $smartPrompt['description'] ?? null;
-                    $prompt->prompt = $smartPrompt['prompt'];
-                    $prompt->type_id = $promptType->getKey();
-                    $prompt->is_smart = true;
+                    $prompt->title = $smartPrompt['title']; /** @phpstan-ignore property.notFound */
+                    $prompt->description = $smartPrompt['description'] ?? null; /** @phpstan-ignore property.notFound */
+                    $prompt->prompt = $smartPrompt['prompt']; /** @phpstan-ignore property.notFound */
+                    $prompt->type_id = $promptType->getKey(); /** @phpstan-ignore property.notFound */
+                    $prompt->is_smart = true; /** @phpstan-ignore property.notFound */
                     $prompt->save();
 
                     $promptIds[] = $smartPrompt['id'];
