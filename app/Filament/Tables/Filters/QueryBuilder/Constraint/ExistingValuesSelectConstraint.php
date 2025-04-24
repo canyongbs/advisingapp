f462ee748d2cb5855cf73c791a0824f969924bca
Orrison <Orrison@users.noreply.github.com>
2025-04-24 11:46:26 +0000

    chore: fix code style

--- app/Filament/Tables/Filters/QueryBuilder/Constraint/ExistingValuesSelectConstraint.php
+++ app/Filament/Tables/Filters/QueryBuilder/Constraint/ExistingValuesSelectConstraint.php
@@ -36,12 +36,12 @@
 
 namespace App\Filament\Tables\Filters\QueryBuilder\Constraints;
 
-use Illuminate\Support\Str;
+use Filament\Tables\Filters\QueryBuilder\Constraints\Operators\IsFilledOperator;
+use Filament\Tables\Filters\QueryBuilder\Constraints\SelectConstraint;
+use Filament\Tables\Filters\QueryBuilder\Constraints\SelectConstraint\Operators\IsOperator;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Query\Expression;
-use Filament\Tables\Filters\QueryBuilder\Constraints\SelectConstraint;
-use Filament\Tables\Filters\QueryBuilder\Constraints\Operators\IsFilledOperator;
-use Filament\Tables\Filters\QueryBuilder\Constraints\SelectConstraint\Operators\IsOperator;
+use Illuminate\Support\Str;
 
 class ExistingValuesSelectConstraint extends SelectConstraint
 {
@@ -53,11 +53,11 @@ class ExistingValuesSelectConstraint extends SelectConstraint
             IsOperator::make()
                 ->modifyQueryUsing(function (IsOperator $operator, Builder $query, string $qualifiedColumn, array $settings): Builder {
                     $value = $settings[$this->isMultiple() ? 'values' : 'value'];
-            
+
                     if (is_array($value)) {
                         return $query->{$operator->isInverse() ? 'whereNotIn' : 'whereIn'}(new Expression("lower({$qualifiedColumn})"), $value);
                     }
-            
+
                     return $query->{$operator->isInverse() ? 'whereNot' : 'where'}(new Expression("lower({$qualifiedColumn})"), $value);
                 }),
             IsFilledOperator::make()
