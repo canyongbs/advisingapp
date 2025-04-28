8c06a0ee9c36c834bb747c9744d4488dea877da8
Karl Murray <karl.murray@canyongbs.com>
2025-04-28 17:04:53 +0000

    ADVAPP-1138 - Start on migrations

---
+++ database/migrations/2025_04_28_141824_add_team_id_to_users_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Tpetry\PostgresqlEnhanced\Schema\Blueprint;
+use Tpetry\PostgresqlEnhanced\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignUuid('team_id')
+                ->nullable()
+                ->constrained('teams');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['team_id']);
+            $table->dropColumn('team_id');
+        });
+    }
+};
