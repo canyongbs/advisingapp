ae75a328fa1df91f8999c1b68f1a7d6e6a58b874
Karl Murray <karl.murray@canyongbs.com>
2025-04-30 21:21:02 +0000

    ADVAPP-1138 - Fix migration

--- database/migrations/2025_04_30_200449_tmp_data_migrate_team_user_id_to_user_table.php
+++ database/migrations/2025_04_30_200449_tmp_data_migrate_team_user_id_to_user_table.php
@@ -41,21 +41,25 @@ return new class () extends Migration {
     public function up(): void
     {
         DB::table('team_user')->chunkById(100, function ($teamUser) {
-            DB::table('users')
-                ->where('id', $teamUser->user_id)  // @phpstan-ignore-line
-                ->update(['team_id' => $teamUser->team_id]);  // @phpstan-ignore-line
+            $teamUser->each(function ($teamUser) {
+                DB::table('users')
+                    ->where('id', $teamUser->user_id)  // @phpstan-ignore-line
+                    ->update(['team_id' => $teamUser->team_id]);  // @phpstan-ignore-line
+            });
         });
     }
 
     public function down(): void
     {
         DB::table('users')->chunkById(100, function ($teamUser) {
-            DB::table('team_user')->insert([
-                'team_id' => $teamUser->team_id,  // @phpstan-ignore-line
-                'user_id' => $teamUser->user_id,  // @phpstan-ignore-line
-                'updated_at' => $teamUser->updated_at,  // @phpstan-ignore-line
-                'created_at' => $teamUser->created_at,  // @phpstan-ignore-line
-            ]);
+            $teamUser->each(function ($teamUser) {
+                DB::table('team_user')->insert([
+                    'team_id' => $teamUser->team_id,  // @phpstan-ignore-line
+                    'user_id' => $teamUser->user_id,  // @phpstan-ignore-line
+                    'updated_at' => $teamUser->updated_at,  // @phpstan-ignore-line
+                    'created_at' => $teamUser->created_at,  // @phpstan-ignore-line
+                ]);
+            });
         });
     }
 };
