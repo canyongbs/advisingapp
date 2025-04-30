fea6ed93b0c3c92d9618a07b358d9b49840681a2
Ankit Kabra <ankit.kabra@canyongbs.com>
2025-04-30 19:11:35 +0000

    Refactor engagement body and timeline item styles for improved typography and consistency

--- app-modules/engagement/resources/views/components/engagement-timeline-item.blade.php
+++ app-modules/engagement/resources/views/components/engagement-timeline-item.blade.php
@@ -84,7 +84,19 @@
         @endif
         <div class="flex flex-col">
             <p class="text-xs text-gray-400 dark:text-gray-500">Body:</p>
-            <div class="prose prose-h1:text-3xl prose-h1:text-3xl prose-h1:leading-[1.3] prose-h2:text-2xl prose-h2:mt-5 prose-h2:mb-5 prose-h2:leading-[1.1] prose-h3:text-xl prose-h3:font-bold prose-h3:leading-[1.6] prose-h3:mb-0 prose-h4:text-lg prose-h4:font-bold prose-h4:leading-[2.5] prose-h4:mt-2 prose-h4:mb-1 prose-h5:text-base prose-h5:font-bold prose-h5:leading-[1.5] prose-h5:mb-1 prose-h6:text-sm prose-h6:font-bold prose-h6:leading-[3] prose-h6:mb-1 prose-hr:mt-4 prose-hr:mb-4 dark:prose-invert">
+            <div class="prose font-bold
+                prose-h1:mt-4 prose-h1:mb-4
+                prose-h2:mt-4 prose-h2:mb-4
+                prose-h3:mt-4 prose-h3:mb-4
+                prose-h4:mt-4 prose-h4:mb-4
+                prose-h5:mt-4 prose-h5:mb-4
+                prose-h6:mt-4 prose-h6:mb-4
+                prose-h1:leading-[1.5] prose-h2:leading-[1.5] prose-h3:leading-[1.5]
+                prose-h4:leading-[1.5] prose-h5:leading-[1.5] prose-h6:leading-[1.5]
+                prose-h4:text-lg prose-h5:text-base prose-h6:text-sm
+                prose-hr:mt-4 prose-hr:mb-4
+                dark:prose-invert"
+            >
                 {{ $record->getBody() }}
             </div>
         </div>
--- resources/views/infolists/components/engagement-body.blade.php
+++ resources/views/infolists/components/engagement-body.blade.php
@@ -1,5 +1,18 @@
 <x-dynamic-component :component="$getEntryWrapperView()" :entry="$entry">
-    <div class="prose prose-h1:text-3xl prose-h1:text-3xl prose-h1:leading-[1.3] prose-h2:text-2xl prose-h2:mt-5 prose-h2:mb-5 prose-h2:leading-[1.1] prose-h3:text-xl prose-h3:font-bold prose-h3:leading-[1.6] prose-h3:mb-0 prose-h4:text-lg prose-h4:font-bold prose-h4:leading-[2.5] prose-h4:mt-2 prose-h4:mb-1 prose-h5:text-base prose-h5:font-bold prose-h5:leading-[1.5] prose-h5:mb-1 prose-h6:text-sm prose-h6:font-bold prose-h6:leading-[3] prose-h6:mb-1 prose-hr:mt-4 prose-hr:mb-4 dark:prose-invert">
-        {{ $getState() }}
-    </div>
+    <div class="prose font-bold
+    prose-h1:mt-4 prose-h1:mb-4
+    prose-h2:mt-4 prose-h2:mb-4
+    prose-h3:mt-4 prose-h3:mb-4
+    prose-h4:mt-4 prose-h4:mb-4
+    prose-h5:mt-4 prose-h5:mb-4
+    prose-h6:mt-4 prose-h6:mb-4
+    prose-h1:leading-[1.5] prose-h2:leading-[1.5] prose-h3:leading-[1.5]
+    prose-h4:leading-[1.5] prose-h5:leading-[1.5] prose-h6:leading-[1.5]
+    prose-h4:text-lg prose-h5:text-base prose-h6:text-sm
+    prose-hr:mt-4 prose-hr:mb-4
+    dark:prose-invert"
+>
+    {{ $getState() }}
+</div>
+
 </x-dynamic-component>
