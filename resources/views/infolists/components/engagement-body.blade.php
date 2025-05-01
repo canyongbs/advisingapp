0bee0d8c1aee129ebf6d26c1294067a9fdd69ffa
Ankit Kabra <ankit.kabra@canyongbs.com>
2025-05-01 08:41:42 +0000

    Refactor typography styles for engagement components to enhance readability and consistency

--- resources/views/infolists/components/engagement-body.blade.php
+++ resources/views/infolists/components/engagement-body.blade.php
@@ -8,8 +8,8 @@
     prose-h6:mt-4 prose-h6:mb-4
     prose-h1:leading-[1.5] prose-h2:leading-[1.5] prose-h3:leading-[1.5]
     prose-h4:leading-[1.5] prose-h5:leading-[1.5] prose-h6:leading-[1.5]
-    prose-h4:text-lg prose-h5:text-base prose-h6:text-sm
-    prose-h5:font-medium prose-h6:font-medium
+    prose-h1:text-3xl prose-h4:text-lg prose-h5:text-base prose-h6:text-sm
+    prose-h1:font-bold prose-h5:font-medium prose-h6:font-medium
     prose-h5:text-[--tw-prose-headings] prose-h6:text-[--tw-prose-headings]
     prose-hr:mt-4 prose-hr:mb-4
     dark:prose-invert"
