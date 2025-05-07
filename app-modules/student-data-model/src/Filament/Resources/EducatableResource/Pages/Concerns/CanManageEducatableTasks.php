45b70c69371d2b4d67220d8989e539f2d08eff5c
kandarp-canyon <kandarp-canyon@users.noreply.github.com>
2025-05-07 18:43:32 +0000

    chore: fix code style

--- app-modules/student-data-model/src/Filament/Resources/EducatableResource/Pages/Concerns/CanManageEducatableTasks.php
+++ app-modules/student-data-model/src/Filament/Resources/EducatableResource/Pages/Concerns/CanManageEducatableTasks.php
@@ -62,7 +62,6 @@ use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
 
 trait CanManageEducatableTasks
 {
--- app-modules/task/src/Filament/RelationManagers/BaseTaskRelationManager.php
+++ app-modules/task/src/Filament/RelationManagers/BaseTaskRelationManager.php
@@ -63,7 +63,6 @@ use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
 
 abstract class BaseTaskRelationManager extends ManageRelatedRecords
 {
