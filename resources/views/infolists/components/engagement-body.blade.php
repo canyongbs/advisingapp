a5c4ceecb60f1a909852a94b9db41129d0d12b77
Orrison <Orrison@users.noreply.github.com>
2025-05-09 14:42:29 +0000

    chore: fix code style

--- resources/views/infolists/components/engagement-body.blade.php
+++ resources/views/infolists/components/engagement-body.blade.php
@@ -31,21 +31,13 @@
 
 </COPYRIGHT>
 --}}
-<x-dynamic-component :component="$getEntryWrapperView()" :entry="$entry">
-    <div class="prose
-    prose-h1:my-4
-    prose-h2:my-4
-    prose-h3:my-4
-    prose-h4:my-4
-    prose-h5:my-4
-    prose-h6:my-4
-    prose-h1:text-3xl prose-h2:text-2xl prose-h3:text-xl prose-h4:text-lg prose-h5:text-base prose-h6:text-sm
-    prose-h1:font-bold prose-h5:font-medium prose-h6:font-medium
-    prose-h5:text-[--tw-prose-headings] prose-h6:text-[--tw-prose-headings]
-    prose-hr:my-4
-    dark:prose-invert"
+<x-dynamic-component
+    :component="$getEntryWrapperView()"
+    :entry="$entry"
 >
-    {{ $getState() }}
-</div>
+    <div
+        class="prose dark:prose-invert prose-h1:my-4 prose-h1:text-3xl prose-h1:font-bold prose-h2:my-4 prose-h2:text-2xl prose-h3:my-4 prose-h3:text-xl prose-h4:my-4 prose-h4:text-lg prose-h5:my-4 prose-h5:text-base prose-h5:font-medium prose-h5:text-[--tw-prose-headings] prose-h6:my-4 prose-h6:text-sm prose-h6:font-medium prose-h6:text-[--tw-prose-headings] prose-hr:my-4">
+        {{ $getState() }}
+    </div>
 
 </x-dynamic-component>
