5c820ece465bc1f16cad91cccf20e848e780339e
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2025-05-14 19:04:08 +0000

    Make sure to use renameColumn not rename
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- app-modules/campaign/database/migrations/2025_05_14_184000_modify_timestamp_columns_of_campaign_actions_table.php
+++ app-modules/campaign/database/migrations/2025_05_14_184000_modify_timestamp_columns_of_campaign_actions_table.php
@@ -10,10 +10,10 @@ return new class () extends Migration {
     {
         DB::transaction(function () {
             Schema::table('campaign_actions', function (Blueprint $table) {
-                $table->rename('last_execution_attempt_at', 'execution_dispatched_at');
+                $table->renameColumn('last_execution_attempt_at', 'execution_dispatched_at');
                 $table->timestampTz('execution_dispatched_at')->nullable()->change();
 
-                $table->rename('successfully_executed_at', 'execution_finished_at');
+                $table->renameColumn('successfully_executed_at', 'execution_finished_at');
                 $table->timestampTz('execution_finished_at')->nullable()->change();
 
                 $table->dropColumn('last_execution_attempt_error');
@@ -25,10 +25,10 @@ return new class () extends Migration {
     {
         DB::transaction(function () {
             Schema::table('campaign_actions', function (Blueprint $table) {
-                $table->rename('execution_dispatched_at', 'last_execution_attempt_at');
+                $table->renameColumn('execution_dispatched_at', 'last_execution_attempt_at');
                 $table->timestamp('last_execution_attempt_at')->nullable()->change();
 
-                $table->rename('execution_finished_at', 'successfully_executed_at');
+                $table->renameColumn('execution_finished_at', 'successfully_executed_at');
                 $table->timestamp('successfully_executed_at')->nullable()->change();
 
                 $table->string('last_execution_attempt_error')->nullable();
