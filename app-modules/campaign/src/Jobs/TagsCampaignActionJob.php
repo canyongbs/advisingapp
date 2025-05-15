936d929d9d3446e9feb8ae9779c76ef75f8630b6
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2025-05-15 20:46:51 +0000

    Update the jobs to make sure they update the action tracking Model
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- app-modules/campaign/src/Jobs/TagsCampaignActionJob.php
+++ app-modules/campaign/src/Jobs/TagsCampaignActionJob.php
@@ -4,23 +4,39 @@ namespace AdvisingApp\Campaign\Jobs;
 
 use AdvisingApp\StudentDataModel\Models\Contracts\Educatable;
 use Exception;
+use Illuminate\Support\Facades\DB;
+use Throwable;
 
 class TagsCampaignActionJob extends ExecuteCampaignActionOnEducatableJob
 {
     public function handle(): void
     {
-        $educatable = $this->actionEducatable->educatable;
+        try {
+            DB::beginTransaction();
 
-        throw_if(
-            ! $educatable instanceof Educatable,
-            new Exception('The educatable model must implement the Educatable contract.')
-        );
+            $educatable = $this->actionEducatable->educatable;
 
-        $educatable
-            ->tags()
-            ->sync(
-                ids: $this->actionEducatable->campaignAction->data['tag_ids'],
-                detaching: $this->actionEducatable->campaignAction->data['remove_prior']
+            throw_if(
+                ! $educatable instanceof Educatable,
+                new Exception('The educatable model must implement the Educatable contract.')
             );
+
+            $educatable
+                ->tags()
+                ->sync(
+                    ids: $this->actionEducatable->campaignAction->data['tag_ids'],
+                    detaching: $this->actionEducatable->campaignAction->data['remove_prior']
+                );
+
+            // Because we are just attaching tags, we don't create anything other than the pivot record.
+            // So we don't need to relate any records.
+            $this->actionEducatable->markSucceeded();
+        } catch (Throwable $e) {
+            DB::rollBack();
+
+            $this->actionEducatable->markFailed();
+
+            throw $e;
+        }
     }
 }
