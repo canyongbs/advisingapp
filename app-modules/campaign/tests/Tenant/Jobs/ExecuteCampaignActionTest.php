58ad7a2c23f0a6ea46e635cfef8c293f3af91b41
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2025-05-16 17:04:56 +0000

    Make the CampaignActionEducatable re-use test a single random test
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- app-modules/campaign/tests/Tenant/Jobs/ExecuteCampaignActionTest.php
+++ app-modules/campaign/tests/Tenant/Jobs/ExecuteCampaignActionTest.php
@@ -123,13 +123,20 @@ it('dispatches the correct job based on the CampaignAction type into the batch',
         ],
     ]);
 
-it('re-uses the same CampaignActionEducatable if it already exists', function (SegmentModel $segmentModel, Collection $educatables, CampaignActionType $actionType) {
+it('re-uses the same CampaignActionEducatable if it already exists', function () {
     Bus::fake();
+
+    $segmentModel = SegmentModel::cases()[array_rand(SegmentModel::cases())];
 
     $segment = Segment::factory()->create([
         'type' => SegmentType::Static,
         'model' => $segmentModel,
     ]);
+
+    $educatables = match ($segmentModel) {
+        SegmentModel::Student => Student::factory()->count(rand(1, 10))->create(),
+        SegmentModel::Prospect => Prospect::factory()->count(rand(1, 10))->create(),
+    };
 
     $educatables->each(function (Model $educatable) use ($segment) {
         $segment->subjects()->create([
@@ -147,7 +154,7 @@ it('re-uses the same CampaignActionEducatable if it already exists', function (S
     $action = CampaignAction::factory()
         ->for($campaign, 'campaign')
         ->create([
-            'type' => $actionType,
+            'type' => CampaignActionType::cases()[array_rand(CampaignActionType::cases())],
         ]);
 
     $educatables->each(function (Model $educatable) use ($action) {
@@ -173,52 +180,4 @@ it('re-uses the same CampaignActionEducatable if it already exists', function (S
             'educatable_type' => $educatable->getMorphClass(),
         ])
     );
-})
-    ->with([
-        'prospects' => [
-            SegmentModel::Prospect,
-            fn () => Prospect::factory()->count(rand(1, 10))->create(),
-        ],
-        'students' => [
-            SegmentModel::Student,
-            fn () => Student::factory()->count(rand(1, 10))->create(),
-        ],
-    ])
-    ->with([
-        'Engagement Email' => [
-            CampaignActionType::BulkEngagementEmail,
-            EngagementCampaignActionJob::class,
-        ],
-        'Engagement SMS' => [
-            CampaignActionType::BulkEngagementSms,
-            EngagementCampaignActionJob::class,
-        ],
-        'Event' => [
-            CampaignActionType::Event,
-            EventCampaignActionJob::class,
-        ],
-        'Alert' => [
-            CampaignActionType::ProactiveAlert,
-            ProactiveAlertCampaignActionJob::class,
-        ],
-        'Interaction' => [
-            CampaignActionType::Interaction,
-            InteractionCampaignActionJob::class,
-        ],
-        'Care Team' => [
-            CampaignActionType::CareTeam,
-            CareTeamCampaignActionJob::class,
-        ],
-        'Task' => [
-            CampaignActionType::Task,
-            TaskCampaignActionJob::class,
-        ],
-        'Subscription' => [
-            CampaignActionType::Subscription,
-            SubscriptionCampaignActionJob::class,
-        ],
-        'Tags' => [
-            CampaignActionType::Tags,
-            TagsCampaignActionJob::class,
-        ],
-    ]);
+});
