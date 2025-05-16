747edb17c155a51eaac11467cb909fe71801f291
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2025-05-16 19:44:49 +0000

    Add a test to cover educatable already invited
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- app-modules/campaign/tests/Tenant/Jobs/EventCampaignActionJobTest.php
+++ app-modules/campaign/tests/Tenant/Jobs/EventCampaignActionJobTest.php
@@ -98,3 +98,86 @@ it('will execute appropriately on each educatable in the segment', function (Edu
             fn () => Prospect::factory()->create(),
         ],
     ]);
+
+it('will not duplicate an invite if the segment educatable was already invited', function (Educatable $educatable) {
+    Bus::fake();
+    Notification::fake();
+
+    /** @var Segment $segment */
+    $segment = Segment::factory()->create([
+        'type' => SegmentType::Static,
+        'model' => match ($educatable::class) {
+            Student::class => SegmentModel::Student,
+            Prospect::class => SegmentModel::Prospect,
+            default => throw new Exception('Invalid model type'),
+        },
+    ]);
+
+    $campaign = Campaign::factory()
+        ->for($segment, 'segment')
+        ->for(User::factory()->licensed(LicenseType::cases()), 'createdBy')
+        ->create();
+
+    /** @var Event $event */
+    $event = Event::factory()->create();
+
+    $event->attendees()->truncate();
+
+    EventAttendee::factory()->create([
+        'event_id' => $event->getKey(),
+        'email' => $educatable->primaryEmailAddress->address,
+        'status' => EventAttendeeStatus::Invited,
+    ]);
+
+    /** @var CampaignAction $action */
+    $action = CampaignAction::factory()
+        ->for($campaign, 'campaign')
+        ->create([
+            'type' => CampaignActionType::Event,
+            'data' => [
+                'event' => $event->id,
+            ],
+        ]);
+
+    $campaignActionEducatable = CampaignActionEducatable::factory()
+        ->for($action, 'campaignAction')
+        // @phpstan-ignore argument.type
+        ->for($educatable, 'educatable')
+        ->create();
+
+    expect($campaignActionEducatable->succeeded_at)->toBeNull()
+        ->and($campaignActionEducatable->last_failed_at)->toBeNull();
+
+    [$job] = new EventCampaignActionJob($campaignActionEducatable)->withFakeBatch();
+
+    $job->handle();
+
+    assertDatabaseCount(
+        EventAttendee::class,
+        1,
+    );
+
+    $attendee = EventAttendee::query()
+        ->where('event_id', $event->getKey())
+        ->where('email', $educatable->primaryEmailAddress->address)
+        ->where('status', EventAttendeeStatus::Invited)
+        ->first();
+
+    expect($attendee)->not()->toBeNull();
+
+    Notification::assertNotSentTo($attendee, RegistrationLinkToEventAttendeeNotification::class);
+
+    $campaignActionEducatable->refresh();
+
+    expect($campaignActionEducatable->succeeded_at)->not()->toBeNull()
+        ->and($campaignActionEducatable->last_failed_at)->toBeNull()
+        ->and($campaignActionEducatable->related)->toBeNull();
+})
+    ->with([
+        'student' => [
+            fn () => Student::factory()->create(),
+        ],
+        'prospect' => [
+            fn () => Prospect::factory()->create(),
+        ],
+    ]);
