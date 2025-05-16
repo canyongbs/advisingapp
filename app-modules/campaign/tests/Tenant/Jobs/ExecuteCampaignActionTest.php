8d33a36d14ea048767af3dd9636e7f10265b383c
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2025-05-16 16:28:49 +0000

    Make the test ensure that the proper CampaignActionEducatable is created
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- app-modules/campaign/tests/Tenant/Jobs/ExecuteCampaignActionTest.php
+++ app-modules/campaign/tests/Tenant/Jobs/ExecuteCampaignActionTest.php
@@ -13,6 +13,7 @@ use AdvisingApp\Campaign\Jobs\TagsCampaignActionJob;
 use AdvisingApp\Campaign\Jobs\TaskCampaignActionJob;
 use AdvisingApp\Campaign\Models\Campaign;
 use AdvisingApp\Campaign\Models\CampaignAction;
+use AdvisingApp\Campaign\Models\CampaignActionEducatable;
 use AdvisingApp\Prospect\Models\Prospect;
 use AdvisingApp\Segment\Enums\SegmentModel;
 use AdvisingApp\Segment\Enums\SegmentType;
@@ -22,6 +23,9 @@ use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Bus;
+
+use function Pest\Laravel\assertDatabaseCount;
+use function Pest\Laravel\assertDatabaseHas;
 
 it('dispatches the correct job based on the CampaignAction type into the batch', function (SegmentModel $segmentModel, Collection $educatables, CampaignActionType $actionType, string $jobClass) {
     Bus::fake();
@@ -52,12 +56,22 @@ it('dispatches the correct job based on the CampaignAction type into the batch',
 
     [$job, $batch] = new ExecuteCampaignAction($action)->withFakeBatch();
 
+    assertDatabaseCount(CampaignActionEducatable::class, 0);
+
     $job->handle();
 
     expect($batch->added)
         ->toHaveCount($educatables->count())
         ->each
         ->toBeInstanceOf($jobClass);
+
+    $educatables->each(
+        fn (Model $educatable) => assertDatabaseHas(CampaignActionEducatable::class, [
+            'campaign_action_id' => $action->getKey(),
+            'educatable_id' => $educatable->getKey(),
+            'educatable_type' => $educatable->getMorphClass(),
+        ])
+    );
 })
     ->with([
         'prospects' => [
