cc46704321df16c45fa25f956d350c29280ca78a
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2025-05-29 14:54:13 +0000

    Remove DispatchTenantSetupCompleteEvent job and handle tenant setup completion directly in CreateTenant action
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- app/Multitenancy/Actions/CreateTenant.php
+++ app/Multitenancy/Actions/CreateTenant.php
@@ -40,18 +40,16 @@ use AdvisingApp\Theme\DataTransferObjects\ThemeConfig;
 use AdvisingApp\Theme\Jobs\UpdateTenantTheme;
 use App\DataTransferObjects\LicenseManagement\LicenseData;
 use App\Jobs\CreateTenantUser;
-use App\Jobs\DispatchTenantSetupCompleteEvent;
 use App\Jobs\MigrateTenantDatabase;
 use App\Jobs\SeedTenantDatabase;
 use App\Jobs\UpdateTenantLicenseData;
 use App\Models\Tenant;
 use App\Multitenancy\DataTransferObjects\TenantConfig;
 use App\Multitenancy\DataTransferObjects\TenantUser;
-use App\Multitenancy\Events\NewTenantSetupFailure;
+use App\Multitenancy\Events\NewTenantSetupComplete;
 use Illuminate\Encryption\Encrypter;
 use Illuminate\Support\Facades\Bus;
 use Illuminate\Support\Facades\Event;
-use Throwable;
 
 class CreateTenant
 {
@@ -78,13 +76,13 @@ class CreateTenant
                 ...($licenseData ? [new UpdateTenantLicenseData($tenant, $licenseData)] : []),
                 ...($themeConfig ? [new UpdateTenantTheme($tenant, $themeConfig)] : []),
                 ...($user ? [new CreateTenantUser($tenant, $user)] : []),
-                new DispatchTenantSetupCompleteEvent($tenant),
             ],
         ])
             ->name("deploy-tenant-{$tenant->getKey()}-{$domain}")
             ->onQueue(config('queue.landlord_queue'))
-            ->catch(function (Throwable $exception) use ($tenant) {
-                Event::dispatch(new NewTenantSetupFailure($tenant, $exception));
+            ->then(function () use ($tenant) {
+                $tenant->update(['setup_complete' => true]);
+                Event::dispatch(new NewTenantSetupComplete($tenant));
             })
             ->dispatch();
 
