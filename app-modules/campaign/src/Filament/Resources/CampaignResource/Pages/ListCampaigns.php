c4c7dc131a428a866877735b3ea815c38b8a8636
Karl Murray <karl.murray@canyongbs.com>
2025-06-02 13:49:40 +0000

    [ADVAPP-1552]: Update ListCompains for more details

--- app-modules/campaign/src/Filament/Resources/CampaignResource/Pages/ListCampaigns.php
+++ app-modules/campaign/src/Filament/Resources/CampaignResource/Pages/ListCampaigns.php
@@ -63,16 +63,26 @@ class ListCampaigns extends ListRecords
                 IdColumn::make(),
                 TextColumn::make('name'),
                 TextColumn::make('segment.name')
-                    ->label('Population Segment'),
-                IconColumn::make('enabled')
+                    ->label('Segment'),
+                TextColumn::make('enabled')
                     ->label('Enabled')
-                    ->boolean()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                IconColumn::make('execution_status')
+                    ->formatStateUsing(fn (bool $state): string => $state ? 'Yes' : 'No')
+                    ->badge()
+                    ->color(fn (bool $state): string => $state ? 'blue' : 'orange'),
+                TextColumn::make('execution_status')
                     ->label('Complete')
                     ->getStateUsing(fn (Campaign $record) => $record->hasBeenExecuted())
-                    ->boolean()
-                    ->toggleable(isToggledHiddenByDefault: true),
+                    ->formatStateUsing(fn (bool $state): string => $state ? 'Yes' : 'No')
+                    ->badge()
+                    ->color(fn (bool $state): string => $state ? 'blue' : 'orange'),
+                TextColumn::make('created_at')
+                    ->label('Created')
+                    ->dateTime()
+                    ->sortable(),
+                TextColumn::make('createdBy.name')
+                    ->label('Created By')
+                    ->searchable()
+                    ->sortable(),
             ])
             ->actions([
                 ViewAction::make(),
