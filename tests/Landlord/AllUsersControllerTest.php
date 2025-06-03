0c702c47984d01684b5085ded91f0756bf5de5fb
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2025-06-03 19:31:50 +0000

    Add todo tests
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

---
+++ tests/Landlord/AllUsersControllerTest.php
@@ -0,0 +1,7 @@
+<?php
+
+describe('AllUsersControllerTest', function () {
+    it('is protected by the landlord-api middleware', function () {})->todo('To be implemented at a later date');
+
+    it('returns a list of all users across all tenants', function () {})->todo('To be implemented at a later date');
+});
