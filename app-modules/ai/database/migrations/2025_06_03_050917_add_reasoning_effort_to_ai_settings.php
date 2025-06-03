87701da2571a95e4190b1cbfb6b1514788d94777
rebecca-canyon <rebecca-canyon@users.noreply.github.com>
2025-06-03 15:38:16 +0000

    chore: fix code style

--- app-modules/ai/database/migrations/2025_06_03_050917_add_reasoning_effort_to_ai_settings.php
+++ app-modules/ai/database/migrations/2025_06_03_050917_add_reasoning_effort_to_ai_settings.php
@@ -41,11 +41,11 @@ use Spatie\LaravelSettings\Migrations\SettingsMigration;
 return new class () extends SettingsMigration {
     public function up(): void
     {
-      try {
+        try {
             $this->migrator->add('ai.reasoning_effort', ReasoningEffort::Medium);
-      } catch (SettingAlreadyExists $exception) {
+        } catch (SettingAlreadyExists $exception) {
             // do nothing
-      }
+        }
     }
 
     public function down(): void
