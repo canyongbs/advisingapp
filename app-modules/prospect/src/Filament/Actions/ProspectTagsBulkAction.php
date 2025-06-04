3943816463266a38d072ce531a65be85e8e13a2f
Ankit Kabra <ankit.kabra@canyongbs.com>
2025-06-04 13:04:59 +0000

    refactor: improve modal description and streamline tag assignment logic for bulk actions

--- app-modules/prospect/src/Filament/Actions/ProspectTagsBulkAction.php
+++ app-modules/prospect/src/Filament/Actions/ProspectTagsBulkAction.php
@@ -2,17 +2,15 @@
 
 namespace AdvisingApp\Prospect\Filament\Actions;
 
-use AdvisingApp\Campaign\Settings\CampaignSettings;
 use AdvisingApp\Prospect\Models\Prospect;
-use AdvisingApp\StudentDataModel\Models\Student;
 use App\Models\Tag;
 use Exception;
-use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Toggle;
 use Filament\Notifications\Notification;
 use Filament\Tables\Actions\BulkAction;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 
 class ProspectTagsBulkAction
 {
@@ -22,36 +20,35 @@ class ProspectTagsBulkAction
             ->icon('heroicon-o-tag')
             ->modalHeading('Bulk assign prospect tags')
             ->label('Bulk Prospect Tags')
-            ->modalDescription(fn (Collection $records) => "You have selected {$records->count()} prospects to apply tags.")
+            ->modalDescription(
+                fn (Collection $records) => "You have selected {$records->count()} " . Str::plural('prospect', $records->count()) . ' to apply tags.'
+            )
             ->form([
-                    Select::make('tag_ids')
-                        ->label('Which tags should be applied?')
-                        ->options(function () {
-                            return Tag::where('type', app(Prospect::class)->getMorphClass())->pluck('name', 'id');
-                        })
-                        ->multiple()
-                        ->searchable()
-                        ->required()
-                        ->exists('tags', 'id'),
-                    Toggle::make('remove_prior')
-                        ->label('Remove all previously assigned tags?')
-                        ->default(false)
-                        ->hintIconTooltip('If checked, all prior tags assignments will be removed.'),
-                
+                Select::make('tag_ids')
+                    ->label('Which tags should be applied?')
+                    ->options(function () {
+                        return Tag::where('type', app(Prospect::class)->getMorphClass())->pluck('name', 'id');
+                    })
+                    ->multiple()
+                    ->searchable()
+                    ->required()
+                    ->exists('tags', 'id'),
+                Toggle::make('remove_prior')
+                    ->label('Remove all previously assigned tags?')
+                    ->default(false)
+                    ->hintIconTooltip('If checked, all prior tags assignments will be removed.'),
             ])
-            ->action(function (array $data, Collection $records){
-                $records->chunk(100)->each(function ($chunk) use ($data) {
-                    $chunk->each(function ($record) use ($data) {
-                        throw_unless($record instanceof Prospect, new Exception("Record must be of type prospect."));
+            ->action(function (array $data, Collection $records) {
+                $records->each(function ($record) use ($data) {
+                    throw_unless($record instanceof Prospect, new Exception('Record must be of type prospect.'));
 
-                        if (!empty($data['tag_ids'])) {
-                            $record->tags()
-                                    ->sync(
-                                        ids: $data['tag_ids'],
-                                        detaching: $data['remove_prior']
-                                    );
-                        }
-                    });
+                    if (! empty($data['tag_ids'])) {
+                        $record->tags()
+                            ->sync(
+                                ids: $data['tag_ids'],
+                                detaching: $data['remove_prior']
+                            );
+                    }
                 });
 
                 Notification::make()
--- app-modules/student-data-model/src/Filament/Actions/StudentTagsBulkAction.php
+++ app-modules/student-data-model/src/Filament/Actions/StudentTagsBulkAction.php
@@ -2,16 +2,15 @@
 
 namespace AdvisingApp\StudentDataModel\Filament\Actions;
 
-use AdvisingApp\Campaign\Settings\CampaignSettings;
 use AdvisingApp\StudentDataModel\Models\Student;
 use App\Models\Tag;
 use Exception;
-use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Toggle;
 use Filament\Notifications\Notification;
 use Filament\Tables\Actions\BulkAction;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 
 class StudentTagsBulkAction
 {
@@ -21,38 +20,37 @@ class StudentTagsBulkAction
             ->icon('heroicon-o-tag')
             ->modalHeading('Bulk assign student tags')
             ->label('Bulk Student Tags')
-            ->modalDescription(fn (Collection $records) => "You have selected {$records->count()} students to apply tags.")
+            ->modalDescription(
+                fn (Collection $records) => "You have selected {$records->count()} " . Str::plural('student', $records->count()) . ' to apply tags.'
+            )
             ->form([
-                    Select::make('tag_ids')
-                        ->label('Which tags should be applied?')
-                        ->options(function () {
-                            return Tag::where('type', app(Student::class)->getMorphClass())->pluck('name', 'id');
-                        })
-                        ->multiple()
-                        ->searchable()
-                        ->required()
-                        ->exists('tags', 'id'),
-                    Toggle::make('remove_prior')
-                        ->label('Remove all previously assigned tags?')
-                        ->default(false)
-                        ->hintIconTooltip('If checked, all prior tags assignments will be removed.'),
-                
+                Select::make('tag_ids')
+                    ->label('Which tags should be applied?')
+                    ->options(function () {
+                        return Tag::where('type', app(Student::class)->getMorphClass())->pluck('name', 'id');
+                    })
+                    ->multiple()
+                    ->searchable()
+                    ->required()
+                    ->exists('tags', 'id'),
+                Toggle::make('remove_prior')
+                    ->label('Remove all previously assigned tags?')
+                    ->default(false)
+                    ->hintIconTooltip('If checked, all prior tags assignments will be removed.'),
             ])
-            ->action(function (array $data, Collection $records){
-                $records->chunk(100)->each(function ($chunk) use ($data) {
-                    $chunk->each(function ($record) use ($data) {
-                        throw_unless($record instanceof Student, new Exception("Record must be of type student."));
+            ->action(function (array $data, Collection $records) {
+                $records->each(function ($record) use ($data) {
+                    throw_unless($record instanceof Student, new Exception('Record must be of type student.'));
 
-                        if (!empty($data['tag_ids'])) {
-                            $record->tags()
-                                    ->sync(
-                                        ids: $data['tag_ids'],
-                                        detaching: $data['remove_prior']
-                                    );
-                        }
-                    });
+                    if (! empty($data['tag_ids'])) {
+                        $record->tags()
+                            ->sync(
+                                ids: $data['tag_ids'],
+                                detaching: $data['remove_prior']
+                            );
+                    }
                 });
-
+                
                 Notification::make()
                     ->title('Tags assigned successfully.')
                     ->success()
