2611dc48aee8ef2146b6403168db2d9bd2818598
Orrison <Orrison@users.noreply.github.com>
2025-06-04 18:40:36 +0000

    chore: fix code style

--- app-modules/engagement/resources/views/components/engagement-timeline-item.blade.php
+++ app-modules/engagement/resources/views/components/engagement-timeline-item.blade.php
@@ -71,7 +71,7 @@
 
     <div
         class="my-4 rounded-lg border-2 border-gray-200 p-2 text-base font-normal text-gray-500 dark:border-gray-800 dark:text-gray-400">
-        @if ($record->channel === NotificationChannel::Email && ! blank($record->subject))
+        @if ($record->channel === NotificationChannel::Email && !blank($record->subject))
             <div class="mb-2 flex flex-col">
                 <p class="text-xs text-gray-400 dark:text-gray-500">Subject:</p>
 
