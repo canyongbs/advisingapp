323ce032956e694bdcbe4b6083cdce7cbb235d69
Ketan Barevadiya <ketan.barevadiya@canyongbs.com>
2025-06-06 10:53:00 +0000

    fix: improve searchable functionality for respondent SIS and Other IDs

--- app-modules/case-management/src/Filament/Resources/CaseResource/Pages/ListCases.php
+++ app-modules/case-management/src/Filament/Resources/CaseResource/Pages/ListCases.php
@@ -97,7 +97,17 @@ class ListCases extends ListRecords
                     ->sortable(query: fn (Builder $query, string $direction): Builder => $query->tap(new EducatableSort($direction))),
                 TextColumn::make('respondent.sisid')
                     ->label('SIS ID')
-                    ->searchable(query: fn (Builder $query, $search) => $query->tap(new EducatableSearch(relationship: 'respondent', search: $search)))
+                    ->searchable(query: function (Builder $query, string $search): Builder {
+                        return $query->whereHasMorph(
+                            'respondent',
+                            Student::class,
+                            function (Builder $query, string $type) use ($search) {
+                                if ($type === Student::class) {
+                                    $query->where('sisid', 'like', "%{$search}%");
+                                }
+                            }
+                        );
+                    })
                     ->sortable(query: function (Builder $query, string $direction): Builder {
                         // Update this if any other relations are added to the Case model respondent relationship
                         return $query->join('students', function (JoinClause $join) {
@@ -107,7 +117,17 @@ class ListCases extends ListRecords
                     }),
                 TextColumn::make('respondent.otherid')
                     ->label('Other ID')
-                    ->searchable(query: fn (Builder $query, $search) => $query->tap(new EducatableSearch(relationship: 'respondent', search: $search)))
+                    ->searchable(query: function (Builder $query, string $search): Builder {
+                        return $query->whereHasMorph(
+                            'respondent',
+                            Student::class,
+                            function (Builder $query, string $type) use ($search) {
+                                if ($type === Student::class) {
+                                    $query->where('otherid', 'like', "%{$search}%");
+                                }
+                            }
+                        );
+                    })
                     ->sortable(query: function (Builder $query, string $direction): Builder {
                         // Update this if any other relations are added to the Case model respondent relationship
                         return $query->join('students', function (JoinClause $join) {
