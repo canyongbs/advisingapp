336c7208629f1ba9ff9514a2101663f6f5a62fb8
Payal Baldaniya <payal.baldaniya@canyongbs.com>
2025-06-13 18:00:16 +0000

    Implement filters and refresh functionality in student reports and widgets

---
+++ app-modules/report/resources/views/filament/pages/students.blade.php
@@ -0,0 +1,26 @@
+@php
+    use AdvisingApp\Report\Filament\Widgets\RefreshWidget;
+
+    $visibleWidgets = collect($this->getVisibleWidgets())
+        ->reject(fn($widget) => $widget->widget === RefreshWidget::class)
+        ->all();
+@endphp
+
+<x-filament-panels::page class="fi-dashboard-page">
+    <div>
+        @livewire(RefreshWidget::class, ['cacheTag' => $this->cacheTag])
+    </div>
+
+    @if (method_exists($this, 'filtersForm'))
+        {{ $this->filtersForm }}
+    @endif
+
+    <x-filament-widgets::widgets
+        :columns="$this->getColumns()"
+        :data="[
+            ...property_exists($this, 'filters') ? ['filters' => $this->filters] : [],
+            ...$this->getWidgetData(),
+        ]"
+        :widgets="$visibleWidgets"
+    />
+</x-filament-panels::page>
