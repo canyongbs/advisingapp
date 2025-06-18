3d38896b380b81229c36ac920bacd61d3d33c35c
jayushi-canyon <jayushi.jain@canyongbs.com>
2025-06-18 20:50:57 +0530

    [ADVAPP-1589]: Clear Spatie permissions cache at the end of an Octane request (#1628)
    Add configuration option to register Octane reset listener for permission refresh

--- app-modules/authorization/config/permission.php
+++ app-modules/authorization/config/permission.php
@@ -143,6 +143,13 @@ return [
     'register_permission_check_method' => true,
 
     /*
+     * When set to true, Laravel\Octane\Events\OperationTerminated event listener will be registered
+     * this will refresh permissions on every TickTerminated, TaskTerminated and RequestTerminated
+     * NOTE: This should not be needed in most cases, but an Octane/Vapor combination benefited from it.
+     */
+    'register_octane_reset_listener' => true,
+
+    /*
      * When set to true the package implements teams using the 'team_foreign_key'. If you want
      * the migrations to register the 'team_foreign_key', you must set this to true
      * before doing the migration. If you already did the migration then you must make a new
