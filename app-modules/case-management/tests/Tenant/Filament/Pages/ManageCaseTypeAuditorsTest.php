242f665341965ae841fe8d6a55b4c6921570abc4
Payal Baldaniya <payal.baldaniya@canyongbs.com>
2025-06-24 06:43:50 +0000

    Refactor test cases to use LicenseType enum for user licensing

--- app-modules/case-management/tests/Tenant/Filament/Pages/ManageCaseTypeAuditorsTest.php
+++ app-modules/case-management/tests/Tenant/Filament/Pages/ManageCaseTypeAuditorsTest.php
@@ -1,10 +1,9 @@
 <?php
 
+use AdvisingApp\Authorization\Enums\LicenseType;
 use AdvisingApp\CaseManagement\Filament\Resources\CaseTypeResource;
 use AdvisingApp\CaseManagement\Filament\Resources\CaseTypeResource\Pages\ManageCaseTypeAuditors;
 use AdvisingApp\CaseManagement\Models\CaseType;
-use AdvisingApp\Prospect\Models\Prospect;
-use AdvisingApp\StudentDataModel\Models\Student;
 use AdvisingApp\Team\Models\Team;
 use App\Models\User;
 use Filament\Tables\Actions\AttachAction;
@@ -13,7 +12,7 @@ use function Pest\Laravel\actingAs;
 use function Pest\Livewire\livewire;
 
 it('can attach audit member to case type', function () {
-    $user = User::factory()->licensed([Student::getLicenseType(), Prospect::getLicenseType()])->create();
+    $user = User::factory()->licensed(LicenseType::cases())->create();
 
     $caseType = CaseType::factory()->create();
 
