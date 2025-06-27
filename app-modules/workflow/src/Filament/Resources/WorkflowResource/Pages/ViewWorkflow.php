a054852d336ff8f03179e9f6b7ce19a457b591bc
Rebecca Sheridan <rebecca.sheridan@canyongbs.com>
2025-06-27 00:49:43 +0000

    Create pages, module (WIP)

---
+++ app-modules/workflow/src/Filament/Resources/WorkflowResource/Pages/ViewWorkflow.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AdvisingApp\Workflow\Filament\Resources\WorkflowResource\Pages;
+
+use AdvisingApp\Workflow\Filament\Resources\WorkflowResource;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewWorkflow extends ViewRecord
+{
+    protected static string $resource = WorkflowResource::class;
+}
