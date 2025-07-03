f821dff49e52956987139a9c0f066ef76a6a8ab4
Rebecca Sheridan <rebecca.sheridan@canyongbs.com>
2025-07-03 16:31:04 +0000

    Create workflow task action

---
+++ app-modules/workflow/src/Models/WorkflowTaskDetails.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AdvisingApp\Workflow\Models;
+
+use AdvisingApp\Audit\Models\Concerns\Auditable as AuditableTrait;
+use AdvisingApp\Workflow\Models\Contracts\WorkflowAction;
+use App\Models\BaseModel;
+use Illuminate\Database\Eloquent\Concerns\HasVersion4Uuids as HasUuids;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use OwenIt\Auditing\Contracts\Auditable;
+
+class WorkflowTaskDetails extends BaseModel implements Auditable, WorkflowAction
+{
+    use SoftDeletes;
+    use AuditableTrait;
+    use HasUuids;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'due',
+    ];
+
+    protected $casts = [
+        'due' => 'datetime',
+    ];
+
+    /**
+     * @return BelongsTo<WorkflowStep, $this>
+     */
+    public function workflowStep(): BelongsTo
+    {
+        return $this->belongsTo(WorkflowStep::class);
+    }
+}
