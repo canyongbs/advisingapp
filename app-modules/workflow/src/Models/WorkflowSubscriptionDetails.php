042439f91ccb2dbfefd64209103e81fe192f890b
Rebecca Sheridan <rebecca.sheridan@canyongbs.com>
2025-07-03 16:42:13 +0000

    Create workflow subscription action

---
+++ app-modules/workflow/src/Models/WorkflowSubscriptionDetails.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AdvisingApp\Workflow\Models;
+
+use AdvisingApp\Audit\Models\Concerns\Auditable as AuditableTrait;
+use AdvisingApp\Workflow\Models\Contracts\WorkflowAction;
+use App\Models\BaseModel;
+use Illuminate\Database\Eloquent\Concerns\HasVersion4Uuids as HasUuids;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use OwenIt\Auditing\Contracts\Auditable;
+
+class WorkflowSubscriptionDetails extends BaseModel implements Auditable, WorkflowAction
+{
+    use SoftDeletes;
+    use AuditableTrait;
+    use HasUuids;
+
+    protected $fillable = [
+        'user_ids',
+        'remove_prior',
+    ];
+
+    protected $casts = [
+        'user_ids' => 'array',
+        'remove_prior' => 'boolean',
+    ];
+
+    /**
+     * @return BelongsTo<WorkflowStep, $this>
+     */
+    public function workflowStep(): BelongsTo
+    {
+        return $this->belongsTo(WorkflowStep::class);
+    }
+}
