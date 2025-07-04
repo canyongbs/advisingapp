df1008424252590b1606ae07e0aafe0477385965
Payal Baldaniya <payal.baldaniya@canyongbs.com>
2025-07-04 08:13:58 +0000

    Refactor CaseDataObject properties to use camelCase and update CreateCaseTest with new assignment type tests

--- app-modules/case-management/src/DataTransferObjects/CaseDataObject.php
+++ app-modules/case-management/src/DataTransferObjects/CaseDataObject.php
@@ -2,20 +2,23 @@
 
 namespace AdvisingApp\CaseManagement\DataTransferObjects;
 
+use Spatie\LaravelData\Attributes\MapName;
 use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Mappers\SnakeCaseMapper;
 use Spatie\LaravelData\Optional;
 
+#[MapName(SnakeCaseMapper::class)]
 class CaseDataObject extends Data
 {
     public function __construct(
-        public string|Optional $division_id,
-        public string|Optional $status_id,
-        public string $type_id,
-        public string|Optional $priority_id,
-        public string|Optional $close_details,
-        public string|Optional $res_details,
-        public string $respondent_type,
-        public string $respondent_id,
+        public string|Optional $divisionId,
+        public string|Optional $statusId,
+        public string $typeId,
+        public string|Optional $priorityId,
+        public string|Optional $closeDetails,
+        public string|Optional $resDetails,
+        public string $respondentType,
+        public string $respondentId,
     ) {}
 
     /**
@@ -26,14 +29,14 @@ class CaseDataObject extends Data
     public static function fromData(array $data): self
     {
         return new self(
-            division_id: $data['division_id'] ?? Optional::create(),
-            status_id: $data['status_id'] ?? Optional::create(),
-            type_id: $data['type_id'],
-            priority_id: $data['priority_id'] ?? Optional::create(),
-            close_details: $data['close_details'] ?? Optional::create(),
-            res_details: $data['res_details'] ?? Optional::create(),
-            respondent_type: $data['respondent_type'],
-            respondent_id: $data['respondent_id'],
+            divisionId: $data['division_id'] ?? Optional::create(),
+            statusId: $data['status_id'] ?? Optional::create(),
+            typeId: $data['type_id'],
+            priorityId: $data['priority_id'] ?? Optional::create(),
+            closeDetails: $data['close_details'] ?? Optional::create(),
+            resDetails: $data['res_details'] ?? Optional::create(),
+            respondentType: $data['respondent_type'],
+            respondentId: $data['respondent_id'],
         );
     }
 }
