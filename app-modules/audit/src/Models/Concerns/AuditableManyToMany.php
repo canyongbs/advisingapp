d4d6ea9afdee80f2b2454d6231c3b38b0de45feb
Rebecca Sheridan <rebecca.sheridan@canyongbs.com>
2025-07-07 20:32:01 +0000

    Correct doc block for newMorphToMany

--- app-modules/audit/src/Models/Concerns/AuditableManyToMany.php
+++ app-modules/audit/src/Models/Concerns/AuditableManyToMany.php
@@ -66,15 +66,6 @@ trait AuditableManyToMany
     }
 
     /**
-     * @param mixed $name
-     * @param mixed $table
-     * @param mixed $foreignPivotKey
-     * @param mixed $relatedPivotKey
-     * @param mixed $parentKey
-     * @param mixed $relatedKey
-     * @param null|mixed $relationName
-     * @param mixed $inverse
-     *
      * @return MorphToMany<covariant Model, covariant Model>
      */
     protected function newMorphToMany(
