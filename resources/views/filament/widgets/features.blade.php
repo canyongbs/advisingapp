be6902a9fc5711614b305a11619f7ba018368923
Karl Murray <karl.murray@canyongbs.com>
2025-07-08 17:02:32 +0000

    [ADVAPP-1652]: Update feature widget

--- resources/views/filament/widgets/features.blade.php
+++ resources/views/filament/widgets/features.blade.php
@@ -9,22 +9,22 @@
     Notice:
 
     - You may not provide the software to third parties as a hosted or managed
-      service, where the service provides users with access to any substantial set of
-      the features or functionality of the software.
+    service, where the service provides users with access to any substantial set of
+    the features or functionality of the software.
     - You may not move, change, disable, or circumvent the license key functionality
-      in the software, and you may not remove or obscure any functionality in the
-      software that is protected by the license key.
+    in the software, and you may not remove or obscure any functionality in the
+    software that is protected by the license key.
     - You may not alter, remove, or obscure any licensing, copyright, or other notices
-      of the licensor in the software. Any use of the licensor’s trademarks is subject
-      to applicable law.
+    of the licensor in the software. Any use of the licensor’s trademarks is subject
+    to applicable law.
     - Canyon GBS LLC respects the intellectual property rights of others and expects the
-      same in return. Canyon GBS™ and Advising App™ are registered trademarks of
-      Canyon GBS LLC, and we are committed to enforcing and protecting our trademarks
-      vigorously.
+    same in return. Canyon GBS™ and Advising App™ are registered trademarks of
+    Canyon GBS LLC, and we are committed to enforcing and protecting our trademarks
+    vigorously.
     - The software solution, including services, infrastructure, and code, is offered as a
-      Software as a Service (SaaS) by Canyon GBS LLC.
+    Software as a Service (SaaS) by Canyon GBS LLC.
     - Use of this software implies agreement to the license terms and conditions as stated
-      in the Elastic License 2.0.
+    in the Elastic License 2.0.
 
     For more information or inquiries please visit our website at
     https://www.canyongbs.com or contact us via email at legal@canyongbs.com.
@@ -32,7 +32,7 @@
 </COPYRIGHT>
 --}}
 @php
-    use AdvisingApp\Assistant\Filament\Pages\PersonalAssistant;
+    use AdvisingApp\Assistant\Filament\Pages\InstitutionalAdvisor;
     use AdvisingApp\Authorization\Enums\LicenseType;
     use AdvisingApp\Prospect\Filament\Pages\RecruitmentCrmDashboard;
     use AdvisingApp\StudentDataModel\Filament\Pages\RetentionCrmDashboard;
@@ -54,12 +54,8 @@
                 </div>
 
                 <div class="flex items-center justify-center">
-                    <x-filament::button
-                        :href="$hasFeature ? RecruitmentCrmDashboard::getUrl() : null"
-                        size="xl"
-                        tag="a"
-                        color="gray"
-                    >
+                    <x-filament::button :href="$hasFeature ? RecruitmentCrmDashboard::getUrl() : null" size="xl" tag="a"
+                        color="gray">
                         Start now
                     </x-filament::button>
                 </div>
@@ -84,12 +80,8 @@
                 </div>
 
                 <div class="flex items-center justify-center">
-                    <x-filament::button
-                        :href="$hasFeature ? RetentionCrmDashboard::getUrl() : null"
-                        size="xl"
-                        tag="a"
-                        color="gray"
-                    >
+                    <x-filament::button :href="$hasFeature ? RetentionCrmDashboard::getUrl() : null" size="xl" tag="a"
+                        color="gray">
                         Start now
                     </x-filament::button>
                 </div>
@@ -114,12 +106,8 @@
                 </div>
 
                 <div class="flex items-center justify-center">
-                    <x-filament::button
-                        :href="$hasFeature ? PersonalAssistant::getUrl() : null"
-                        size="xl"
-                        tag="a"
-                        color="gray"
-                    >
+                    <x-filament::button :href="$hasFeature ? InstitutionalAdvisor::getUrl() : null" size="xl" tag="a"
+                        color="gray">
                         Start now
                     </x-filament::button>
                 </div>
