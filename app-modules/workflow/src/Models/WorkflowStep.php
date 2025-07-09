70d42048e254fff6c97ff83960342267c060e9d0
Rebecca Sheridan <rebecca.sheridan@canyongbs.com>
2025-07-09 16:09:47 +0000

    Change workflow step relationship to workflow back to belongsTo

--- app-modules/workflow/src/Models/WorkflowStep.php
+++ app-modules/workflow/src/Models/WorkflowStep.php
@@ -39,8 +39,7 @@ namespace AdvisingApp\Workflow\Models;
 use AdvisingApp\Audit\Models\Concerns\Auditable as AuditableTrait;
 use App\Models\BaseModel;
 use Illuminate\Database\Eloquent\Concerns\HasVersion4Uuids as HasUuids;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use OwenIt\Auditing\Contracts\Auditable;
 
@@ -54,10 +53,10 @@ class WorkflowStep extends BaseModel implements Auditable
     use HasUuids;
 
     /**
-     * @return MorphTo<Model, $this>
+     * @return BelongsTo<Workflow, $this>
      */
-    public function workflow(): MorphTo
+    public function workflow(): BelongsTo
     {
-        return $this->morphTo();
+        return $this->belongsTo(Workflow::class);
     }
 }
