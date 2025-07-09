7edae36029e03c05173df7a2882bbee192366380
Orrison <6799341+Orrison@users.noreply.github.com>
2025-07-09 23:14:30 +0000

    chore: fix code style

--- app-modules/workflow/src/Models/WorkflowRun.php
+++ app-modules/workflow/src/Models/WorkflowRun.php
@@ -40,7 +40,6 @@ use AdvisingApp\Audit\Models\Concerns\Auditable as AuditableTrait;
 use App\Models\BaseModel;
 use Illuminate\Database\Eloquent\Concerns\HasVersion4Uuids as HasUuids;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use OwenIt\Auditing\Contracts\Auditable;
 
