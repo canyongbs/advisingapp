c9c01f24fd8ada75dd7813570cd65371a9f9797d
Rebecca Sheridan <rebecca.sheridan@canyongbs.com>
2025-07-14 05:10:05 +0000

    Update WorkflowStep details column to current_details. Turn WorkflowAction into WorkflowDetails, an abstract class

--- app-modules/workflow/src/Models/WorkflowStep.php
+++ app-modules/workflow/src/Models/WorkflowStep.php
@@ -37,7 +37,6 @@
 namespace AdvisingApp\Workflow\Models;
 
 use AdvisingApp\Audit\Models\Concerns\Auditable as AuditableTrait;
-use AdvisingApp\Workflow\Models\Contracts\WorkflowAction;
 use App\Models\BaseModel;
 use Illuminate\Database\Eloquent\Concerns\HasVersion4Uuids as HasUuids;
 use Illuminate\Database\Eloquent\Model;
@@ -87,11 +86,4 @@ class WorkflowStep extends BaseModel implements Auditable
         return $this->morphTo();
     }
 
-    // /**
-    //  * @return HasOne<covariant WorkflowAction, $this>
-    //  */
-    // public function workflowAction(): HasOne
-    // {
-    //   return $this->hasOne(WorkflowAction::class);
-    // }
 }
