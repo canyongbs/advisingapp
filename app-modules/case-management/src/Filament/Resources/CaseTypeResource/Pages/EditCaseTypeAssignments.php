c4cfcdcd0ee43f68518605100262c2bcc605640d
Karl Murray <karl.murray@canyongbs.com>
2025-07-16 16:46:40 +0000

    [ADVAPP-1663]: Cleanup AssignementsFeature

--- app-modules/case-management/src/Filament/Resources/CaseTypeResource/Pages/EditCaseTypeAssignments.php
+++ app-modules/case-management/src/Filament/Resources/CaseTypeResource/Pages/EditCaseTypeAssignments.php
@@ -40,7 +40,6 @@ use AdvisingApp\CaseManagement\Enums\CaseTypeAssignmentTypes;
 use AdvisingApp\CaseManagement\Filament\Resources\CaseTypeResource;
 use AdvisingApp\CaseManagement\Models\CaseType;
 use AdvisingApp\CaseManagement\Rules\CaseTypeAssignmentsIndividualUserMustBeAManager;
-use App\Features\AssignmentsFeature;
 use App\Filament\Forms\Components\Heading;
 use App\Filament\Forms\Components\Paragraph;
 use App\Filament\Resources\Pages\EditRecord\Concerns\EditPageRedirection;
@@ -69,7 +68,7 @@ class EditCaseTypeAssignments extends EditRecord
 
     public static function canAccess(array $parameters = []): bool
     {
-        return AssignmentsFeature::active() && parent::canAccess($parameters);
+        return parent::canAccess($parameters);
     }
 
     public function form(Form $form): Form
