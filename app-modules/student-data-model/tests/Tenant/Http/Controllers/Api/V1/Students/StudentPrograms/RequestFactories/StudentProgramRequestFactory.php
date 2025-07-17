525c4c3cf1243c536566cd1c18c29901db1356ce
Ankit Kabra <ankit.kabra@canyongbs.com>
2025-07-17 17:16:43 +0000

    Refactor acad_plan handling in StudentProgramRequestFactory and update test assertion in StudentProgramControllerTest for consistency

--- app-modules/student-data-model/tests/Tenant/Http/Controllers/Api/V1/Students/StudentPrograms/RequestFactories/StudentProgramRequestFactory.php
+++ app-modules/student-data-model/tests/Tenant/Http/Controllers/Api/V1/Students/StudentPrograms/RequestFactories/StudentProgramRequestFactory.php
@@ -12,10 +12,10 @@ class StudentProgramRequestFactory extends RequestFactory
         return array_filter([
             'acad_career' => $this->faker->randomElement(['NC', 'CRED']),
             'division' => $this->faker->randomElement(['ABC01', 'ABD02', 'ABE03']),
-            'acad_plan' => json_encode([
+            'acad_plan' => [
                 'major' => $this->faker->words(3),
                 'minor' => $this->faker->words(3),
-            ]),
+            ],
             'prog_status' => 'AC',
             'cum_gpa' => $this->faker->randomFloat(2, 0, 4),
             'semester' => $this->faker->numerify('####'),
