894dfc8ad95e77f6119e91078e32a58b82bd5587
Rebecca Sheridan <rebecca.sheridan@canyongbs.com>
2025-07-18 04:20:11 +0000

    Workflow UI

---
+++ app-modules/workflow/src/Filament/Resources/WorkflowResource.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace AdvisingApp\Workflow\Filament\Resources;
+
+use AdvisingApp\Workflow\Filament\Resources\WorkflowResource\Pages;
+use AdvisingApp\Workflow\Filament\Resources\WorkflowResource\RelationManagers;
+use AdvisingApp\Workflow\Models\Workflow;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class WorkflowResource extends Resource
+{
+    protected static ?string $model = Workflow::class;
+
+    protected static bool $shouldRegisterNavigation = false;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListWorkflows::route('/'),
+            'edit' => Pages\EditWorkflow::route('/{record}/edit'),
+        ];
+    }
+}
