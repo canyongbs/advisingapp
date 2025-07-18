7dcde10fe76263bbdef9111f73b35292ebb66226
Ankit Kabra <ankit.kabra@canyongbs.com>
2025-07-18 09:39:29 +0000

    feat(enrollments): add ListStudentEnrollmentsController and StudentEnrollmentResource for student enrollment management

--- app-modules/student-data-model/routes/api.php
+++ app-modules/student-data-model/routes/api.php
@@ -40,6 +40,7 @@ use AdvisingApp\StudentDataModel\Http\Controllers\Api\V1\Students\ListStudentsCo
 use AdvisingApp\StudentDataModel\Http\Controllers\Api\V1\Students\StudentEmailAddresses\CreateStudentEmailAddressController;
 use AdvisingApp\StudentDataModel\Http\Controllers\Api\V1\Students\StudentEmailAddresses\DeleteStudentEmailAddressController;
 use AdvisingApp\StudentDataModel\Http\Controllers\Api\V1\Students\StudentEmailAddresses\UpdateStudentEmailAddressController;
+use AdvisingApp\StudentDataModel\Http\Controllers\Api\V1\Students\StudentEnrollments\ListStudentEnrollmentsController;
 use AdvisingApp\StudentDataModel\Http\Controllers\Api\V1\Students\StudentPhoneNumbers\CreateStudentPhoneNumberController;
 use AdvisingApp\StudentDataModel\Http\Controllers\Api\V1\Students\StudentPhoneNumbers\DeleteStudentPhoneNumberController as StudentPhoneNumbersDeleteStudentPhoneNumberController;
 use AdvisingApp\StudentDataModel\Http\Controllers\Api\V1\Students\StudentPhoneNumbers\UpdateStudentPhoneNumberController;
@@ -84,5 +85,8 @@ Route::api(majorVersion: 1, routes: function () {
                     Route::patch('/{studentPhoneNumber}', UpdateStudentPhoneNumberController::class)->name('update');
                     Route::delete('/{studentPhoneNumber}', StudentPhoneNumbersDeleteStudentPhoneNumberController::class)->name('delete');
                 });
+
+            Route::get('{student}/enrollments', ListStudentEnrollmentsController::class)->name('enrollments.index');
+            // Route::put('{student}/enrollments', StudentProgramsController::class)->name('enrollments.put');
         });
 });
