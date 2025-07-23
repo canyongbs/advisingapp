1a206d7982278c08a1d14a407c6d7a0b9eb0eaf6
Kevin Ullyott <Orrison@users.noreply.github.com>
2025-07-23 16:51:44 -0400

    [ADVAPP-1675]: Hide cases resource from the main navigation (#1736)
    Hide cases resource from the main navigation
    
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- app-modules/case-management/src/Filament/Resources/CaseResource.php
+++ app-modules/case-management/src/Filament/Resources/CaseResource.php
@@ -57,6 +57,9 @@ class CaseResource extends Resource
 
     protected static ?string $modelLabel = 'Case';
 
+    // TODO: Look into whether or not we should just delete this resource
+    protected static bool $shouldRegisterNavigation = false;
+
     public static function shouldShowFormSubmission(Page $page): bool
     {
         if (! is_null($page->record) && ! is_null($page->record->caseFormSubmission)) {
