d14d8b3b297de6a5e1c0e9b0465e0a8f3ed83a01
Orrison <6799341+Orrison@users.noreply.github.com>
2025-07-24 04:45:33 +0000

    chore: fix code style

--- app-modules/workflow/database/migrations/2025_07_23_224210_drop_workflow_step_id__and_make_appropriate_columns_nullable_from_workflow_case_details_table.php
+++ app-modules/workflow/database/migrations/2025_07_23_224210_drop_workflow_step_id__and_make_appropriate_columns_nullable_from_workflow_case_details_table.php
@@ -38,8 +38,7 @@ use Illuminate\Database\Migrations\Migration;
 use Tpetry\PostgresqlEnhanced\Schema\Blueprint;
 use Tpetry\PostgresqlEnhanced\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     public function up(): void
     {
         Schema::table('workflow_case_details', function (Blueprint $table) {
