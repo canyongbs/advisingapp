8f6df267a0c61fd3e82fea36e812d2ab070a2650
Orrison <6799341+Orrison@users.noreply.github.com>
2025-07-24 18:18:02 +0000

    chore: fix code style

--- app-modules/workflow/database/migrations/2025_07_24_181533_data_activate_workflow_feature_flag.php
+++ app-modules/workflow/database/migrations/2025_07_24_181533_data_activate_workflow_feature_flag.php
@@ -36,11 +36,8 @@
 
 use App\Features\WorkflowFeature;
 use Illuminate\Database\Migrations\Migration;
-use Tpetry\PostgresqlEnhanced\Schema\Blueprint;
-use Tpetry\PostgresqlEnhanced\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     public function up(): void
     {
         WorkflowFeature::activate();
