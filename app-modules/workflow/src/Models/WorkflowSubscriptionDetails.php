f392db314ea00270404bdccab10ed186bd09c839
Rebecca Sheridan <rebecca.sheridan@canyongbs.com>
2025-07-25 12:27:44 +0000

    Remove magic methods

--- app-modules/workflow/src/Models/WorkflowSubscriptionDetails.php
+++ app-modules/workflow/src/Models/WorkflowSubscriptionDetails.php
@@ -70,7 +70,7 @@ class WorkflowSubscriptionDetails extends WorkflowDetails implements Auditable
     public function hasBeenExecuted(): bool
     {
         $workflowStep = WorkflowStep::whereCurrentDetailsType(WorkflowActionType::Subscription)
-            ->whereCurrentDetailsId($this->id)
+            ->where('current_details_id', $this->id)
             ->first();
 
         $workflowRun = WorkflowRun::whereWorkflowTriggerId($workflowStep->workflow->workflowTrigger->getKey())->first();
