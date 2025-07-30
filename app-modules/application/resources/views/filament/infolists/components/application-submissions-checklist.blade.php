6db44838243353f917a1b7500356a02dbf6f32b3
Amit Solanki <amit.solanki@canyongbs.com>
2025-07-30 07:47:58 +0000

    Fix wire:key usage in application submissions checklist component

--- app-modules/application/resources/views/filament/infolists/components/application-submissions-checklist.blade.php
+++ app-modules/application/resources/views/filament/infolists/components/application-submissions-checklist.blade.php
@@ -2,6 +2,6 @@
     $applicationSubmission = $this->submission ?? $this->cachedMountedTableActionRecord;
 @endphp
 
-<div wire:key="checklist-{{ $applicationSubmission }}">
+<div wire:key="checklist-{{ $applicationSubmission->getKey() }}">
     @livewire('application::application-submissions-checklist', ['applicationSubmission' => $applicationSubmission])
 </div>
