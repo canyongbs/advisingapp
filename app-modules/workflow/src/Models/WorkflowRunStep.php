70ee939eb5d33acffbb22a11f5bf7c0ad9ee35b5
Orrison <6799341+Orrison@users.noreply.github.com>
2025-08-06 20:18:58 +0000

    chore: fix code style

--- app-modules/workflow/src/Models/WorkflowRunStep.php
+++ app-modules/workflow/src/Models/WorkflowRunStep.php
@@ -38,7 +38,6 @@ namespace AdvisingApp\Workflow\Models;
 
 use App\Models\BaseModel;
 use Illuminate\Database\Eloquent\Concerns\HasVersion4Uuids as HasUuids;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
