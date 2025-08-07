0087df557620979169e21c68297a902196aea99b
rebecca-canyon <198644076+rebecca-canyon@users.noreply.github.com>
2025-08-07 02:28:07 +0000

    chore: fix code style

--- app-modules/workflow/src/Models/WorkflowStep.php
+++ app-modules/workflow/src/Models/WorkflowStep.php
@@ -37,7 +37,6 @@
 namespace AdvisingApp\Workflow\Models;
 
 use AdvisingApp\Audit\Models\Concerns\Auditable as AuditableTrait;
-use AdvisingApp\Workflow\Enums\WorkflowActionType;
 use App\Models\BaseModel;
 use Illuminate\Database\Eloquent\Concerns\HasVersion4Uuids as HasUuids;
 use Illuminate\Database\Eloquent\Model;
