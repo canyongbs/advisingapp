8d043561899aa9156c7421b7e12f3e27d611b749
Payal Baldaniya <payal.baldaniya@canyongbs.com>
2025-08-08 09:50:21 +0000

    Update copyright notice and remove unused docblock in Project model

--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -1,5 +1,39 @@
 <?php
 
+/*
+<COPYRIGHT>
+
+    Copyright © 2016-2025, Canyon GBS LLC. All rights reserved.
+
+    Advising App™ is licensed under the Elastic License 2.0. For more details,
+    see https://github.com/canyongbs/advisingapp/blob/main/LICENSE.
+
+    Notice:
+
+    - You may not provide the software to third parties as a hosted or managed
+      service, where the service provides users with access to any substantial set of
+      the features or functionality of the software.
+    - You may not move, change, disable, or circumvent the license key functionality
+      in the software, and you may not remove or obscure any functionality in the
+      software that is protected by the license key.
+    - You may not alter, remove, or obscure any licensing, copyright, or other notices
+      of the licensor in the software. Any use of the licensor’s trademarks is subject
+      to applicable law.
+    - Canyon GBS LLC respects the intellectual property rights of others and expects the
+      same in return. Canyon GBS™ and Advising App™ are registered trademarks of
+      Canyon GBS LLC, and we are committed to enforcing and protecting our trademarks
+      vigorously.
+    - The software solution, including services, infrastructure, and code, is offered as a
+      Software as a Service (SaaS) by Canyon GBS LLC.
+    - Use of this software implies agreement to the license terms and conditions as stated
+      in the Elastic License 2.0.
+
+    For more information or inquiries please visit our website at
+    https://www.canyongbs.com or contact us via email at legal@canyongbs.com.
+
+</COPYRIGHT>
+*/
+
 // @formatter:off
 // phpcs:ignoreFile
 /**
@@ -4441,16 +4475,12 @@ namespace AdvisingApp\Pipeline\Models{
  * @property-read \Illuminate\Database\Eloquent\Collection<int, \AdvisingApp\Audit\Models\Audit> $audits
  * @property-read int|null $audits_count
  * @property-read \App\Models\User $createdBy
- * @property-read \Illuminate\Database\Eloquent\Collection<int, \AdvisingApp\Pipeline\Models\EducatablePipelineStage> $educatablePipelineStages
- * @property-read int|null $educatable_pipeline_stages_count
  * @property-read \AdvisingApp\Pipeline\Models\EducatablePipelineStage|null $pivot
- * @property-read \Illuminate\Database\Eloquent\Collection<int, \AdvisingApp\Prospect\Models\Prospect> $prospectPipelineStages
- * @property-read int|null $prospect_pipeline_stages_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \AdvisingApp\Prospect\Models\Prospect> $educatablePipelineStages
+ * @property-read int|null $educatable_pipeline_stages_count
  * @property-read \AdvisingApp\Segment\Models\Segment $segment
  * @property-read \Illuminate\Database\Eloquent\Collection<int, \AdvisingApp\Pipeline\Models\PipelineStage> $stages
  * @property-read int|null $stages_count
- * @property-read \Illuminate\Database\Eloquent\Collection<int, \AdvisingApp\StudentDataModel\Models\Student> $studentPipelineStages
- * @property-read int|null $student_pipeline_stages_count
  * @method static \AdvisingApp\Pipeline\Database\Factories\PipelineFactory factory($count = null, $state = [])
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Pipeline newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Pipeline newQuery()
@@ -4521,40 +4551,6 @@ namespace AdvisingApp\Portal\Models{
  */
 	#[\AllowDynamicProperties]
 	class IdeHelperPortalAuthentication {}
-}
-
-namespace AdvisingApp\Project\Models{
-/**
- * @property string $id
- * @property string $name
- * @property string|null $description
- * @property string $created_by_type
- * @property string $created_by_id
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property \Illuminate\Support\Carbon|null $deleted_at
- * @property-read \Illuminate\Database\Eloquent\Collection<int, \AdvisingApp\Audit\Models\Audit> $audits
- * @property-read int|null $audits_count
- * @property-read \Illuminate\Database\Eloquent\Model $createdBy
- * @method static \AdvisingApp\Project\Database\Factories\ProjectFactory factory($count = null, $state = [])
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Project newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Project newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Project onlyTrashed()
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Project query()
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Project whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Project whereCreatedById($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Project whereCreatedByType($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Project whereDeletedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Project whereDescription($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Project whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Project whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Project whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Project withTrashed(bool $withTrashed = true)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Project withoutTrashed()
- * @mixin \Eloquent
- */
-	#[\AllowDynamicProperties]
-	class IdeHelperProject {}
 }
 
 namespace AdvisingApp\Prospect\Models{
@@ -6534,7 +6530,7 @@ namespace AdvisingApp\Workflow\Models{
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property \Illuminate\Support\Carbon|null $deleted_at
- * @property-read Model $details
+ * @property-read \Illuminate\Database\Eloquent\Model $details
  * @property-read \AdvisingApp\Workflow\Models\WorkflowRun $workflowRun
  * @method static \Illuminate\Database\Eloquent\Builder<static>|WorkflowRunStep newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|WorkflowRunStep newQuery()
@@ -6760,4 +6756,3 @@ namespace AdvisingApp\Workflow\Models{
 	#[\AllowDynamicProperties]
 	class IdeHelperWorkflowTrigger {}
 }
-
--- app-modules/project/src/Models/Project.php
+++ app-modules/project/src/Models/Project.php
@@ -48,9 +48,6 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use OwenIt\Auditing\Contracts\Auditable;
 
 #[ObservedBy([ProjectObserver::class])]
-/**
- * @mixin IdeHelperProject
- */
 class Project extends BaseModel implements Auditable
 {
     /** @use HasFactory<ProjectFactory> */
