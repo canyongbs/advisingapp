b2d8d50c1014b3041837dd42d4b79ea43adb206a
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2025-08-11 19:02:05 +0000

    Create the LoginMagicLink Model
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

---
+++ app-modules/authorization/database/migrations/2025_08_11_181204_create_login_magic_links_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Tpetry\PostgresqlEnhanced\Schema\Blueprint;
+use Tpetry\PostgresqlEnhanced\Support\Facades\Schema;
+
+return new class () extends Migration {
+    public function up(): void
+    {
+        Schema::create('login_magic_links', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('code');
+            $table->foreignUuid('user_id')->constrained('users')->cascadeOnDelete();
+            $table->timestamp('expires_at');
+            $table->timestamp('used_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('login_magic_links');
+    }
+};
---
+++ app-modules/authorization/src/Models/LoginMagicLink.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AdvisingApp\Authorization\Models;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Concerns\HasVersion4Uuids as HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\Multitenancy\Models\Concerns\UsesTenantConnection;
+
+class LoginMagicLink extends Model
+{
+    //    use HasFactory;
+    use HasUuids;
+    use UsesTenantConnection;
+
+    protected $fillable = [];
+
+    /**
+     * @return BelongsTo<User, $this>
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
