ac3f546bd0f976920d11f541f2e89817085fb723
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2025-08-13 16:05:10 +0000

    Add test for successful login and redirection using magic link
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- app-modules/authorization/tests/Tenant/Http/Controllers/MagicLinkLoginControllerTest.php
+++ app-modules/authorization/tests/Tenant/Http/Controllers/MagicLinkLoginControllerTest.php
@@ -1,9 +1,12 @@
 <?php
 
 use AdvisingApp\Authorization\Models\LoginMagicLink;
+use Filament\Facades\Filament;
 use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Str;
 
+use function Pest\Laravel\assertAuthenticatedAs;
 use function Pest\Laravel\get;
 
 it('requires a valid signed URL ', function () {
@@ -28,5 +31,34 @@ it('requires a valid signed URL ', function () {
 //it('rejects a magic link with an invalid code', function () {});
 //
 //it('rejects a magic link with a non-matching user ID', function () {});
-//
-//it('logs in the user and redirects to the admin panel home', function () {});
+
+it('logs in the user and redirects to the admin panel home', function () {
+    $code = Str::random();
+
+    $magicLink = LoginMagicLink::factory()->withCode($code)->create();
+
+    $panel = Filament::getPanel('admin');
+
+    get(URL::temporarySignedRoute(
+        name: 'magic-link.login',
+        expiration: now()->addMinutes(10)->toImmutable(),
+        parameters: [
+            'magicLink' => $magicLink->getKey(),
+            'payload' => urlencode(
+                Crypt::encrypt(
+                    [
+                        'code' => $code,
+                        'user_id' => $magicLink->user_id,
+                    ]
+                )
+            ),
+        ],
+    ))
+        ->assertRedirect($panel->getHomeUrl());
+
+    assertAuthenticatedAs($magicLink->user, $panel->getAuthGuard());
+
+    $magicLink->refresh();
+
+    expect($magicLink->used_at)->not->toBeNull();
+});
