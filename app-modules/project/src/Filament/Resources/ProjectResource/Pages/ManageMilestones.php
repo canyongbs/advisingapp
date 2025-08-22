dfd4ea14128ebd76fe44ddac37f22342ac9bb07f
Karl Murray <karl.murray@canyongbs.com>
2025-08-22 21:48:43 +0000

    [ADVAPP-1843]: Create Migration, Update ManageMilestones page.

--- app-modules/project/src/Filament/Resources/ProjectResource/Pages/ManageMilestones.php
+++ app-modules/project/src/Filament/Resources/ProjectResource/Pages/ManageMilestones.php
@@ -36,21 +36,20 @@
 
 namespace AdvisingApp\Project\Filament\Resources\ProjectResource\Pages;
 
-use AdvisingApp\Project\Filament\Resources\ProjectResource;
-use AdvisingApp\Project\Models\ProjectMilestone;
 use App\Features\ProjectMilestoneTargetDateFeature;
-use App\Filament\Tables\Columns\IdColumn;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Form;
-use Filament\Resources\Pages\ManageRelatedRecords;
-use Filament\Tables\Actions\CreateAction;
-use Filament\Tables\Actions\DeleteAction;
 use Filament\Tables\Actions\EditAction;
 use Filament\Tables\Columns\TextColumn;
-use Filament\Tables\Table;
+use Filament\Forms\Components\TextInput;
+use App\Features\ProjectMilestoneFeature;
+use App\Filament\Tables\Columns\IdColumn;
+use Filament\Tables\Actions\CreateAction;
+use Filament\Tables\Actions\DeleteAction;
+use AdvisingApp\Project\Models\ProjectMilestone;
+use Filament\Resources\Pages\ManageRelatedRecords;
+use AdvisingApp\Project\Filament\Resources\ProjectResource;
 
 class ManageMilestones extends ManageRelatedRecords
 {
