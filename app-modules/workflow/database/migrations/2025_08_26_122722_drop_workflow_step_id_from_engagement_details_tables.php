2384e3d5f9d3ddda805e2f3aa45c77ec32e6d50a
Payal Baldaniya <payal.baldaniya@canyongbs.com>
2025-08-26 14:46:07 +0000

    Remove workflow_step_id from engagement details and implement email engagement block with template selection

---
+++ app-modules/workflow/database/migrations/2025_08_26_122722_drop_workflow_step_id_from_engagement_details_tables.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Tpetry\PostgresqlEnhanced\Schema\Blueprint;
+use Tpetry\PostgresqlEnhanced\Support\Facades\Schema;
+
+return new class () extends Migration {
+    public function up(): void
+    {
+        Schema::table('workflow_engagement_email_details', function (Blueprint $table) {
+            $table->dropConstrainedForeignId('workflow_step_id');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('workflow_engagement_email_details', function (Blueprint $table) {
+            $table->foreignUuid('workflow_step_id')->constrained('workflow_steps');
+        });
+    }
+};
