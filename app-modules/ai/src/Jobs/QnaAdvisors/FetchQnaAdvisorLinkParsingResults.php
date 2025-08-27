0bbeadd11884bcb613030dbbf85bef4904c508bf
Dan Harrin <dan.harrin@canyongbs.com>
2025-08-27 11:23:10 +0000

    Move job
    Signed-off-by: Dan Harrin <dan.harrin@canyongbs.com>

--- app-modules/ai/src/Jobs/QnaAdvisors/FetchQnaAdvisorLinkParsingResults.php
+++ app-modules/ai/src/Jobs/QnaAdvisors/FetchQnaAdvisorLinkParsingResults.php
@@ -34,7 +34,7 @@
 </COPYRIGHT>
 */
 
-namespace AdvisingApp\Ai\Jobs;
+namespace AdvisingApp\Ai\Jobs\QnaAdvisors;
 
 use AdvisingApp\Ai\Models\QnaAdvisorLink;
 use AdvisingApp\Ai\Settings\AiIntegrationsSettings;
