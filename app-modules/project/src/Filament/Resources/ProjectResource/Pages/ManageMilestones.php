2e8a76669bf1377e2a77914d900cd744689b9a4e
krlmrr <34105282+krlmrr@users.noreply.github.com>
2025-08-28 20:55:35 +0000

    Update code formatting and copyright headers

--- app-modules/project/src/Filament/Resources/ProjectResource/Pages/ManageMilestones.php
+++ app-modules/project/src/Filament/Resources/ProjectResource/Pages/ManageMilestones.php
@@ -38,7 +38,6 @@ namespace AdvisingApp\Project\Filament\Resources\ProjectResource\Pages;
 
 use AdvisingApp\Project\Filament\Resources\ProjectResource;
 use AdvisingApp\Project\Models\ProjectMilestone;
-use App\Features\ProjectMilestoneTargetDateFeature;
 use App\Filament\Tables\Columns\IdColumn;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Select;
