12b75dd9e9442bfe0b7871d1b76793c4bcd5a134
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2025-09-04 19:58:15 +0000

    Start the flow of setting up web sockets different based on auth
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- widgets/qna-advisor/src/App.vue
+++ widgets/qna-advisor/src/App.vue
@@ -62,7 +62,7 @@ const messages = ref([]);
 const currentResponse = ref('');
 const isLoading = ref(false);
 const nextRequestOptions = ref(null);
-let privateChannel = null;
+let websocketChannel = null;
 
 const scriptUrl = new URL(document.currentScript.getAttribute('src'));
 const protocol = scriptUrl.protocol;
@@ -99,18 +99,19 @@ onMounted(async () => {
 });
 
 onUnmounted(() => {
-    if (privateChannel) {
-        privateChannel.stopListening('advisor-message.chunk');
-        privateChannel.stopListening('advisor-message.next-request-options');
+    if (websocketChannel) {
+        websocketChannel.stopListening('advisor-message.chunk');
+        websocketChannel.stopListening('advisor-message.next-request-options');
     }
     if (window.Echo) {
         window.Echo.disconnect();
     }
 });
 
-function setupWebsockets(config) {
+function setupWebsockets(config, authRequired = false) {
     try {
         window.Pusher = Pusher;
+
         window.Echo = new Echo({
             ...config,
             authorizer: (channel, options) => {
@@ -122,9 +123,9 @@ function setupWebsockets(config) {
                         }, {
                             headers: {
                                 'Content-Type': 'application/json',
-                                ...(authStore.getAccessToken ? { 'Authorization': `Bearer ${authStore.getAccessToken}` } : {})
+                                ...(authRequired && authStore.getAccessToken ? { 'Authorization': `Bearer ${authStore.getAccessToken}` } : {})
                             },
-                            withCredentials: true
+                            // withCredentials: true
                         })
                         .then(response => {
                             callback(false, response.data);
@@ -138,7 +139,11 @@ function setupWebsockets(config) {
         });
 
         if (chatId.value) {
-            privateChannel = window.Echo.channel(`qna-advisor-chat-${chatId.value}`)
+            let channelName = `qna-advisor-chat-${chatId.value}`;
+
+            websocketChannel = authRequired ? window.Echo.private(channelName) : window.Echo.channel(channelName)
+
+            websocketChannel
                 .listen('.qna-advisor-message.chunk', (data) => {
                     if (data.error) {
                         console.error('Advisor message error:', data.error);
@@ -221,6 +226,8 @@ async function authenticate(formData, node) {
                 if (typeof response.data.access_token !== 'undefined') {
                     authStore.$patch({ accessToken: response.data.access_token });
                     authentication.value.promptToAuthenticate = false;
+
+
                 }
             })
             .catch((error) => {
