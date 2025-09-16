3181b347c959f2ee35c1a71d6b1d33b716f6786c
Orrison <6799341+Orrison@users.noreply.github.com>
2025-09-16 19:06:57 +0000

    Update code formatting and copyright headers

--- app-modules/report/tests/Tenant/Filament/Widgets/ProspectMessagesDetailStatsTest.php
+++ app-modules/report/tests/Tenant/Filament/Widgets/ProspectMessagesDetailStatsTest.php
@@ -13,6 +13,9 @@
     - You may not provide the software to third parties as a hosted or managed
       service, where the service provides users with access to any substantial set of
       the features or functionality of the software.
+    - You may not move, change, disable, or circumvent the license key functionality
+      in the software, and you may not remove or obscure any functionality in the
+      software that is protected by the license key.
     - You may not alter, remove, or obscure any licensing, copyright, or other notices
       of the licensor in the software. Any use of the licensor’s trademarks is subject
       to applicable law.
