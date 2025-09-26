0dac28dd078d3405fbe8802640626716e4dfb0b8
Jayushi Jain <jayushi.jain@canyongbs.com>
2025-09-26 10:03:23 +0000

    remove feature flag

--- app-modules/ai/database/migrations/2025_09_23_142058_add_is_generate_prospects_enabled_column_to_qna_advisors_table.php
+++ app-modules/ai/database/migrations/2025_09_23_142058_add_is_generate_prospects_enabled_column_to_qna_advisors_table.php
@@ -34,7 +34,6 @@
 </COPYRIGHT>
 */
 
-use App\Features\QnaAdvisorGenerateProspectsFeature;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 use Tpetry\PostgresqlEnhanced\Schema\Blueprint;
@@ -47,16 +46,12 @@ return new class () extends Migration {
             Schema::table('qna_advisors', function (Blueprint $table) {
                 $table->boolean('is_generate_prospects_enabled')->default(false);
             });
-
-            QnaAdvisorGenerateProspectsFeature::activate();
         });
     }
 
     public function down(): void
     {
         DB::transaction(function () {
-            QnaAdvisorGenerateProspectsFeature::deactivate();
-
             Schema::table('qna_advisors', function (Blueprint $table) {
                 $table->dropColumn('is_generate_prospects_enabled');
             });
