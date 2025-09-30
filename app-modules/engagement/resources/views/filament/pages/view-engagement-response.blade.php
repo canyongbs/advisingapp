e2bf45c69664899545b6494e2fd6eed62c0a82ac
krlmrr <34105282+krlmrr@users.noreply.github.com>
2025-09-30 20:07:35 +0000

    Update code formatting and copyright headers

--- app-modules/engagement/resources/views/filament/pages/view-engagement-response.blade.php
+++ app-modules/engagement/resources/views/filament/pages/view-engagement-response.blade.php
@@ -9,22 +9,22 @@
     Notice:
 
     - You may not provide the software to third parties as a hosted or managed
-    service, where the service provides users with access to any substantial set of
-    the features or functionality of the software.
+      service, where the service provides users with access to any substantial set of
+      the features or functionality of the software.
     - You may not move, change, disable, or circumvent the license key functionality
-    in the software, and you may not remove or obscure any functionality in the
-    software that is protected by the license key.
+      in the software, and you may not remove or obscure any functionality in the
+      software that is protected by the license key.
     - You may not alter, remove, or obscure any licensing, copyright, or other notices
-    of the licensor in the software. Any use of the licensor’s trademarks is subject
-    to applicable law.
+      of the licensor in the software. Any use of the licensor’s trademarks is subject
+      to applicable law.
     - Canyon GBS LLC respects the intellectual property rights of others and expects the
-    same in return. Canyon GBS™ and Advising App™ are registered trademarks of
-    Canyon GBS LLC, and we are committed to enforcing and protecting our trademarks
-    vigorously.
+      same in return. Canyon GBS™ and Advising App™ are registered trademarks of
+      Canyon GBS LLC, and we are committed to enforcing and protecting our trademarks
+      vigorously.
     - The software solution, including services, infrastructure, and code, is offered as a
-    Software as a Service (SaaS) by Canyon GBS LLC.
+      Software as a Service (SaaS) by Canyon GBS LLC.
     - Use of this software implies agreement to the license terms and conditions as stated
-    in the Elastic License 2.0.
+      in the Elastic License 2.0.
 
     For more information or inquiries please visit our website at
     https://www.canyongbs.com or contact us via email at legal@canyongbs.com.
@@ -37,7 +37,10 @@
 
 <x-filament-panels::page>
     <div>
-        <x-filament::link :href="\AdvisingApp\Engagement\Filament\Pages\Inbox::getUrl()" icon="heroicon-m-arrow-left">
+        <x-filament::link
+            :href="\AdvisingApp\Engagement\Filament\Pages\Inbox::getUrl()"
+            icon="heroicon-m-arrow-left"
+        >
             Back to Inbox
         </x-filament::link>
     </div>
@@ -45,15 +48,20 @@
     {{ $this->infolist }}
 
     @if (
-            ($this->record->sender instanceof Prospect || $this->record->sender instanceof Student) &&
+        ($this->record->sender instanceof Prospect || $this->record->sender instanceof Student) &&
             auth()->user()->can('create', [
-                Engagement::class,
-                $this->record->sender instanceof Prospect ? $this->record->sender : null,
-            ])
-        )
-        <div class="grid gap-6" x-data="{ isReplying: false }">
+                    Engagement::class,
+                    $this->record->sender instanceof Prospect ? $this->record->sender : null,
+                ]))
+        <div
+            class="grid gap-6"
+            x-data="{ isReplying: false }"
+        >
             <div class="flex items-center gap-3">
-                <x-filament::button icon="heroicon-s-arrow-uturn-left" x-on:click="isReplying = !isReplying">
+                <x-filament::button
+                    icon="heroicon-s-arrow-uturn-left"
+                    x-on:click="isReplying = !isReplying"
+                >
                     Reply
                 </x-filament::button>
 
@@ -62,11 +70,19 @@
                 </x-filament::button>
             </div>
 
-            <form class="grid gap-6" x-show="isReplying" wire:submit.prevent="reply">
+            <form
+                class="grid gap-6"
+                x-show="isReplying"
+                wire:submit.prevent="reply"
+            >
                 {{ $this->replyForm }}
 
                 <div>
-                    <x-filament::button type="submit" wire:target="reply" icon="heroicon-m-paper-airplane">
+                    <x-filament::button
+                        type="submit"
+                        wire:target="reply"
+                        icon="heroicon-m-paper-airplane"
+                    >
                         Send Reply
                     </x-filament::button>
                 </div>
