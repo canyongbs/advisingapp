5494b258ecf82ca20f61eaf4ead9cd5ffc30a67e
Karl Murray <karl.murray@canyongbs.com>
2025-09-30 13:24:00 +0000

    [ADVAPP-1982]: Remove DB::transactions

--- app-modules/ai/database/migrations/2025_09_19_105542_add_interaction_id_column_to_qna_advisor_threads_table.php
+++ app-modules/ai/database/migrations/2025_09_19_105542_add_interaction_id_column_to_qna_advisor_threads_table.php
@@ -35,26 +35,21 @@
 */
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Support\Facades\DB;
 use Tpetry\PostgresqlEnhanced\Schema\Blueprint;
 use Tpetry\PostgresqlEnhanced\Support\Facades\Schema;
 
 return new class () extends Migration {
     public function up(): void
     {
-        DB::transaction(function () {
-            Schema::table('qna_advisor_threads', function (Blueprint $table) {
-                $table->foreignUuid('interaction_id')->nullable()->constrained();
-            });
+        Schema::table('qna_advisor_threads', function (Blueprint $table) {
+            $table->foreignUuid('interaction_id')->nullable()->constrained();
         });
     }
 
     public function down(): void
     {
-        DB::transaction(function () {
-            Schema::table('qna_advisor_threads', function (Blueprint $table) {
-                $table->dropColumn('interaction_id');
-            });
+        Schema::table('qna_advisor_threads', function (Blueprint $table) {
+            $table->dropColumn('interaction_id');
         });
     }
 };
