a9921e3da36a65514c5741722bb63a4e7fbb0ffa
Karl Murray <karl@karlm.dev>
2025-09-30 09:42:38 -0500

    [ADVAPP-1982]: Feature Flag Cleanup:  QnaAdvisorThreadInteractionRelationshipFeature (#1931)
    * [ADVAPP-1982]: Feature Flag Cleanup:  QnaAdvisorThreadInteractionRelationshipFeature
    
    * [ADVAPP-1982]: Remove DB::transactions
    
    ---------
    
    Co-authored-by: Karl Murray <karl.murray@canyongbs.com>

--- app-modules/ai/database/migrations/2025_09_19_105542_add_interaction_id_column_to_qna_advisor_threads_table.php
+++ app-modules/ai/database/migrations/2025_09_19_105542_add_interaction_id_column_to_qna_advisor_threads_table.php
@@ -34,32 +34,22 @@
 </COPYRIGHT>
 */
 
-use App\Features\QnaAdvisorThreadInteractionRelationshipFeature;
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Support\Facades\DB;
 use Tpetry\PostgresqlEnhanced\Schema\Blueprint;
 use Tpetry\PostgresqlEnhanced\Support\Facades\Schema;
 
 return new class () extends Migration {
     public function up(): void
     {
-        DB::transaction(function () {
-            Schema::table('qna_advisor_threads', function (Blueprint $table) {
-                $table->foreignUuid('interaction_id')->nullable()->constrained();
-            });
-
-            QnaAdvisorThreadInteractionRelationshipFeature::activate();
+        Schema::table('qna_advisor_threads', function (Blueprint $table) {
+            $table->foreignUuid('interaction_id')->nullable()->constrained();
         });
     }
 
     public function down(): void
     {
-        DB::transaction(function () {
-            QnaAdvisorThreadInteractionRelationshipFeature::deactivate();
-
-            Schema::table('qna_advisor_threads', function (Blueprint $table) {
-                $table->dropColumn('interaction_id');
-            });
+        Schema::table('qna_advisor_threads', function (Blueprint $table) {
+            $table->dropColumn('interaction_id');
         });
     }
 };
