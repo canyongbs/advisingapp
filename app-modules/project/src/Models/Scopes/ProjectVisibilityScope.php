8f0f0b9226b0299796e9b5dcc6e5ac199be62828
Karl Murray <karl.murray@canyongbs.com>
2025-10-09 18:50:21 +0000

    [ADVAPP-1848]: Fix liniting issue.

--- app-modules/project/src/Models/Scopes/ProjectVisibilityScope.php
+++ app-modules/project/src/Models/Scopes/ProjectVisibilityScope.php
@@ -36,6 +36,7 @@
 
 namespace AdvisingApp\Project\Models\Scopes;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Scope;
