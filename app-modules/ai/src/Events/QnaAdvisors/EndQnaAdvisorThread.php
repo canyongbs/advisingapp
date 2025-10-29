56de475b4f2dddcab6f80dfa5528b44b1dcf7c24
rebecca-canyon <198644076+rebecca-canyon@users.noreply.github.com>
2025-10-29 19:08:36 +0000

    Update code formatting and copyright headers

--- app-modules/ai/src/Events/QnaAdvisors/EndQnaAdvisorThread.php
+++ app-modules/ai/src/Events/QnaAdvisors/EndQnaAdvisorThread.php
@@ -40,7 +40,6 @@ use AdvisingApp\Ai\Models\QnaAdvisorThread;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 
