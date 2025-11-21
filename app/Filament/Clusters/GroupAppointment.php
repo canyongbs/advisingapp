2c5b0dce0eddae1576bf1b8a75b4582e9df0517f
jayushi-canyon <jayushi.jain@canyongbs.com>
2025-11-21 17:56:28 +0530

    Change navigation count

--- app/Filament/Clusters/GroupAppointment.php
+++ app/Filament/Clusters/GroupAppointment.php
@@ -41,7 +41,7 @@ use UnitEnum;
 
 class GroupAppointment extends Cluster
 {
-    protected static ?int $navigationSort = 60;
+    protected static ?int $navigationSort = 70;
 
     protected static string | UnitEnum | null $navigationGroup = 'CRM';
 
