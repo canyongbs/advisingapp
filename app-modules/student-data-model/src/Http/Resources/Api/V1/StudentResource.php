f23925af2d3eecd7baf037e02133f77a3229aedf
amit-canyon <amit.solanki@canyongbs.com>
2025-11-21 23:09:40 +0530

    Fix duplicate gender field in StudentResource to improve data consistency

--- app-modules/student-data-model/src/Http/Resources/Api/V1/StudentResource.php
+++ app-modules/student-data-model/src/Http/Resources/Api/V1/StudentResource.php
@@ -63,7 +63,6 @@ class StudentResource extends JsonResource
             'birthdate' => $this->resource->birthdate ? $this->resource->birthdate->format('Y-m-d') : null,
             'hsgrad' => $this->resource->hsgrad ? (HsGradeTypeChangeFeature::active() ? Date::parse($this->resource->hsgrad)->format('Y-m-d') : $this->resource->hsgrad) : null,
             'gender' => $this->resource->gender,
-            'gender' => $this->resource->gender,
             'sms_opt_out' => $this->resource->sms_opt_out,
             'email_bounce' => $this->resource->email_bounce,
             'dual' => $this->resource->dual,
