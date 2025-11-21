c7220d3ac4b76c4a8a0cd21217549c8ac5c434c2
Dan Harrin <dan.harrin@canyongbs.com>
2025-11-21 16:33:25 +0000

    Rearrange image order
    Signed-off-by: Dan Harrin <dan.harrin@canyongbs.com>

--- app-modules/authorization/resources/views/components/login-resource-portal-card.blade.php
+++ app-modules/authorization/resources/views/components/login-resource-portal-card.blade.php
@@ -32,7 +32,7 @@
 </COPYRIGHT>
 --}}
 <div class="@container flex h-full items-center rounded-xl bg-turkish-300 p-6 text-white">
-    <div class="@sm:flex-row flex flex-1 flex-col items-center justify-between gap-x-4 gap-y-6">
+    <div class="@sm:flex-row @sm:items-center flex flex-1 flex-col items-start justify-between gap-x-4 gap-y-6">
         <div>
             <p class="text-sm font-semibold text-gray-800">Resource Portal</p>
             <p class="my-4 text-sm text-gray-700">Visit our resource portal for answers to frequently asked product and
@@ -44,7 +44,7 @@
             >Visit Resource Portal</a>
         </div>
         <img
-            class="block max-h-28"
+            class="@sm:order-last order-first block max-h-28"
             src="{{ Vite::asset('resources/images/changelog_image.svg') }}"
             alt="Drawing of a man aside an open book"
         />
--- app-modules/authorization/resources/views/components/login-version-card.blade.php
+++ app-modules/authorization/resources/views/components/login-version-card.blade.php
@@ -32,7 +32,7 @@
 </COPYRIGHT>
 --}}
 <div class="@container flex h-full items-center rounded-xl bg-turkish-500 p-6 text-white">
-    <div class="@sm:flex-row flex flex-1 flex-col items-center justify-between gap-x-4 gap-y-6">
+    <div class="@sm:flex-row @sm:items-center flex flex-1 flex-col items-start justify-between gap-x-4 gap-y-6">
         <div>
             <p class="text-sm font-semibold text-white">Version {{ app('current-version') }} is now available!</p>
             <p class="my-4 text-sm text-white">Your instance of Advising App&#174; was automatically updated with our
@@ -44,7 +44,7 @@
             >Learn More</a>
         </div>
         <img
-            class="block max-h-28"
+            class="@sm:order-last order-first block max-h-28"
             src="{{ Vite::asset('resources/images/version_image.svg') }}"
             alt="Drawing of a man touching a floating card displaying information"
         />
