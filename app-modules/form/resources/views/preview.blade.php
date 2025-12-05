eb1722ce20d6e0414a302fca956b29a82529e85c
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2025-12-05 14:59:42 -0500

    Fix rendering of forms and applications widgets in preview
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- app-modules/form/resources/views/preview.blade.php
+++ app-modules/form/resources/views/preview.blade.php
@@ -38,15 +38,7 @@
 <x-layouts.app title="Form Preview">
     <div class="flex items-center justify-center px-4 py-16">
         <div class="w-full max-w-4xl">
-            <script
-                src="{{ asset('js/widgets/form/advising-app-form-widget.js') }}"
-                type="module"
-            ></script>
-
-            <form-embed
-                url="{{ route('forms.api.preview', $form) }}"
-                preview="true"
-            ></form-embed>
+            {!! resolve(GenerateSubmissibleEmbedCode::class)->handle($form, true) !!}
         </div>
     </div>
 </x-layouts.app>
