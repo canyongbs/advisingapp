6b43df26e08a400d9c0991cc7cd672a7d9247f2c
jayushi-canyon <jayushi.jain@canyongbs.com>
2025-12-25 02:16:31 +0530

    [ADVAPP-2233]: Modify confidential task scoping to include auditors and managers of related projects (#2158)
    * add where clause
    
    * add test cases
    
    * Add new condition

--- app-modules/task/src/Models/Scopes/ConfidentialTaskScope.php
+++ app-modules/task/src/Models/Scopes/ConfidentialTaskScope.php
@@ -62,7 +62,23 @@ class ConfidentialTaskScope implements Scope
                 ->orWhereHas('confidentialAccessProjects', function (Builder $query) {
                     $query->whereHas('createdBy', function (Builder $query) {
                         $query->where('created_by_id', auth()->id());
-                    });
+                    })
+                        ->orWhereHas('managerUsers', function (Builder $query) {
+                            $query->where('user_id', auth()->id());
+                        })
+                        ->orWhereHas('managerTeams', function (Builder $query) {
+                            $query->whereHas('users', function (Builder $query) {
+                                $query->where('users.id', auth()->id());
+                            });
+                        })
+                        ->orWhereHas('auditorUsers', function (Builder $query) {
+                            $query->where('user_id', auth()->id());
+                        })
+                        ->orWhereHas('auditorTeams', function (Builder $query) {
+                            $query->whereHas('users', function (Builder $query) {
+                                $query->where('users.id', auth()->id());
+                            });
+                        });
                 });
         });
     }
