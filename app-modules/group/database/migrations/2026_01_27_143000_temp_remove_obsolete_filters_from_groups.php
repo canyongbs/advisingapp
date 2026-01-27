7ffb332c83ea2b4ec2f93d6f2c61c6939c195543
payal-canyon <payal.baldaniya@canyongbs.com>
2026-01-27 22:39:51 +0530

    feat(migration): add migration to remove obsolete filter types from segments

---
+++ app-modules/group/database/migrations/2026_01_27_143000_temp_remove_obsolete_filters_from_groups.php
@@ -0,0 +1,88 @@
+<?php
+
+/*
+<COPYRIGHT>
+
+    Copyright © 2016-2026, Canyon GBS LLC. All rights reserved.
+
+    Advising App™ is licensed under the Elastic License 2.0. For more details,
+    see https://github.com/canyongbs/advisingapp/blob/main/LICENSE.
+
+    Notice:
+
+    - You may not provide the software to third parties as a hosted or managed
+      service, where the service provides users with access to any substantial set of
+      the features or functionality of the software.
+    - You may not move, change, disable, or circumvent the license key functionality
+      in the software, and you may not remove or obscure any functionality in the
+      software that is protected by the license key.
+    - You may not alter, remove, or obscure any licensing, copyright, or other notices
+      of the licensor in the software. Any use of the licensor’s trademarks is subject
+      to applicable law.
+    - Canyon GBS LLC respects the intellectual property rights of others and expects the
+      same in return. Canyon GBS™ and Advising App™ are registered trademarks of
+      Canyon GBS LLC, and we are committed to enforcing and protecting our trademarks
+      vigorously.
+    - The software solution, including services, infrastructure, and code, is offered as a
+      Software as a Service (SaaS) by Canyon GBS LLC.
+    - Use of this software implies agreement to the license terms and conditions as stated
+      in the Elastic License 2.0.
+
+    For more information or inquiries please visit our website at
+    https://www.canyongbs.com or contact us via email at legal@canyongbs.com.
+
+</COPYRIGHT>
+*/
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
+
+return new class () extends Migration {
+    public function up(): void
+    {
+        DB::transaction(function () {
+            $obsoleteFilterTypes = ['performanceCumErn', 'performanceFirstGen'];
+
+            DB::table('segments')
+                ->whereNotNull('filters')
+                ->chunkById(100, function (Collection $segments) use ($obsoleteFilterTypes) {
+                    foreach ($segments as $segment) {
+                        $originalFilters = $segment->filters;
+                        $filters = json_decode($segment->filters, associative: true) ?? [];
+
+                        if (empty($filters)) {
+                            continue;
+                        }
+
+                        $modified = false;
+
+                        if (isset($filters['queryBuilder']['rules']) && is_array($filters['queryBuilder']['rules'])) {
+                            foreach ($filters['queryBuilder']['rules'] as $ruleKey => $rule) {
+                                if (isset($rule['type']) && in_array($rule['type'], $obsoleteFilterTypes)) {
+                                    unset($filters['queryBuilder']['rules'][$ruleKey]);
+                                    $modified = true;
+                                }
+                            }
+                        }
+
+                        if (! $modified) {
+                            continue;
+                        }
+
+                        $updatedFilters = json_encode($filters);
+
+                        if ($updatedFilters === $originalFilters) {
+                            continue;
+                        }
+
+                        DB::table('segments')
+                            ->where('id', $segment->id)
+                            ->update([
+                                'filters' => $updatedFilters,
+                            ]);
+                    }
+                });
+        });
+    }
+};
