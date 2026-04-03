9be6ec80ffe2814823a8cbcdf0d35cc51143a5db
payal-canyon <payal.baldaniya@canyongbs.com>
2026-04-03 19:07:37 +0530

    Add minimum booking lead time (in hours) for personal and group appointments

--- app-modules/meeting-center/src/Actions/GetAvailableGroupAppointmentSlots.php
+++ app-modules/meeting-center/src/Actions/GetAvailableGroupAppointmentSlots.php
@@ -55,7 +55,7 @@ class GetAvailableGroupAppointmentSlots
      */
     public function __invoke(BookingGroup $bookingGroup, int $year, int $month): array
     {
-        $members = $bookingGroup->allMembers();
+        $members = $bookingGroup->allMembers()->load('personalBookingPage');
 
         if ($members->isEmpty()) {
             return [];
