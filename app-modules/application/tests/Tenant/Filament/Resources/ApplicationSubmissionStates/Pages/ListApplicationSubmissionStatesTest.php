209521e512bcaf6ba0ff9fb945e3c5062a7e725d
monali-canyon <263637457+monali-canyon@users.noreply.github.com>
2026-04-08 13:21:57 +0000

    Update code formatting and copyright headers

--- app-modules/application/tests/Tenant/Filament/Resources/ApplicationSubmissionStates/Pages/ListApplicationSubmissionStatesTest.php
+++ app-modules/application/tests/Tenant/Filament/Resources/ApplicationSubmissionStates/Pages/ListApplicationSubmissionStatesTest.php
@@ -36,7 +36,6 @@
 
 use AdvisingApp\Application\Enums\ApplicationSubmissionStateClassification;
 use AdvisingApp\Application\Filament\Resources\ApplicationSubmissionStates\Pages\ListApplicationSubmissionStates;
-use AdvisingApp\Application\Models\ApplicationSubmission;
 use AdvisingApp\Application\Models\ApplicationSubmissionState;
 
 use function Pest\Livewire\livewire;
