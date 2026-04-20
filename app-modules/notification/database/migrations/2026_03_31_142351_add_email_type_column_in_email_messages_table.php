b8210b59af575a3b2fd54aa07c3d54013f8854ee
Mittal Solanki <mittal.parmar@canyongbs.com>
2026-04-20 20:54:29 +0530

    Feature flag cleanup changes

--- app-modules/notification/database/migrations/2026_03_31_142351_add_email_type_column_in_email_messages_table.php
+++ app-modules/notification/database/migrations/2026_03_31_142351_add_email_type_column_in_email_messages_table.php
@@ -34,26 +34,21 @@
 </COPYRIGHT>
 */
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Support\Facades\DB;
 use Tpetry\PostgresqlEnhanced\Schema\Blueprint;
 use Tpetry\PostgresqlEnhanced\Support\Facades\Schema;
 
 return new class () extends Migration {
     public function up(): void
     {
-        DB::transaction(function () {
-            Schema::table('email_messages', function (Blueprint $table) {
-                $table->string('email_type')->initial('transactional');
-            });
+        Schema::table('email_messages', function (Blueprint $table) {
+            $table->string('email_type')->initial('transactional');
         });
     }
 
     public function down(): void
     {
-        DB::transaction(function () {
-            Schema::table('email_messages', function (Blueprint $table) {
-                $table->dropColumn('email_type');
-            });
+        Schema::table('email_messages', function (Blueprint $table) {
+            $table->dropColumn('email_type');
         });
     }
 };
