ff1bd83c8650c64cd34c94a3fe1779ea97be6182
Mittal Solanki <mittal.parmar@canyongbs.com>
2026-04-21 14:29:42 +0530

    Feature flag cleanup changes

--- app-modules/authorization/database/migrations/2026_03_26_152610_delete_login_magic_links_table.php
+++ app-modules/authorization/database/migrations/2026_03_26_152610_delete_login_magic_links_table.php
@@ -35,7 +35,6 @@
 */
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Support\Facades\DB;
 use Tpetry\PostgresqlEnhanced\Schema\Blueprint;
 use Tpetry\PostgresqlEnhanced\Support\Facades\Schema;
 
@@ -46,15 +45,13 @@ return new class () extends Migration {
     }
 
     public function down(): void
-    {
-        DB::transaction(function () {
-            Schema::create('login_magic_links', function (Blueprint $table) {
-                $table->uuid('id')->primary();
-                $table->text('code');
-                $table->foreignUuid('user_id')->constrained('users')->cascadeOnDelete();
-                $table->timestamp('used_at')->nullable();
-                $table->timestamps();
-            });
+    {        
+        Schema::create('login_magic_links', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->text('code');
+            $table->foreignUuid('user_id')->constrained('users')->cascadeOnDelete();
+            $table->timestamp('used_at')->nullable();
+            $table->timestamps();
         });
     }
 };
