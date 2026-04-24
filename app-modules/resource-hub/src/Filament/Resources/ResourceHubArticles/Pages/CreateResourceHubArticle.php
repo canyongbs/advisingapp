956bfc0f7771d0718683285812d4e162874e85a9
monali-canyon <263637457+monali-canyon@users.noreply.github.com>
2026-04-24 14:00:13 +0000

    Update code formatting and copyright headers

--- app-modules/resource-hub/src/Filament/Resources/ResourceHubArticles/Pages/CreateResourceHubArticle.php
+++ app-modules/resource-hub/src/Filament/Resources/ResourceHubArticles/Pages/CreateResourceHubArticle.php
@@ -41,7 +41,6 @@ use AdvisingApp\ResourceHub\Filament\Resources\ResourceHubArticles\ResourceHubAr
 use AdvisingApp\ResourceHub\Models\ResourceHubCategory;
 use AdvisingApp\ResourceHub\Models\ResourceHubQuality;
 use AdvisingApp\ResourceHub\Models\ResourceHubStatus;
-use App\Models\User;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
