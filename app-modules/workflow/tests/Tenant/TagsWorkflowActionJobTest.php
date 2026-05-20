1202a796a5c64ebcc6ebf602a09d24f9c76629c6
rebecca-canyon <198644076+rebecca-canyon@users.noreply.github.com>
2026-05-20 05:53:36 +0000

    Update code formatting and copyright headers

--- app-modules/workflow/tests/Tenant/TagsWorkflowActionJobTest.php
+++ app-modules/workflow/tests/Tenant/TagsWorkflowActionJobTest.php
@@ -1,6 +1,39 @@
 <?php
 
-use AdvisingApp\Notification\Models\Contracts\Subscribable;
+/*
+<COPYRIGHT>
+
+    Copyright © 2016-2026, Canyon GBS Inc. All rights reserved.
+
+    Advising App® is licensed under the Elastic License 2.0. For more details,
+    see https://github.com/canyongbs/advisingapp/blob/main/LICENSE.
+
+    Notice:
+
+    - You may not provide the software to third parties as a hosted or managed
+      service, where the service provides users with access to any substantial set of
+      the features or functionality of the software.
+    - You may not move, change, disable, or circumvent the license key functionality
+      in the software, and you may not remove or obscure any functionality in the
+      software that is protected by the license key.
+    - You may not alter, remove, or obscure any licensing, copyright, or other notices
+      of the licensor in the software. Any use of the licensor’s trademarks is subject
+      to applicable law.
+    - Canyon GBS Inc. respects the intellectual property rights of others and expects the
+      same in return. Canyon GBS® and Advising App® are registered trademarks of
+      Canyon GBS Inc., and we are committed to enforcing and protecting our trademarks
+      vigorously.
+    - The software solution, including services, infrastructure, and code, is offered as a
+      Software as a Service (SaaS) by Canyon GBS Inc.
+    - Use of this software implies agreement to the license terms and conditions as stated
+      in the Elastic License 2.0.
+
+    For more information or inquiries please visit our website at
+    https://www.canyongbs.com or contact us via email at legal@canyongbs.com.
+
+</COPYRIGHT>
+*/
+
 use AdvisingApp\Prospect\Models\Prospect;
 use AdvisingApp\StudentDataModel\Models\Contracts\Educatable;
 use AdvisingApp\StudentDataModel\Models\Student;
@@ -8,10 +41,8 @@ use AdvisingApp\Workflow\Jobs\TagsWorkflowActionJob;
 use AdvisingApp\Workflow\Models\WorkflowRun;
 use AdvisingApp\Workflow\Models\WorkflowRunStep;
 use AdvisingApp\Workflow\Models\WorkflowTagsDetails;
-use AdvisingApp\Workflow\Models\WorkflowTrigger;
 use App\Enums\TagType;
 use App\Models\Tag;
-use App\Models\User;
 use Illuminate\Support\Facades\Bus;
 
 it('will execute appropriateonly on each educatable in the group', function (Educatable $educatable, bool $removePrior) {
@@ -21,15 +52,15 @@ it('will execute appropriateonly on each educatable in the group', function (Edu
         'related_type' => $educatable->getMorphClass(),
         'related_id' => $educatable->getKey(),
     ]);
-    
+
     $priorTags = $educatable->tags()->pluck('tag_id');
 
     $tags = Tag::factory(3)->create([
-        'type' => match($educatable->getMorphClass()) {
+        'type' => match ($educatable->getMorphClass()) {
             'student' => TagType::Student,
             'prospect' => TagType::Prospect,
             default => null,
-        }
+        },
     ]);
 
     $tagsDetails = WorkflowTagsDetails::factory()->create([
@@ -70,7 +101,7 @@ it('will execute appropriateonly on each educatable in the group', function (Edu
     $expectedTagIds = $removePrior
         ? $tags->pluck('id')->toArray()
         : [...$priorTags, ...$tags->pluck('id')->toArray()];
-    
+
     $relatedModel->tags()
         ->each(function (Tag $tag) use ($expectedTagIds) {
             expect($tag)->toBeInstanceOf(Tag::class);
