d65ab1322a0134cca4716a3fe5b21cdab8b11eb0
Mittal Solanki <mittal.parmar@canyongbs.com>
2026-06-08 23:22:24 +0530

    remove transaction changes

--- app-modules/application/database/migrations/2026_05_22_100001_create_application_notification_users_table.php
+++ app-modules/application/database/migrations/2026_05_22_100001_create_application_notification_users_table.php
@@ -35,28 +35,23 @@
 */
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Support\Facades\DB;
 use Tpetry\PostgresqlEnhanced\Schema\Blueprint;
 use Tpetry\PostgresqlEnhanced\Support\Facades\Schema;
 
 return new class () extends Migration {
     public function up(): void
     {
-        DB::transaction(function () {
-            Schema::create('application_notification_users', function (Blueprint $table) {
-                $table->uuid('id')->primary();
-                $table->foreignUuid('application_id')->constrained('applications')->cascadeOnDelete();
-                $table->foreignUuid('user_id')->constrained('users')->cascadeOnDelete();
-                $table->unique(['application_id', 'user_id']);
-                $table->timestamps();
-            });
+        Schema::create('application_notification_users', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('application_id')->constrained('applications')->cascadeOnDelete();
+            $table->foreignUuid('user_id')->constrained('users')->cascadeOnDelete();
+            $table->unique(['application_id', 'user_id']);
+            $table->timestamps();
         });
     }
 
     public function down(): void
     {
-        DB::transaction(function () {
-            Schema::dropIfExists('application_notification_users');
-        });
+        Schema::dropIfExists('application_notification_users');
     }
 };
