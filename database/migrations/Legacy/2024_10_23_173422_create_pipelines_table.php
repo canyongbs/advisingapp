d49406513b05a675f41a64f7b52680cc8eff3a56
Rebecca Sheridan <rebecca.sheridan@canyongbs.com>
2026-07-17 16:41:20 +0000

    Recreate errantly deleted migration

--- database/migrations/Legacy/2024_10_23_173422_create_pipelines_table.php
+++ database/migrations/Legacy/2024_10_23_173422_create_pipelines_table.php
@@ -41,18 +41,19 @@ use Illuminate\Support\Facades\Schema;
 return new class () extends Migration {
     public function up(): void
     {
-        Schema::create('pipeline_stages', function (Blueprint $table) {
+        Schema::create('pipelines', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('name');
-            $table->foreignUuid('pipeline_id')->constrained()->onDelete('cascade');
-            $table->unsignedBigInteger('order');
+            $table->text('description');
+            $table->foreignUuid('user_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignUuid('segment_id')->constrained()->onDelete('cascade');
+            $table->string('default_stage')->nullable();
             $table->timestamps();
-            $table->unique(['name', 'pipeline_id']);
         });
     }
 
     public function down(): void
     {
-        Schema::dropIfExists('pipeline_stages');
+        Schema::dropIfExists('pipelines');
     }
 };
