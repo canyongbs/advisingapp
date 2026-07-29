049dd5830391066406bca97fe4659f2db87c57f0
Mittal Parmar <mittal.parmar@canyongbs.com>
2026-07-29 22:33:30 +0530

    [ADVAPP-2835]: Cleanup: Event Archiving Feature (#2728)
    feature flag cleanup changes

--- app-modules/meeting-center/database/migrations/2026_07_16_162334_add_archived_at_to_events_table.php
+++ app-modules/meeting-center/database/migrations/2026_07_16_162334_add_archived_at_to_events_table.php
@@ -34,32 +34,22 @@
 </COPYRIGHT>
 */
 
-use App\Features\EventArchivingFeature;
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Support\Facades\DB;
 use Tpetry\PostgresqlEnhanced\Schema\Blueprint;
 use Tpetry\PostgresqlEnhanced\Support\Facades\Schema;
 
 return new class () extends Migration {
     public function up(): void
     {
-        DB::transaction(function () {
-            Schema::table('events', function (Blueprint $table) {
-                $table->timestamp('archived_at')->nullable();
-            });
-
-            EventArchivingFeature::activate();
+        Schema::table('events', function (Blueprint $table) {
+            $table->timestamp('archived_at')->nullable();
         });
     }
 
     public function down(): void
     {
-        DB::transaction(function () {
-            Schema::table('events', function (Blueprint $table) {
-                $table->dropColumn('archived_at');
-            });
-
-            EventArchivingFeature::deactivate();
+        Schema::table('events', function (Blueprint $table) {
+            $table->dropColumn('archived_at');
         });
     }
 };
