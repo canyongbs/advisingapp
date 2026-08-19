437340ae8c39f9b0f86f2cdde1314322e2e9a911
rebecca-canyon <rebecca.sheridan@canyongbs.com>
2026-08-19 12:14:47 -0400

    Name which disk logo is copied to

--- database/migrations/2026_08_14_214258_tmp_seed_notification_settings.php
+++ database/migrations/2026_08_14_214258_tmp_seed_notification_settings.php
@@ -59,7 +59,7 @@ return new class () extends Migration {
 
                 $logo = $notificationSetting->getFirstMedia('logo');
 
-                $logo?->copy(NotificationSettings::getSettingsPropertyModel('notifications.logo'), 'logo');
+                $logo?->copy(NotificationSettings::getSettingsPropertyModel('notifications.logo'), 'logo', 's3');
             }
 
             NotificationSettingsFeature::activate();
