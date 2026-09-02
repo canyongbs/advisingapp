946027ef7c541a60d5bbf130d31565454840c9b1
monali4-canyon <monali.vaghamshi+4@canyongbs.com>
2026-09-02 22:15:38 +0530

    Rename unique constraint for forms name migration to improve clarity

--- app-modules/form/database/migrations/2026_09_02_110000_convert_forms_name_to_citext.php
+++ app-modules/form/database/migrations/2026_09_02_110000_convert_forms_name_to_citext.php
@@ -57,7 +57,7 @@ return new class () extends Migration {
 
     protected bool $usesSoftDeletes = true;
 
-    private string $uniqueConstraint = 'forms_name_unique_index';
+    private string $uniqueConstraint = 'forms_name_unique';
 
     public function up(): void
     {
