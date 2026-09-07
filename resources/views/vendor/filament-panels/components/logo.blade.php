d799794a4be51c94fa9e3c7e741a35d5648acfe3
jayushi-canyon <jayushi.jain@canyongbs.com>
2026-09-07 14:18:30 +0530

    change width pixel

--- resources/views/vendor/filament-panels/components/logo.blade.php
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -60,9 +60,8 @@
         src="{{ $logoUrl }}"
         alt="{{ config('app.name') }}"
         @class([
-            'h-full max-h-9',
-            'w-[50%]' => $logo->mime_type == 'image/svg+xml',
-            'max-w-[50%]' => $logo->mime_type != 'image/svg+xml',
+            'h-full max-h-9 max-w-[180px]',
+            'w-full' => $logo->mime_type == 'image/svg+xml',
             'dark:hidden' => $darkLogo,
         ])
     />
@@ -79,9 +78,8 @@
             src="{{ $darkLogoUrl }}"
             alt="{{ config('app.name') }}"
             @class([
-            'h-full max-h-9',
-            'w-[50%]' => $darkLogo->mime_type == 'image/svg+xml',
-            'max-w-[50%]' => $darkLogo->mime_type != 'image/svg+xml',
+            'h-full max-h-9 max-w-[180px]',
+            'w-full' => $darkLogo->mime_type == 'image/svg+xml',
             'hidden dark:block',
         ])
         />
