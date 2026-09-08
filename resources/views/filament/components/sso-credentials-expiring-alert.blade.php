02a99e75301499abc094b236b8fb8b68051b17f5
Mittal Solanki <mittal.parmar@canyongbs.com>
2026-09-08 17:56:33 +0530

    [ADVAPP-2209]: Revert SSO alert banner color rendering changes per review

--- resources/views/filament/components/sso-credentials-expiring-alert.blade.php
+++ resources/views/filament/components/sso-credentials-expiring-alert.blade.php
@@ -32,12 +32,13 @@
     </COPYRIGHT>
 --}}
 
+@php
+    use Filament\Support\Colors\Color;
+@endphp
+
 <div
-    class="sticky top-16 z-10 flex h-10 items-center px-6 py-2 text-sm font-medium"
-    style="
-        background-color: {{ $color->getRgb() }};
-        color: {{ \Filament\Support\Colors\Color::isLight($color->getRgb()) ? 'rgb(0, 0, 0)' : 'rgb(255, 255, 255)' }};
-    "
+    class="bg-custom-600 sticky top-16 z-10 flex h-10 items-center px-6 py-2 text-sm font-medium text-white"
+    style="--color-600: {{ Color::all()[$color->value][600] }}"
     wire:loading.remove
 >
     The single sign-on key will expire within 45 days and must be updated to prevent service interruption.
