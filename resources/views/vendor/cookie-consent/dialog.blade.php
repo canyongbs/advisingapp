7563b702ce0b6310904116d3226f70e55196c5c1
monali4-canyon <monali.vaghamshi+4@canyongbs.com>
2026-09-09 16:20:40 +0530

    style: Update cookie consent button border color for improved visibility

--- resources/views/vendor/cookie-consent/dialog.blade.php
+++ resources/views/vendor/cookie-consent/dialog.blade.php
@@ -53,7 +53,7 @@
                 <div class="mt-2 flex-shrink-0 w-full sm:mt-0 sm:w-auto">
                     <button
                         type="button"
-                        class="js-cookie-consent-agree cookie-consent__agree inline-flex items-center justify-center rounded-lg border border-primary-600 bg-white px-4 py-2 text-sm font-semibold text-primary-600 shadow-sm transition hover:bg-primary-50 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2"
+                        class="js-cookie-consent-agree cookie-consent__agree inline-flex items-center justify-center rounded-lg border border-black/5 bg-white px-4 py-2 text-sm font-semibold text-primary-600 shadow-sm transition hover:bg-primary-50 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2"
                     >
                         @if(! empty(app(PortalSettings::class)->gdpr_banner_button_label))
                             {{ str(app(PortalSettings::class)->gdpr_banner_button_label->getLabel()) }}
