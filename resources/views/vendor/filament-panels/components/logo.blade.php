7cc8967a166105243000d0df838288fae75a0c23
jayushi-canyon <jayushi.jain@canyongbs.com>
2026-09-22 20:32:47 +0530

    Add brand name

--- resources/views/vendor/filament-panels/components/logo.blade.php
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -81,12 +81,12 @@
     <img
         src="{{ Vite::asset('resources/images/default-logo-light-201124.svg') }}"
         class="h-full max-h-9 max-w-full min-h-8 dark:hidden block"
-        alt="Logo"
+        alt="AdvisingApp"
     />
 
     <img
         src="{{ Vite::asset('resources/images/default-logo-dark-201124.svg') }}"
         class="h-full max-h-9 max-w-full min-h-8 hidden dark:block"
-        alt="Logo"
+        alt="AdvisingApp"
     />
 @endif
--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -47,15 +47,15 @@
             @if ($notificationLogo?->hasMedia('logo'))
                 <img src="{{ $notificationLogo->getFirstMediaUrl('logo') }}"
                      style="height: 75px; max-height: 75px; max-width: 100vw;"
-                     alt="Logo">
+                     alt="AdvisingApp">
             @elseif ($themeSettings->is_logo_active && $logo)
                  <img src="{{ $logo->getUrl() }}"
                      style="height: 75px; max-height: 75px; max-width: 100vw;"
-                     alt="Logo">
+                     alt="AdvisingApp">
             @else
                 <img src="{{ url(Vite::asset('resources/images/default-logo-light-201124.svg')) }}"
                      style="height: 75px; max-height: 75px; max-width: 100vw;"
-                     alt="Logo">
+                     alt="AdvisingApp">
             @endif
         </a>
     </td>
